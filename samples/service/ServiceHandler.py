26e8ec801072a78c5f687c7e797524d3deedd0b6
Anthony Tuininga <anthony.tuininga@gmail.com>
2009-03-05 17:25:13 +0000

    Added some documentation on how services are run with cx_Freeze.

--- samples/service/ServiceHandler.py
+++ samples/service/ServiceHandler.py
@@ -1,20 +1,32 @@
+"""
+Implements a simple service using cx_Freeze.
+
+This sample makes use of cx_PyGenLib (http://cx-pygenlib.sourceforge.net) and
+cx_Logging (http://cx-logging.sourceforge.net).
+"""
+
 import cx_Logging
 import cx_Threads
 import sys
 
 class Handler(object):
 
+    # no parameters are permitted; all configuration should be placed in the
+    # configuration file and handled in the Initialize() method
     def __init__(self):
         cx_Logging.Info("creating handler instance")
         self.stopEvent = cx_Threads.Event()
 
+    # called when the service is starting
     def Initialize(self, configFileName):
         cx_Logging.Info("initializing: config file name is %r", configFileName)
 
+    # called when the service is starting immediately after Initialize()
     def Run(self):
         cx_Logging.Info("running service....")
         self.stopEvent.Wait()
 
+    # called when the service is being stopped by the service manager GUI
     def Stop(self):
         cx_Logging.Info("stopping service...")
         self.stopEvent.Set()
