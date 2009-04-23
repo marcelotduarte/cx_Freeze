16b70d6c8f080a0391a2c04b8f4716ee3819caf5
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2009-04-23 21:21:43 +0000

    Ensure that Py_Finalize() is called even when exceptions take place!

--- source/bases/ConsoleKeepPath.c
+++ source/bases/ConsoleKeepPath.c
@@ -49,6 +49,7 @@ int main(int argc, char **argv)
 #else
     const char *fileName;
 #endif
+    int status;
 
     // initialize Python
 #if PY_MAJOR_VERSION >= 3
@@ -75,10 +76,11 @@ int main(int argc, char **argv)
 #endif
 
     // do the work
+    status = 0;
     if (ExecuteScript(fileName) < 0)
-        return 1;
+        status = 1;
 
     Py_Finalize();
-    return 0;
+    return status;
 }
 
