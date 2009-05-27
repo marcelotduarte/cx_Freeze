428442736958959d46a8ec21dad5c6ae9ad81134
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2009-05-27 17:30:00 +0000

    Added support for Python 3.x.

--- source/bases/Win32GUI.c
+++ source/bases/Win32GUI.c
@@ -6,6 +6,28 @@
 #include <Python.h>
 #include <windows.h>
 
+// define PyInt_* macros for Python 3.x
+#ifndef PyInt_Check
+#define PyInt_Check             PyLong_Check
+#define PyInt_AsLong            PyLong_AsLong
+#endif
+
+
+// define methods for manipulating strings
+#if PY_MAJOR_VERSION >= 3
+    #define cxString_Check              PyUnicode_Check
+    #define cxString_Format             PyUnicode_Format
+    #define cxString_FromAscii(str) \
+        PyUnicode_DecodeASCII(str, strlen(str), NULL)
+    #define cxString_Join               PyUnicode_Join
+#else
+    #define cxString_Check              PyString_Check
+    #define cxString_Format             PyString_Format
+    #define cxString_FromAscii(str)     PyString_FromString(str)
+    #define cxString_Join               _PyString_Join
+#endif
+
+
 //-----------------------------------------------------------------------------
 // FatalError()
 //   Handle a fatal error.
@@ -20,64 +42,22 @@ static int FatalError(
 
 
 //-----------------------------------------------------------------------------
-// StringifyObject()
-//   Stringify a Python object.
+// DisplayMessageFromPythonObjects()
+//   Display message from Python objects. The Python objects are expected to be
+// of the correct type (Unicode for Python 3.x and string for Python 2.x). The
+// method returns -1 as a convenience to the caller.
 //-----------------------------------------------------------------------------
-static char *StringifyObject(
-    PyObject *object,                   // object to stringify
-    PyObject **stringRep)               // string representation
+static int DisplayMessageFromPythonObjects(
+    PyObject *caption,                  // caption
+    PyObject *message)                  // message
 {
-    if (object) {
-        *stringRep = PyObject_Str(object);
-        if (*stringRep)
-            return PyString_AS_STRING(*stringRep);
-        return "Unable to stringify";
-    }
-
-    // object is NULL
-    *stringRep = NULL;
-    return "None";
-}
-
-
-//-----------------------------------------------------------------------------
-// FatalPythonErrorNoTraceback()
-//   Handle a fatal Python error without traceback.
-//-----------------------------------------------------------------------------
-static int FatalPythonErrorNoTraceback(
-    PyObject *origType,                 // exception type
-    PyObject *origValue,                // exception value
-    char *message)                      // message to display
-{
-    PyObject *typeStrRep, *valueStrRep, *origTypeStrRep, *origValueStrRep;
-    char *totalMessage, *typeStr, *valueStr, *origTypeStr, *origValueStr;
-    PyObject *type, *value, *traceback;
-    int totalMessageLength;
-    char *messageFormat;
-
-    // fetch error and string representations of the error
-    PyErr_Fetch(&type, &value, &traceback);
-    origTypeStr = StringifyObject(origType, &origTypeStrRep);
-    origValueStr = StringifyObject(origValue, &origValueStrRep);
-    typeStr = StringifyObject(type, &typeStrRep);
-    valueStr = StringifyObject(value, &valueStrRep);
-
-    // fill out the message to be displayed
-    messageFormat = "Type: %s\nValue: %s\nOther Type: %s\nOtherValue: %s\n%s";
-    totalMessageLength = strlen(origTypeStr) + strlen(origValueStr) +
-            strlen(typeStr) + strlen(valueStr) + strlen(message) +
-            strlen(messageFormat) + 1;
-    totalMessage = malloc(totalMessageLength);
-    if (!totalMessage)
-        return FatalError("Out of memory!");
-    sprintf(totalMessage, messageFormat, typeStr, valueStr, origTypeStr,
-            origValueStr, message);
-
-    // display the message
-    MessageBox(NULL, totalMessage,
-            "cx_Freeze: Python error in main script (traceback unavailable)",
+#if PY_MAJOR_VERSION >= 3
+    MessageBoxW(NULL, PyUnicode_AS_UNICODE(message),
+            PyUnicode_AS_UNICODE(caption), MB_ICONERROR);
+#else
+    MessageBox(NULL, PyString_AS_STRING(message), PyString_AS_STRING(caption),
             MB_ICONERROR);
-    free(totalMessage);
+#endif
     return -1;
 }
 
@@ -99,40 +79,82 @@ static PyObject *ArgumentValue(
 
 
 //-----------------------------------------------------------------------------
+// FatalPythonErrorNoTraceback()
+//   Handle a fatal Python error without traceback.
+//-----------------------------------------------------------------------------
+static int FatalPythonErrorNoTraceback(
+    PyObject *origValue,                // exception value
+    char *contextMessage)               // context message to display
+{
+    PyObject *contextMessageObj, *message, *format, *formatArgs, *caption;
+    PyObject *type, *value, *traceback;
+
+    // create caption and message objects
+    PyErr_Fetch(&type, &value, &traceback);
+    contextMessageObj = cxString_FromAscii(contextMessage);
+    if (!contextMessageObj)
+        return FatalError("Cannot create context message string object.");
+    format = cxString_FromAscii("%s\nException: %s\nOriginal Exception: %s");
+    if (!format)
+        return FatalError("Cannot create format string object.");
+    formatArgs = PyTuple_New(3);
+    if (!formatArgs)
+        return FatalError("Cannot create format args tuple.");
+    PyTuple_SET_ITEM(formatArgs, 0, ArgumentValue(contextMessageObj));
+    PyTuple_SET_ITEM(formatArgs, 1, ArgumentValue(value));
+    PyTuple_SET_ITEM(formatArgs, 2, ArgumentValue(origValue));
+    message = cxString_Format(format, formatArgs);
+    if (!message)
+        return FatalError("Cannot format exception values.");
+    caption = cxString_FromAscii("cx_Freeze: Python error in main script "
+            "(traceback unavailable)");
+    if (!caption)
+        return FatalError("Cannot create caption string object.");
+
+    // display message box
+    return DisplayMessageFromPythonObjects(caption, message);
+}
+
+
+//-----------------------------------------------------------------------------
 // HandleSystemExitException()
 //   Handles a system exit exception differently. If an integer value is passed
 // through then that becomes the exit value; otherwise the string value of the
 // value passed through is displayed in a message box.
 //-----------------------------------------------------------------------------
-static void HandleSystemExitException()
+static int HandleSystemExitException()
 {
-    char *message, *caption = "cx_Freeze: Application Terminated";
-    PyObject *type, *value, *traceback, *valueStr, *captionObj;
+    PyObject *caption, *message, *type, *value, *traceback, *code;
     int exitCode = 0;
 
     PyErr_Fetch(&type, &value, &traceback);
+    caption = PyObject_GetAttrString(value, "caption");
+    if (!caption || !cxString_Check(caption)) {
+        PyErr_Clear();
+        caption = cxString_FromAscii("cx_Freeze: Application Terminated");
+        if (!caption)
+            return FatalError("Cannot create caption string object.");
+    }
 
-    captionObj = PyObject_GetAttrString(value, "caption");
-    if (captionObj && PyString_Check(captionObj))
-        caption = PyString_AS_STRING(captionObj);
-    if (PyInstance_Check(value)) {
-        PyObject *code = PyObject_GetAttrString(value, "code");
-        if (code) {
-            Py_DECREF(value);
-            value = code;
-            if (value == Py_None)
-                Py_Exit(0);
-        }
+    code = PyObject_GetAttrString(value, "code");
+    if (!code)
+        PyErr_Clear();
+    else {
+        value = code;
+        if (value == Py_None)
+            Py_Exit(0);
     }
     if (PyInt_Check(value))
         exitCode = PyInt_AsLong(value);
     else {
-        message = StringifyObject(value, &valueStr);
-        MessageBox(NULL, message, caption, MB_ICONERROR);
-        Py_XDECREF(valueStr);
+        message = PyObject_Str(value);
+        if (!message)
+            return FatalError("Cannot get string representation of messsage.");
+        DisplayMessageFromPythonObjects(caption, message);
         exitCode = 1;
     }
     Py_Exit(exitCode);
+    return -1;
 }
 
 
@@ -143,92 +165,72 @@ static void HandleSystemExitException()
 static int FatalScriptError()
 {
     PyObject *type, *value, *traceback, *argsTuple, *module, *method, *result;
-    char *tracebackStr, *caption = "cx_Freeze: Python error in main script";
-    PyObject *captionObj, *hook, *origHook;
-    int tracebackLength, i;
+    PyObject *caption, *hook, *origHook, *emptyString, *message;
 
     // if a system exception, handle it specially
     if (PyErr_ExceptionMatches(PyExc_SystemExit))
-        HandleSystemExitException();
+        return HandleSystemExitException();
 
     // get the exception details
     PyErr_Fetch(&type, &value, &traceback);
+    argsTuple = PyTuple_New(3);
+    if (!argsTuple)
+        return FatalPythonErrorNoTraceback(value, "Cannot create args tuple.");
+    PyTuple_SET_ITEM(argsTuple, 0, ArgumentValue(type));
+    PyTuple_SET_ITEM(argsTuple, 1, ArgumentValue(value));
+    PyTuple_SET_ITEM(argsTuple, 2, ArgumentValue(traceback));
 
     // call the exception hook
     hook = PySys_GetObject("excepthook");
     origHook = PySys_GetObject("__excepthook__");
     if (hook && hook != origHook) {
-        argsTuple = PyTuple_New(3);
-        if (!argsTuple)
-            return FatalPythonErrorNoTraceback(type, value,
-                    "cannot create hook args tuple");
-        PyTuple_SET_ITEM(argsTuple, 0, ArgumentValue(type));
-        PyTuple_SET_ITEM(argsTuple, 1, ArgumentValue(value));
-        PyTuple_SET_ITEM(argsTuple, 2, ArgumentValue(traceback));
         result = PyObject_CallObject(hook, argsTuple);
-        Py_DECREF(argsTuple);
         if (!result)
-            return FatalPythonErrorNoTraceback(type, value,
-                    "error in sys.excepthook");
-        Py_DECREF(result);
-        return 1;
+            return FatalPythonErrorNoTraceback(value,
+                    "Error in sys.excepthook.");
+        return -1;
     }
 
     // import the traceback module
     module = PyImport_ImportModule("traceback");
     if (!module)
-        return FatalPythonErrorNoTraceback(type, value,
+        return FatalPythonErrorNoTraceback(value,
                 "Cannot import traceback module.");
 
     // get the format_exception method
     method = PyObject_GetAttrString(module, "format_exception");
-    Py_DECREF(module);
     if (!method)
-        return FatalPythonErrorNoTraceback(type, value,
+        return FatalPythonErrorNoTraceback(value,
               "Cannot get format_exception method.");
-
-    // create a tuple for the arguments
-    argsTuple = PyTuple_New(3);
-    if (!argsTuple) {
-        Py_DECREF(method);
-        return FatalPythonErrorNoTraceback(type, value,
-                "Cannot create arguments tuple for traceback.");
-    }
-    PyTuple_SET_ITEM(argsTuple, 0, ArgumentValue(type));
-    PyTuple_SET_ITEM(argsTuple, 1, ArgumentValue(value));
-    PyTuple_SET_ITEM(argsTuple, 2, ArgumentValue(traceback));
 
     // call the format_exception method
     result = PyObject_CallObject(method, argsTuple);
-    Py_DECREF(method);
-    Py_DECREF(argsTuple);
     if (!result)
-        return FatalPythonErrorNoTraceback(type, value,
-                "Failed calling format_exception method.");
+        return FatalPythonErrorNoTraceback(value,
+                "Exception raised when calling format_exception.");
 
-    // determine length of string representation of formatted traceback
-    tracebackLength = 1;
-    for (i = 0; i < PyList_GET_SIZE(result); i++)
-        tracebackLength += PyString_GET_SIZE(PyList_GET_ITEM(result, i));
+    // convert to string
+    emptyString = cxString_FromAscii("");
+    if (!emptyString)
+        return FatalPythonErrorNoTraceback(value,
+                "Cannot create empty string object.");
+    message = cxString_Join(emptyString, result);
+    if (!message)
+        return FatalPythonErrorNoTraceback(value,
+                "Cannot join exception strings.");
 
-    // create a string representation of the formatted traceback
-    tracebackStr = malloc(tracebackLength);
-    if (!tracebackStr) {
-        Py_DECREF(result);
-        return FatalError("Out of memory!");
+    // acquire caption
+    caption = PyObject_GetAttrString(value, "caption");
+    if (!caption || !cxString_Check(caption)) {
+        PyErr_Clear();
+        caption = cxString_FromAscii("cx_Freeze: Python error in main script");
+        if (!caption)
+            return FatalPythonErrorNoTraceback(value,
+                    "Cannot create default caption string.");
     }
-    tracebackStr[0] = '\0';
-    for (i = 0; i < PyList_GET_SIZE(result); i++)
-        strcat(tracebackStr, PyString_AS_STRING(PyList_GET_ITEM(result, i)));
-    Py_DECREF(result);
 
-    // bring up the error
-    captionObj = PyObject_GetAttrString(value, "caption");
-    if (captionObj && PyString_Check(captionObj))
-        caption = PyString_AS_STRING(captionObj);
-    MessageBox(NULL, tracebackStr, caption, MB_ICONERROR);
-    Py_Finalize();
-    return 1;
+    // display message box
+    return DisplayMessageFromPythonObjects(caption, message);
 }
 
 
@@ -245,18 +247,43 @@ int WINAPI WinMain(
     LPSTR commandLine,                  // command line
     int showFlag)                       // show flag
 {
+#if PY_MAJOR_VERSION >= 3
+    char fileName[MAXPATHLEN + 1];
+    wchar_t **wargv, *wfileName;
+    int i, size;
+#else
     const char *fileName;
+#endif
     int status;
 
     // initialize Python
     Py_NoSiteFlag = 1;
     Py_FrozenFlag = 1;
     Py_IgnoreEnvironmentFlag = 1;
+#if PY_MAJOR_VERSION >= 3
+    Py_SetPythonHome(L"");
+    wargv = PyMem_Malloc(sizeof(wchar_t*) * __argc);
+    if (!wargv)
+        return 2;
+    for (i = 0; i < __argc; i++) {
+        size = strlen(__argv[i]);
+        wargv[i] = PyMem_Malloc(sizeof(wchar_t) * (size + 1));
+        if (!wargv[i])
+            return 2;
+        mbstowcs(wargv[i], __argv[i], size + 1);
+    }
+    Py_SetProgramName(wargv[0]);
+    wfileName = Py_GetProgramFullPath();
+    wcstombs(fileName, wfileName, MAXPATHLEN);
+    Py_Initialize();
+    PySys_SetArgv(__argc, wargv);
+#else
     Py_SetPythonHome("");
     Py_SetProgramName(__argv[0]);
     fileName = Py_GetProgramFullPath();
     Py_Initialize();
     PySys_SetArgv(__argc, __argv);
+#endif
 
     // do the work
     status = 0;
