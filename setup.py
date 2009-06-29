87014bff4a70b50749107fa4a7c7937d7fa1545d
Anthony Tuininga <anthony.tuininga@gmail.com>
2009-06-29 17:02:04 +0000

    Only build the Win32Service base executable in Python 2.x for now.

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ if sys.platform == "win32":
             extra_link_args = ["-mwindows"])
     extensions.append(gui)
     moduleInfo = find_cx_Logging()
-    if moduleInfo is not None:
+    if moduleInfo is not None and sys.version_info[:2] < (3, 0):
         includeDir, libraryDir = moduleInfo
         includeDirs.append(includeDir)
         service = Extension("cx_Freeze.bases.Win32Service",
