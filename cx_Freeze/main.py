84989eb7e380b69961010730dee2a590d3dceec4
Anthony Tuininga <anthony.tuininga@gmail.com>
2010-01-04 21:23:43 +0000

    Copyright extended to 2010.

--- cx_Freeze/main.py
+++ cx_Freeze/main.py
@@ -19,7 +19,7 @@ installation."""
 VERSION = \
 """
 %%prog %s
-Copyright (c) 2007-2009 Anthony Tuininga. All rights reserved.
+Copyright (c) 2007-2010 Anthony Tuininga. All rights reserved.
 Copyright (c) 2001-2006 Computronix Corporation. All rights reserved.""" % \
         cx_Freeze.version
 
