dfadbfb33888e9ccea99ec754ac4007c5fef9834
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2010-07-05 17:57:15 +0000

    Only Python 2.5 still has the threading shutdown problem; all newer releases have this code happen automatically within Py_Finalize.

--- initscripts/ConsoleKeepPath.py
+++ initscripts/ConsoleKeepPath.py
@@ -12,7 +12,7 @@ importer = zipimport.zipimporter(INITSCRIPT_ZIP_FILE_NAME)
 code = importer.get_code(m.__name__)
 exec code in m.__dict__
 
-if sys.version_info[:2] >= (2, 5):
+if sys.version_info[:2] == (2, 5):
     module = sys.modules.get("threading")
     if module is not None:
         module._shutdown()
