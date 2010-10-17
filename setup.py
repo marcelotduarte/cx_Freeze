f0b7bfc4f8e55ddd3cd47d607315730029d200f7
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2010-10-17 04:05:14 +0000

    Preparing to release 4.2.1.

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ classifiers = [
 setup(name = "cx_Freeze",
         description = "create standalone executables from Python scripts",
         long_description = "create standalone executables from Python scripts",
-        version = "4.2",
+        version = "4.2.1",
         cmdclass = commandClasses,
         options = dict(bdist_rpm = dict(doc_files = docFiles),
                 install = dict(optimize = 1)),
