7c3f3ab8d8bd12c7d4070b7f8476a996d8156ea6
Anthony Tuininga <anthony.tuininga@gmail.com>
2012-11-20 13:52:00 -0700

    Added XML to reference the files as Almar Klein indicated is necessary.

--- cx_Freeze/freezer.py
+++ cx_Freeze/freezer.py
@@ -51,6 +51,9 @@ MSVCR_MANIFEST_TEMPLATE = """
     version="9.0.21022.8"
     processorArchitecture="{PROC_ARCH}"
     publicKeyToken="1fc8b3b9a1e18e3b"/>
+<file name="MSVCR90.DLL"/>
+<file name="MSVCM90.DLL"/>
+<file name="MSVCP90.DLL"/>
 </assembly>
 """
 
