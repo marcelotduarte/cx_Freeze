83e193b58612d0c3023480d9ad14fee64efd7f7c
Thomas Kluyver <thomas@kluyver.me.uk>
2013-01-16 16:22:42 +0000

    Display message to alleviate concerns about modules listed as missing.
    Because I've had to explain several times that this isn't a problem.

---
+++ samples/PyQt4/PyQt4app.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+app = QApplication(sys.argv)
+form = QDialog()
+form.show()
+app.exec_()
+
---
+++ samples/relimport/pkg1/sub4.py
@@ -0,0 +1,4 @@
+import sys
+
+sys.stdout.write("importing pkg1.sub4\n")
+
