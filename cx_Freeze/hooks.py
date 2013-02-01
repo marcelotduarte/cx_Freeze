cdb7c0219ba8e0f2f100a3850ca2bf780045c3b3
Philippe Pinard <philippe.pinard@gmail.com>
2013-02-01 13:03:43 +0100

    Add implicit packages required by library h5py.

--- cx_Freeze/hooks.py
+++ cx_Freeze/hooks.py
@@ -222,6 +222,14 @@ def load_gtk__gtk(finder, module):
     finder.IncludeModule("pangocairo")
 
 
+def load_h5py(finder, module):
+    """h5py module has a number of implicit imports"""
+    finder.IncludeModule('h5py.defs')
+    finder.IncludeModule('h5py.utils')
+    finder.IncludeModule('h5py._proxy')
+    finder.IncludeModule('h5py.api_gen')
+
+
 def load_matplotlib(finder, module):
     """the matplotlib module requires data to be found in mpl-data in the
        same directory as the frozen executable so oblige it"""
