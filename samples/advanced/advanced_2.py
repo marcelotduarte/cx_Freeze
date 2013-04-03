29a747b853eb0ee4e2adbe05cda5066eaf545f92
Jonas Gr?ger <jonas.groeger@gmail.com>
2013-04-03 07:23:29 +0200

    Made samples PEP8 compatible and consistent. Fixed zope example.

--- samples/advanced/advanced_2.py
+++ samples/advanced/advanced_2.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 
-sys.stdout.write("Hello from cx_Freeze Advanced #2\n\n")
+sys.stdout.write('Hello from cx_Freeze Advanced #2\n\n')
 
-module = __import__("testfreeze_2")
-
+module = __import__('testfreeze_2')
--- samples/relimport/pkg1/sub1.py
+++ samples/relimport/pkg1/sub1.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import sys
 
-sys.stdout.write("importing pkg1.sub1\n")
+sys.stdout.write('importing pkg1.sub1\n')
 
 from . import sub2
