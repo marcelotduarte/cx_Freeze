a46d7ae4567e53e7e63c20df1182a25142ceb28f
Dan McCombs <dmccombs@dyn.com>
2014-01-09 08:47:00 -0500

    Determine module name to exec the same was as is done in Console.py, fixing a bug where the frozen executable fails to launch in some cases when using the ConsoleSetLibPath init script. Also, change the exec statement to be compatible with Python 3. This tuple form also works as low as Python 2.3.

--- cx_Freeze/initscripts/ConsoleSetLibPath.py
+++ cx_Freeze/initscripts/ConsoleSetLibPath.py
@@ -8,13 +8,12 @@
 # the environment variable LD_LIBRARY_PATH is only checked at startup.
 #------------------------------------------------------------------------------
 
-import encodings
 import os
 import sys
-import warnings
 import zipimport
 
 paths = os.environ.get("LD_LIBRARY_PATH", "").split(os.pathsep)
+
 if DIR_NAME not in paths:
     paths.insert(0, DIR_NAME)
     os.environ["LD_LIBRARY_PATH"] = os.pathsep.join(paths)
@@ -28,10 +27,18 @@ os.environ["TK_LIBRARY"] = os.path.join(DIR_NAME, "tk")
 
 m = __import__("__main__")
 importer = zipimport.zipimporter(INITSCRIPT_ZIP_FILE_NAME)
-code = importer.get_code(m.__name__)
-exec code in m.__dict__
+
+if INITSCRIPT_ZIP_FILE_NAME != SHARED_ZIP_FILE_NAME:
+    moduleName = m.__name__
+else:
+    name, ext = os.path.splitext(os.path.basename(os.path.normcase(FILE_NAME)))
+    moduleName = "%s__main__" % name
+
+code = importer.get_code(moduleName)
+exec(code, m.__dict__)
 
 versionInfo = sys.version_info[:3]
+
 if versionInfo >= (2, 5, 0) and versionInfo <= (2, 6, 4):
     module = sys.modules.get("threading")
     if module is not None:
