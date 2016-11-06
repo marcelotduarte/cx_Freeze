886aae61bbfd9dad9468aa53b5dc6a4c8a4472e5
Anthony Tuininga <anthony.tuininga@gmail.com>
2016-11-05 19:00:31 -0600

    Bumped copyright into 2016.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cx_Freeze'
-copyright = u'2014, Anthony Tuininga'
+copyright = u'2016, Anthony Tuininga'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
