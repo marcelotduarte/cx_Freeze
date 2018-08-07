e101b6aae6515bae39606a39240ad9d529057d6b
Yann Diorcet <yann.diorcet@xenocs.com>
2018-08-08 00:09:23 +0200

    Don't fail on _AddVersionResource

--- cx_Freeze/freezer.py
+++ cx_Freeze/freezer.py
@@ -161,15 +161,20 @@ class Freezer(object):
             print(warning_msg)
             print("version must be specified")
             return
-        fileName = exe.targetName
-        versionInfo = VersionInfo(self.metadata.version,
-                comments = self.metadata.long_description,
-                description = self.metadata.description,
-                company = self.metadata.author,
-                product = self.metadata.name,
-                copyright = exe.copyright,
-                trademarks = exe.trademarks)
-        stamp(fileName, versionInfo)
+        try:
+            fileName = exe.targetName
+            versionInfo = VersionInfo(self.metadata.version,
+                    comments = self.metadata.long_description,
+                    description = self.metadata.description,
+                    company = self.metadata.author,
+                    product = self.metadata.name,
+                    copyright = exe.copyright,
+                    trademarks = exe.trademarks)
+            stamp(fileName, versionInfo)
+        except Exception as e:
+            print("*** WARNING *** unable to create version resource")
+            print("Error: %s" % e)
+            return
 
     def _CopyFile(self, source, target, copyDependentFiles,
             includeMode = False):
