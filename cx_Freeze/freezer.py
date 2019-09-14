marcelotduarte/cx_Freeze

commit de60196379cdecc6b1595d90a7ce142bc1b67da5

On windows, try to get dependent files of .exe or .dll. Other types are free of check. This speeds up and elimites or reduces binderror. (#492)

cx_Freeze/freezer.py
@@ -283,19 +283,21 @@ class Freezer(object):
         dependentFiles = self.dependentFiles.get(path)
         if dependentFiles is None:
             if sys.platform == "win32":
-                origPath = os.environ["PATH"]
-                os.environ["PATH"] = origPath + os.pathsep + \
-                        os.pathsep.join(sys.path)
-                import cx_Freeze.util
-                try:
-                    dependentFiles = cx_Freeze.util.GetDependentFiles(path)
-                except cx_Freeze.util.BindError as exc:
-                    # Sometimes this gets called when path is not actually a
-                    # library See issue 88
+                if path.endswith(('.exe', '.dll')):
+                    origPath = os.environ["PATH"]
+                    os.environ["PATH"] = origPath + os.pathsep + \
+                            os.pathsep.join(sys.path)
+                    try:
+                        dependentFiles = cx_Freeze.util.GetDependentFiles(path)
+                    except cx_Freeze.util.BindError as exc:
+                        # Sometimes this gets called when path is not actually a
+                        # library See issue 88
+                        dependentFiles = []
+                        fmt = "error during GetDependentFiles() of \"%s\": %s\n"
+                        sys.stderr.write(fmt % (path, str(exc)))
+                    os.environ["PATH"] = origPath
+                else:
                     dependentFiles = []
-                    fmt = "error during GetDependentFiles() of \"%s\": %s\n"
-                    sys.stderr.write(fmt % (path, str(exc)))
-                os.environ["PATH"] = origPath
             else:
                 dependentFiles = []
                 if sys.platform == "darwin":

commit 819d4d0d7d4be1d201f04f6373cf1e220e25aa37

fix a regression introduced in #492

cx_Freeze/freezer.py
@@ -254,7 +254,7 @@ class Freezer(object):
         dependentFiles = self.dependentFiles.get(path)
         if dependentFiles is None:
             if sys.platform == "win32":
-                if path.endswith(('.exe', '.dll')):
+                if path.endswith(('.exe', '.dll', '.pyd')):
                     origPath = os.environ["PATH"]
                     os.environ["PATH"] = origPath + os.pathsep + \
                             os.pathsep.join(sys.path)