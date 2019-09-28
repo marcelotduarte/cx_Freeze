marcelotduarte/cx_Freeze

commit de60196379cdecc6b1595d90a7ce142bc1b67da5

On windows, try to get dependent files of .exe or .dll. Other types are free of check. This speeds up and elimites or reduces binderror. (#492)

cx_Freeze/freezer.py
@@ -283,19 +283,21 @@ class Freezer(object):
         dependentFiles = self.dependentFiles.get(path)
         if dependentFiles is None:
             if sys.platform == "win32":
-                origPath = os.environ["PATH"]
-                os.environ["PATH"] = origPath + os.pathsep + \
-                        os.pathsep.join(sys.path)
-                import cx_Freeze.util
-                try:
-                    dependentFiles = cx_Freeze.util.GetDependentFiles(path)
-                except cx_Freeze.util.BindError as exc:
-                    # Sometimes this gets called when path is not actually a
-                    # library See issue 88
+                if path.endswith(('.exe', '.dll')):
+                    origPath = os.environ["PATH"]
+                    os.environ["PATH"] = origPath + os.pathsep + \
+                            os.pathsep.join(sys.path)
+                    try:
+                        dependentFiles = cx_Freeze.util.GetDependentFiles(path)
+                    except cx_Freeze.util.BindError as exc:
+                        # Sometimes this gets called when path is not actually a
+                        # library See issue 88
+                        dependentFiles = []
+                        fmt = "error during GetDependentFiles() of \"%s\": %s\n"
+                        sys.stderr.write(fmt % (path, str(exc)))
+                    os.environ["PATH"] = origPath
+                else:
                     dependentFiles = []
-                    fmt = "error during GetDependentFiles() of \"%s\": %s\n"
-                    sys.stderr.write(fmt % (path, str(exc)))
-                os.environ["PATH"] = origPath
             else:
                 dependentFiles = []
                 if sys.platform == "darwin":

commit 7566412ec2f1a6bf35312439cb1eca1e8e434a82

fix issues with dll dependencies (#512)
* fix a regression introduced in #492

* this is the second part to fix the regression and fix multiple get dependencies when the filename has different case

cx_Freeze/freezer.py
@@ -250,11 +250,12 @@ class Freezer(object):
         """Return the file's dependencies using platform-specific tools (the
            imagehlp library on Windows, otool on Mac OS X and ldd on Linux);
            limit this list by the exclusion lists as needed"""
+        path = os.path.normcase(path)
         dirname = os.path.dirname(path)
         dependentFiles = self.dependentFiles.get(path)
         if dependentFiles is None:
             if sys.platform == "win32":
-                if path.endswith(('.exe', '.dll')):
+                if path.endswith(('.exe', '.dll', '.pyd')):
                     origPath = os.environ["PATH"]
                     os.environ["PATH"] = origPath + os.pathsep + \
                             os.pathsep.join(sys.path)