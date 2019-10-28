ec522e22fc71f4aa20cb339b8d45e4ae27d0ff47
marcelotduarte <marcelotduarte@gmail.com>
2019-10-28 02:34:26 -0300

    update

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class build_ext(distutils.command.build_ext.build_ext):
             return
         if sys.platform == "win32" and self.compiler.compiler_type == "mingw32":
             ext.sources.append("source/bases/manifest.rc")
-        os.environ["LD_RUN_PATH"] = "${ORIGIN}:${ORIGIN}/lib"
+        os.environ["LD_RUN_PATH"] = "${ORIGIN}/../lib:${ORIGIN}/lib"
         objects = self.compiler.compile(ext.sources,
                 output_dir = self.build_temp,
                 include_dirs = ext.include_dirs,
