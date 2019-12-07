026c9af3b55fe80e08eac0d088438faa39e3ee77
Anthony Tuininga <anthony.tuininga@gmail.com>
2019-12-07 14:15:03 -0700

    Correct header documentation.

--- cx_Freeze/samples/distutils/setup.py
+++ cx_Freeze/samples/distutils/setup.py
@@ -1,4 +1,4 @@
-# A setup script to demonstrate the use of sqlite3
+# A setup script to demonstrate the use of distutils
 #
 # Run the build process by running the command 'python setup.py build'
 #
