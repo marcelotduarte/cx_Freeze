c6a61ec1b252b0e9ffc599467cca769e8a4d93b1
Marcelo Duarte <marcelotduarte@gmail.com>
2019-12-29 00:54:04 -0300

    verbose

--- cx_Freeze/samples/pytz/test_pytz.py
+++ cx_Freeze/samples/pytz/test_pytz.py
@@ -1,13 +1,16 @@
+'''sample to show the datetime in RFC1123 (timezone is required)'''
 from datetime import datetime
-from tzlocal import get_localzone
+import pytz
+import tzlocal
 
 RFC1123 = '%a, %d %b %Y %H:%M:%S %z'
 
 try:
-    tz = get_localzone()
-except:
-    import pytz
+    tz = tzlocal.get_localzone()
+    print('Using your local timezone:', tz.zone)
+except pytz.exceptions.UnknownTimeZoneError as exc:
+    print('Detected your local timezone:', exc.args[0])
+    print("WARNING: fail to load pytz timezones, fallback to UTC")
     tz = pytz.utc
-    print("WARNING: fail to load pytz timezones")
 finally:
     print(datetime.now(tz).strftime(RFC1123))
