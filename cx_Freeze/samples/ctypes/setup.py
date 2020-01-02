d7c14d78a586336467b11301f4593074b1ffa608
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2020-01-02 02:08:34 -0300

    ctypes sample to show issue #523 (#546)

---
+++ cx_Freeze/samples/ctypes/setup.py
@@ -0,0 +1,17 @@
+'''A setup script to demonstrate build using ctypes'''
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import setup, Executable
+
+setup(name='test_ctypes',
+      version='0.1',
+      description='cx_Freeze script to test ctypes',
+      executables=[Executable("test_ctypes.py")],
+      options={
+          'build_exe': {'zip_include_packages': ['*'],
+                        'zip_exclude_packages': []}
+          })
