688da41a29de98889271ae7147b29a8a30af6c55
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2020-01-02 22:27:31 -0300

    define version in one place and avoid circular dependencies (#552)

--- cx_Freeze/__init__.py
+++ cx_Freeze/__init__.py
@@ -7,7 +7,6 @@ elif sys.platform == "darwin":
     from cx_Freeze.macdist import bdist_dmg, bdist_mac
 from cx_Freeze.finder import Module, ModuleFinder
 from cx_Freeze.freezer import ConfigError, ConstantsModule, Executable, Freezer
-from cx_Freeze.main import main
 
-version = "6.1"
-__version__ = version
+__version__ = "6.1"
+version = __version__
