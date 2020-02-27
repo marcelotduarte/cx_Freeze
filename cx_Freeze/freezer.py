98e2cae8f0df3fc9569ba3125f44798ebb6e4fbd
Marcelo Duarte <marcelotduarte@gmail.com>
2020-02-27 17:42:32 -0300

    correctly set __package__ in a module in a zip file; zipimport set __package__ as None, then change it; the code was based on new zipimport for py38 and adapted to set __package__

--- cx_Freeze/freezer.py
+++ cx_Freeze/freezer.py
@@ -519,9 +519,10 @@ class Freezer(object):
                             ignore = ignorePatterns)
 
             # if an extension module is found in a package that is to be
-            # included in a zip file, save a Python loader in the zip file and
-            # copy the actual file to the build directory because shared
-            # libraries cannot be loaded from a zip file
+            # included in a zip file, copy the actual file to the build
+            # directory because shared libraries cannot be loaded from a
+            # zip file. The module will be loaded by ExtensionFinder based on
+            # importlib.machinery at __startup__ script
             if module.code is None and module.file is not None \
                     and not includeInFileSystem:
                 parts = module.name.split(".")[:-1]
--- cx_Freeze/initscripts/__startup__.py
+++ cx_Freeze/initscripts/__startup__.py
@@ -8,6 +8,8 @@ import os
 import sys
 import importlib.machinery
 import importlib.util
+from importlib import _bootstrap_external
+import zipimport
 
 class ExtensionFinder(importlib.machinery.PathFinder):
 
@@ -32,6 +34,100 @@ class ExtensionFinder(importlib.machinery.PathFinder):
                     return importlib.util.spec_from_loader(fullname, loader)
 
 sys.meta_path.append(ExtensionFinder)
+
+#adapted from https://github.com/python/cpython/pull/6809
+path_sep = _bootstrap_external.path_sep
+_module_type = type(sys)
+
+
+class ZipImporter(zipimport.zipimporter):
+
+    def load_module(self, fullname):
+        code, ispackage, modpath = _get_module_code(self, fullname)
+        mod = sys.modules.get(fullname)
+        if mod is None or not isinstance(mod, _module_type):
+            mod = _module_type(fullname)
+            sys.modules[fullname] = mod
+        mod.__loader__ = self
+
+        try:
+            path = _get_module_path(self, fullname)
+            if ispackage:
+                # add __path__ to the module *before* the code gets executed
+                fullpath = _bootstrap_external._path_join(self.archive, path)
+                mod.__path__ = [fullpath]
+                package = path.replace(path_sep, ".")
+            else:
+                package = path.replace(path_sep, ".").rpartition('.')[0]
+            mod.__package__ = package
+
+            if not hasattr(mod, '__builtins__'):
+                mod.__builtins__ = __builtins__
+            _bootstrap_external._fix_up_module(mod.__dict__, fullname, modpath)
+            exec(code, mod.__dict__)
+        except:
+            del sys.modules[fullname]
+            raise
+
+        try:
+            mod = sys.modules[fullname]
+        except KeyError:
+            raise ImportError('Loaded module %r not found in sys.modules' % fullname)
+        return mod
+
+    def __repr__(self):
+        return '<ZipImporter object "%s%s%s">' % (self.archive, path_sep, self.prefix)
+
+# _zip_searchorder defines how we search for a module in the Zip
+# archive: we first search for a package __init__, then for
+# non-package .pyc, and .py entries. The .pyc entries
+# are swapped by initzipimport() if we run in optimized mode. Also,
+# '/' is replaced by path_sep there.
+_zip_searchorder = (
+    (path_sep + '__init__.pyc', True, True),
+    (path_sep + '__init__.py', False, True),
+    ('.pyc', True, False),
+    ('.py', False, False),
+)
+
+# Given a module name, return the potential file path in the
+# archive (without extension).
+def _get_module_path(self, fullname):
+    return self.prefix + fullname.rpartition('.')[2]
+
+# Given a string buffer containing Python source code, compile it
+# and return a code object.
+def _compile_source(pathname, source):
+    source = importlib.util.decode_source(source)
+    return compile(source, pathname, 'exec', dont_inherit=True)
+
+# Get the code object associated with the module specified by 'fullname'.
+def _get_module_code(self, fullname):
+    path = _get_module_path(self, fullname)
+    for suffix, isbytecode, ispackage in _zip_searchorder:
+        fullpath = path + suffix
+        #_bootstrap._verbose_message('trying {}{}{}', self.archive, path_sep, fullpath, verbosity=2)
+        try:
+            toc_entry = self._files[fullpath]
+        except KeyError:
+            pass
+        else:
+            modpath = toc_entry[0]
+            if isbytecode:
+                code = self.get_code(fullname)
+            else:
+                data = self.get_data(fullname)
+                code = _compile_source(modpath, data)
+            if code is None:
+                # bad magic number or non-matching mtime in byte code, try next
+                continue
+            modpath = toc_entry[0]
+            return code, ispackage, modpath
+    else:
+        raise zipimport.ZipImportError("can't find module %r" % fullname)
+
+sys.path_hooks[0] = ZipImporter
+
 
 def run():
     baseName = os.path.normcase(os.path.basename(sys.executable))
