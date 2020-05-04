0c4c0978e76d0c9d766c1dd29cc75b02fe8624d7
Marcelo Duarte <marcelotduarte@gmail.com>
2020-05-04 04:54:35 -0300

    ident

--- cx_Freeze/samples/service/setup.py
+++ cx_Freeze/samples/service/setup.py
@@ -1,12 +1,13 @@
-"""A simple setup script for creating a Windows service.
-   See the comments in the Config.py and ServiceHandler.py files for
-   more information on how to set this up.
+"""
+A simple setup script for creating a Windows service.
+See the comments in the Config.py and ServiceHandler.py files for more
+information on how to set this up.
 
-   Installing the service is done with the option --install <Name> and
-   uninstalling the service is done with the option --uninstall <Name>. The
-   value for <Name> is intended to differentiate between different invocations
-   of the same service code -- for example for accessing different databases or
-   using different configuration files.
+Installing the service is done with the option --install <Name> and
+uninstalling the service is done with the option --uninstall <Name>. The
+value for <Name> is intended to differentiate between different invocations
+of the same service code -- for example for accessing different databases or
+using different configuration files.
 """
 
 from cx_Freeze import setup, Executable
