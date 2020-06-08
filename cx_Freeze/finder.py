7a146cbabf0707b749c61417159e47df4553128b
Marcelo Duarte <marcelotduarte@gmail.com>
2020-06-08 20:33:52 -0300

    detect namespace packages

--- cx_Freeze/finder.py
+++ cx_Freeze/finder.py
@@ -395,6 +395,15 @@ class ModuleFinder(object):
             self._modules[name] = module
             return module
 
+        # detect namespace packages
+        try:
+            spec = importlib.util.find_spec(name)
+        except ValueError:
+            spec = None
+        if spec and spec.origin is None and spec.submodule_search_locations:
+            return self._LoadNamespacePackage(name,
+                            spec.submodule_search_locations[0], parentModule)
+        # other modules or packages
         try:
             fp, path, info = self._FindModule(searchName, path, namespace)
             if info[-1] == imp.C_BUILTIN and parentModule is not None:
@@ -469,6 +478,14 @@ class ModuleFinder(object):
             self._ReplacePackageInCode(module)
 
         module.in_import = False
+        return module
+
+    def _LoadNamespacePackage(self, name, path, parent):
+        """Load the namespace package, given its name and path."""
+        module = self._AddModule(name, path=[path], parent=parent)
+        filename = os.path.join(path, "__init__.py")
+        module.code = compile("", filename, "exec")
+        logging.debug("Adding module [%s] [PKG_NAMESPACE_DIRECTORY]", name)
         return module
 
     def _LoadPackage(self, name, path, parent, deferredImports, namespace):
