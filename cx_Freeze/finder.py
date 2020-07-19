5d63c893dfd872714436466092c3a01937801fca
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2020-07-19 06:29:11 -0300

    fix detect namespaces in py35 (#700)

--- cx_Freeze/finder.py
+++ cx_Freeze/finder.py
@@ -3,7 +3,6 @@ Base class for finding modules.
 """
 
 import dis
-import glob
 import imp
 import importlib.machinery
 import importlib.util
@@ -399,7 +398,7 @@ class ModuleFinder(object):
         # detect namespace packages
         try:
             spec = importlib.util.find_spec(name)
-        except (AttributeError, ModuleNotFoundError, ValueError):
+        except (AttributeError, ImportError, TypeError, ValueError):
             spec = None
         if spec and spec.origin in (None, 'namespace') and \
                 spec.submodule_search_locations:
