04bc8bfd75abd641fb4236659346ffaaf4ff6b0f
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2020-08-03 19:13:45 -0300

    make use of normalize_to_list (#744)

--- cx_Freeze/common.py
+++ cx_Freeze/common.py
@@ -5,19 +5,19 @@ This module contains utility functions shared between cx_Freeze modules.
 import types
 
 
-def normalize_to_list(value):
+def normalize_to_list(value) -> list:
     """
     Takes the different formats of options containing multiple values and
     returns the value as a list object.
     """
     if value is None:
-        normalizedValue = []
+        normalized_value = []
     elif isinstance(value, str):
-        normalizedValue = value.split(',')
+        normalized_value = value.split(",")
     else:
-        normalizedValue = list(value)
+        normalized_value = list(value)
 
-    return normalizedValue
+    return normalized_value
 
 
 def rebuild_code_object(co, code=None, constants=None, filename=None):
@@ -25,11 +25,23 @@ def rebuild_code_object(co, code=None, constants=None, filename=None):
     code = code or co.co_code
     constants = tuple(constants or co.co_consts)
     filename = filename or co.co_filename
-    params = [co.co_argcount, co.co_kwonlyargcount,
-              co.co_nlocals, co.co_stacksize, co.co_flags, code,
-              constants, co.co_names, co.co_varnames, filename,
-              co.co_name, co.co_firstlineno, co.co_lnotab, co.co_freevars,
-              co.co_cellvars]
+    params = [
+        co.co_argcount,
+        co.co_kwonlyargcount,
+        co.co_nlocals,
+        co.co_stacksize,
+        co.co_flags,
+        code,
+        constants,
+        co.co_names,
+        co.co_varnames,
+        filename,
+        co.co_name,
+        co.co_firstlineno,
+        co.co_lnotab,
+        co.co_freevars,
+        co.co_cellvars,
+    ]
     if hasattr(co, "co_posonlyargcount"):
         # PEP570 added "positional only arguments" in Python 3.8
         params.insert(1, co.co_posonlyargcount)
