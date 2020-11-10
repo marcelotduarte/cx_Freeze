39ed5cb9e21c95958e98867d17c4ed47cfb2bd5c
caines <ian.caines@gmail.com>
2020-11-10 14:59:47 -0500

    Updating antiquated code in certain cx_Freeze samples.

--- cx_Freeze/samples/advanced/modules/testfreeze_2.py
+++ cx_Freeze/samples/advanced/modules/testfreeze_2.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
-sys.stdout.write('Test freeze module #2\n')
+print('Test freeze module #2')
--- cx_Freeze/samples/asmodule/asmodule.py
+++ cx_Freeze/samples/asmodule/asmodule.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import datetime
+from datetime import datetime
 import sys
 
 def SayHello():
-    sys.stdout.write('Hello from cx_Freeze\n')
-    sys.stdout.write('The current date is %s\n\n' %
-            datetime.datetime.today().strftime('%B %d, %Y %H:%M:%S'))
+    print('Hello from cx_Freeze')
+    print('The current date is {}\n'.format(
+                 datetime.today().strftime('%B %d, %Y %H:%M:%S')))
 
-    sys.stdout.write('Executable: %r\n' % sys.executable)
-    sys.stdout.write('Prefix: %r\n' % sys.prefix)
-    sys.stdout.write('File system encoding: %r\n\n' % \
-            sys.getfilesystemencoding())
+    print('Executable: {}'.format(sys.executable))
+    print('Prefix: {}'.format(sys.prefix))
+    print('Default encoding: {}'.format(sys.getdefaultencoding()))
+    print('File system encoding: {}\n'.format(sys.getfilesystemencoding()))
 
-    sys.stdout.write('ARGUMENTS:\n')
+    print('ARGUMENTS:')
     for a in sys.argv:
-        sys.stdout.write('%s\n' % a)
-    sys.stdout.write('\n')
+        print('{}'.format(a))
+    print('')
 
-    sys.stdout.write('PATH:\n')
+    print('PATH:')
     for p in sys.path:
-        sys.stdout.write('%s\n' % p)
-    sys.stdout.write('\n')
+        print('{}'.format(p))
+    print()
+    return
+
+
 
 if __name__ == '__main__':
     SayHello()
