94f70686a8f5205a6044649d1ed05cdfe8799548
Marcelo Duarte <marcelotduarte@gmail.com>
2020-12-31 09:17:43 -0300

    micro-optimization at startup

--- cx_Freeze/initscripts/__startup__.py
+++ cx_Freeze/initscripts/__startup__.py
@@ -5,30 +5,36 @@ determines the name of the initscript that is to be executed.
 
 import os
 import sys
-import importlib.machinery
-import importlib.util
+from importlib.machinery import (
+    EXTENSION_SUFFIXES,
+    ExtensionFileLoader,
+    ModuleSpec,
+    PathFinder,
+)
 
 
-class ExtensionFinder(importlib.machinery.PathFinder):
+class ExtensionFinder(PathFinder):
     @classmethod
     def find_spec(cls, fullname, path=None, target=None):
-        """This finder is only for extension modules found within packages that
+        """
+        This finder is only for extension modules found within packages that
         are included in the zip file (instead of as files on disk);
         extension modules cannot be found within zip files but are stored in
         the lib subdirectory; if the extension module is found in a package,
-        however, its name has been altered so this finder is needed."""
+        however, its name has been altered so this finder is needed.
+        """
         if path is None:
-            return
-        suffixes = importlib.machinery.EXTENSION_SUFFIXES
-        loaderClass = importlib.machinery.ExtensionFileLoader
+            return None
+        suffixes = EXTENSION_SUFFIXES
         for entry in sys.path:
             if ".zip" in entry:
                 continue
             for ext in suffixes:
                 location = os.path.join(entry, fullname + ext)
                 if os.path.isfile(location):
-                    loader = loaderClass(fullname, location)
-                    return importlib.util.spec_from_loader(fullname, loader)
+                    loader = ExtensionFileLoader(fullname, location)
+                    return ModuleSpec(fullname, loader, origin=location)
+        return None
 
 
 sys.meta_path.append(ExtensionFinder)
@@ -52,7 +58,7 @@ def run():
                     files.append(k)
         if len(files) != 1:
             raise RuntimeError(
-                "Apparently the original executable has been renamed to "
+                "Apparently, the original executable has been renamed to "
                 f"'{name}'. When multiple executables are generated, "
                 "renaming is not allowed."
             ) from None
