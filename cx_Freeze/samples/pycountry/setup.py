87ff3b24c56621b7613c5d14e8141ef1af2447ce
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2021-03-05 13:23:57 -0300

    Add sample for pycountry (#955)

---
+++ cx_Freeze/samples/pycountry/setup.py
@@ -0,0 +1,17 @@
+"""
+A setup script to demonstrate build using pycountry
+"""
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import setup, Executable
+
+setup(
+    name="test",
+    version="0.1",
+    description="cx_Freeze script to test pycountry",
+    executables=[Executable("test.py")],
+)
