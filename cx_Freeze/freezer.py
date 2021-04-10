marcelotduarte/cx_Freeze

commit ea3153c56b45d3244133b84190d68fa1cc7bd99a

avoid duplicates of libpythonXX.so and pythonXX.ddl (#978)

cx_Freeze/freezer.py
@@ -253,25 +253,19 @@ class Freezer:
         finder.IncludeFile(startupModule)
 
         # Ensure the copy of default python libraries
-        python_shared_libs = tuple(self._GetDefaultBinIncludes())
+        python_libs = tuple(self._GetDefaultBinIncludes())
         dependent_files = set()
         dependent_files.update(self._GetDependentFiles(exe.base))
-        dependent_files.update(self._GetDependentFiles(sys.executable))
-        for name in python_shared_libs:
-            normalized_name = os.path.normcase(name)
-            found = False
-            for dependent_name in list(dependent_files):
-                if os.path.normcase(dependent_name).endswith(normalized_name):
-                    found = True
-                    break
-                source_dir = os.path.dirname(dependent_name)
+        if not dependent_files:
+            dependent_files.update(self._GetDependentFiles(sys.executable))
+        if not dependent_files:
+            for name in python_libs:
+                source_dir = os.path.dirname(exe.base)
                 source = os.path.join(source_dir, name)
                 if os.path.isfile(source):
                     dependent_files.add(source)
-                    found = True
-                    break
-            if not found:
-                print("*** WARNING *** default bin include not found:", name)
+        if not dependent_files:
+            print("*** WARNING *** shared libraries not found:", python_libs)
 
         # Search the C runtimes, using the directory of the python libraries
         # and the directories of the base executable
@@ -289,7 +283,7 @@ class Freezer:
         target_dir = os.path.join(self.targetDir, "lib")
         for source in dependent_files:
             # but python dlls should be copied within the executable
-            if sys.platform == "win32" and source.endswith(python_shared_libs):
+            if sys.platform == "win32" and source.endswith(python_libs):
                 target = os.path.join(self.targetDir, os.path.basename(source))
             else:
                 target = os.path.join(target_dir, os.path.basename(source))

commit 7f5a5d9cea22a8d492add38c8d82fe03c86b11c5

Fix regression introduced in #978 (#1010)

cx_Freeze/freezer.py
@@ -242,17 +242,20 @@ class Freezer:
         )
 
         # Ensure the copy of default python libraries
-        python_libs = tuple(self._GetDefaultBinIncludes())
-        dependent_files = set()
-        dependent_files.update(self._GetDependentFiles(exe.base))
+        dependent_files = set(self._GetDependentFiles(exe.base))
         if not dependent_files:
             dependent_files.update(self._GetDependentFiles(sys.executable))
-        if not dependent_files:
-            for name in python_libs:
-                source_dir = os.path.dirname(exe.base)
-                source = os.path.join(source_dir, name)
+        python_libs = tuple(self._GetDefaultBinIncludes())
+        python_dirs = set([sys.base_exec_prefix, sys.exec_prefix]) # Win
+        python_dirs.add(sysconfig.get_config_var("srcdir")) # Linux
+        for file in dependent_files:
+            python_dirs.add(os.path.dirname(file))
+        for name in python_libs:
+            for python_dir in python_dirs:
+                source = os.path.join(python_dir, name)
                 if os.path.isfile(source):
                     dependent_files.add(source)
+                    break
         if not dependent_files:
             print("*** WARNING *** shared libraries not found:", python_libs)
 