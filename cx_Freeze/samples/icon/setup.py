2aaa5c3e8c085793afacf15a6bca51651231d8de
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2021-05-19 12:09:20 -0300

    Support for icons with non-ascii names [windows] (#1066)

--- cx_Freeze/samples/icon/setup.py
+++ cx_Freeze/samples/icon/setup.py
@@ -1,20 +1,19 @@
 """
-A setup script to create two executables and demonstrate the use a valid and
-an invalid icon.
+A setup script to create executables and demonstrate the use icons, that can be
+valid or invalid icons.
 """
 
 from cx_Freeze import setup, Executable
 
 executables = [
     Executable("test_icon.py", icon="icon.ico", target_name="test_icon"),
-    Executable(
-        "test_icon.py", icon="favicon.png", target_name="test_icon-invalid"
-    ),
+    Executable("test_icon.py", icon="ícone.ico", target_name="teste_ícone"),
+    Executable("test_icon.py", icon="favicon.png", target_name="test_invalid"),
 ]
 
 setup(
     name="Icon sample",
-    version="0.2",
+    version="0.3",
     description="Test Icon",
     executables=executables,
 )
