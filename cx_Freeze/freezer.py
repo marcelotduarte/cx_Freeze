marcelotduarte/cx_Freeze

commit ff98e36200e6315fd29329f118129c1e23ec5288

Only copy a file if should copy a file (#995)
Files marked in the exclusion list were being copied. Files were only excluded when they were detected as a dependency, however, when the entire folder was copied, unnecessary files were copied. Packages like pyqt5 and pyside2 have many duplicate files and were copied and generated a list of dependencies in another directory again.
Now, that is over, and I hope it does not cause regressions (at least it did not in my tests and my projects).

cx_Freeze/freezer.py
@@ -116,6 +116,9 @@ class Freezer:
         includeMode=False,
         machOReference: Optional[MachOReference] = None,
     ):
+        if not self._ShouldCopyFile(source):
+            return
+
         normalizedSource = os.path.normcase(os.path.normpath(source))
         normalizedTarget = os.path.normcase(os.path.normpath(target))
         norm_target_name = os.path.basename(normalizedTarget)
@@ -447,12 +450,6 @@ class Freezer:
                         dependentFile = dependentFile[:pos].strip()
                     if dependentFile:
                         dependentFiles.append(dependentFile)
-
-            dependentFiles = [
-                os.path.normcase(f)
-                for f in dependentFiles
-                if self._ShouldCopyFile(f)
-            ]
             self.dependentFiles[path] = dependentFiles
         return dependentFiles
 
@@ -565,11 +562,12 @@ class Freezer:
         self.binExcludes = [os.path.normcase(name) for name in filenames]
 
         paths = list(self.binPathIncludes or [])
+        paths += [path for path in self.path if os.path.isdir(path)]
         self.binPathIncludes = [os.path.normcase(name) for name in paths]
 
         paths = list(self.binPathExcludes or [])
         paths += self._GetDefaultBinPathExcludes()
-        self.binPathExcludes = [os.path.normcase(n) for n in paths]
+        self.binPathExcludes = [os.path.normcase(name) for name in paths]
 
         # control runtime files
         self.runtime_files = set()

commit a30a4da1284924a600446ba9e653a6020b7d2363

fix regression introduced in #995 (and #985)

cx_Freeze/freezer.py
@@ -84,7 +84,7 @@ class Freezer(ABC):
         self.replacePaths = list(replacePaths or [])
         self.compress = True if compress is None else compress
         self.optimize_flag = optimizeFlag
-        self.path = sys.path if path is None else path
+        self.path = path
         self.include_msvcr = includeMSVCR
         self.targetdir = targetDir
         self.binIncludes = binIncludes
@@ -378,12 +378,15 @@ class Freezer(ABC):
         self.binExcludes = [os.path.normcase(name) for name in filenames]
 
         paths = list(self.binPathIncludes or [])
-        paths += [path for path in self.path if os.path.isdir(path)]
-        self.binPathIncludes = [os.path.normcase(name) for name in paths]
+        self.binPathIncludes = [
+            os.path.normcase(name) for name in paths if os.path.isdir(name)
+        ]
 
         paths = list(self.binPathExcludes or [])
         paths += self._default_bin_path_excludes()
-        self.binPathExcludes = [os.path.normcase(name) for name in paths]
+        self.binPathExcludes = [
+            os.path.normcase(name) for name in paths if os.path.isdir(name)
+        ]
 
         for source, target in self.includeFiles + self.zipIncludes:
             if not os.path.exists(source):