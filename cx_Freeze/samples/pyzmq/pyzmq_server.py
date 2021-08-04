cdf2ed70546b99ac570fe174f4507766fe177419
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2021-08-04 03:41:43 -0300

    Improve sample to support pyzmq < 20 and timeout (#1190)

--- cx_Freeze/samples/pyzmq/pyzmq_server.py
+++ cx_Freeze/samples/pyzmq/pyzmq_server.py
@@ -1,6 +1,23 @@
 import time
 import sys
 import zmq
+from contextlib import AbstractContextManager
+
+
+class ignore(AbstractContextManager):
+    def __init__(self, call):
+        self.call = call
+
+    def __enter__(self):
+        if hasattr(self.call, "__enter__"):
+            return self.call
+        return self
+
+    def __exit__(self, *exc_info):
+        if hasattr(self.call, "__exit__"):
+            return self.call.__exit__
+        return None
+
 
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
@@ -12,16 +29,20 @@ reply = "Pong"
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind(url)
 
-
+print(f"Server listening at {url}")
 try:
-    print(f"Server listening at {url}")
-    while True:
-        message = socket.recv_string()
-        print(f"Server received: {message}")
-        time.sleep(1)
-        print(f"Server sending: {reply}")
-        socket.send_string(reply)
+    with ignore(socket.bind(url)):
+        while True:
+            message = socket.recv_string()
+            print(f"Server received: {message}")
+            if message == "Close":
+                reply = "Closing"
+            else:
+                time.sleep(1)
+            print(f"Server sending: {reply}")
+            socket.send_string(reply)
+            if message == "Close":
+                break
 except KeyboardInterrupt:
     sys.exit(0)
