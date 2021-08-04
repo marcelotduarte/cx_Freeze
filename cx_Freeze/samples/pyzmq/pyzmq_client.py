cd5a6b2fda0915a0ee998a9e12daeeb1dea89a1b
Marcelo Duarte <marcelotduarte@gmail.com>
2021-08-04 03:41:14 -0300

    Improve sample to support pyzmq < 20 and timeout

--- cx_Freeze/samples/pyzmq/pyzmq_client.py
+++ cx_Freeze/samples/pyzmq/pyzmq_client.py
@@ -1,10 +1,33 @@
+import time
 import sys
 import zmq
+from contextlib import AbstractContextManager
 
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-else:
-    port = 5556
+
+class ignore(AbstractContextManager):
+    def __init__(self, call):
+        self.call = call
+
+    def __enter__(self):
+        if hasattr(self.call, "__enter__"):
+            return self.call
+        return self
+
+    def __exit__(self, *exc_info):
+        if hasattr(self.call, "__exit__"):
+            return self.call.__exit__
+        return None
+
+
+port = 5556
+timeout = None
+for arg in sys.argv[1:]:
+    if arg.startswith("--timeout="):
+        timeout = int(arg[len("--timeout=") :])
+    if arg.startswith("--port="):
+        timeout = int(arg[len("--port=") :])
+    elif arg.isdecimal():
+        port = int(arg)
 
 url = f"tcp://localhost:{port}"
 request = "Ping"
@@ -12,13 +35,21 @@ request = "Ping"
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 
+if timeout:
+    timeout += time.monotonic()
+
+
+print(f"Client connecting to {url}")
 try:
-    print(f"Client connecting to {url}")
-    with socket.connect(url):
+    with ignore(socket.connect(url)):
         while True:
+            if timeout and time.monotonic() >= timeout:
+                request = "Close"
             print(f"Client sending: {request}")
             socket.send_string(request)
             reply = socket.recv_string()
             print(f"Client received: {reply}")
+            if request == "Close":
+                break
 except KeyboardInterrupt:
     sys.exit(0)
