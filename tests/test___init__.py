e3833d5437379834894ece1bbe87bb0c491656fe
Joe Deuchar <deucharj@gmail.com>
2021-10-06 12:03:05 +0200

    Fix failing test + nuke uneeded file + gitignore update

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,4 +1,5 @@
 import sys
+import types
 import importlib
 import pytest
 import cx_Freeze
@@ -14,12 +15,18 @@ import cx_Freeze
 )
 def test_exposed_namespaces(mocker, platform, extra_modules):
     """ This test asserts that all the namespaces that should be exposed when `importing cx_Freeze` are available """
+    # Mock platform specific modules so we don't get import errors due modules not being available on test device
+    platform_specific_modules = ["msilib", ]
+    for mod_name in platform_specific_modules:
+        fake_mod = types.ModuleType(mod_name)
+        sys.modules[mod_name] = fake_mod
+
     if platform:  # Mock platform before import :)
         mocker.patch.object(sys, "platform", platform)
 
     importlib.reload(cx_Freeze)
 
-    expected_namespaces = [  # This namespaces are there regardless of platform
+    expected_namespaces = [  # These namespaces are there regardless of platform
         "bdist_rpm",
         "build",
         "build_exe",
