783509e0047ed04f1bdc1050fc316b7b3dfb8e7f
Marcelo Duarte <marcelotduarte@gmail.com>
2021-11-27 03:13:58 -0300

    fade to black

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,11 @@ import os
 
 @pytest.fixture()
 def fix_test_dir():
-    """ This fixture returns the root of the test folder """
+    """This fixture returns the root of the test folder"""
     return os.path.dirname(__file__)
 
 
 @pytest.fixture()
 def fix_test_samples_dir(fix_test_dir):
-    """ This fixture returns the samples folder for the tests """
+    """This fixture returns the samples folder for the tests"""
     return os.path.join(fix_test_dir, "samples")
