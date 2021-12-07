4b9f25050ce7acb03d160fffb2bbc3b2d9f090dd
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2021-12-07 04:31:58 -0300

    Update PyQt5 sample (#1307)

--- cx_Freeze/samples/pyqt5/setup_zip.py
+++ cx_Freeze/samples/pyqt5/setup_zip.py
@@ -1,31 +1,64 @@
-# A simple setup script to create an executable using PyQt5. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# PyQt5app.py is a very simple type of PyQt5 application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
+"""
+A simple setup script to create an executable using PyQt5. This also
+demonstrates the method for creating a Windows executable that does not have
+an associated console.
+
+PyQt5app.py is a very simple type of PyQt5 application
+
+Run the build process by running the command 'python setup_zip.py build'
+
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application
+"""
 
 import sys
 from cx_Freeze import setup, Executable
 
+try:
+    from cx_Freeze.hooks import get_qt_plugins_paths
+except ImportError:
+    include_files = []
+else:
+    # Inclusion of extra plugins (new in cx_Freeze 6.8b2)
+    # cx_Freeze imports automatically the following plugins depending of the
+    # use of some modules:
+    # imageformats - QtGui
+    # platforms - QtGui
+    # mediaservice - QtMultimedia
+    # printsupport - QtPrintSupport
+    #
+    # So, "platforms" is used here for demonstration purposes.
+    include_files = get_qt_plugins_paths("PyQt5", "platforms")
+
+# base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-options = {
-    "build_exe": {"includes": "atexit", "zip_include_packages": ["PyQt5"]}
+build_exe_options = {
+    "excludes": ["tkinter"],
+    "include_files": include_files,
+    "zip_include_packages": ["PyQt5"],
 }
 
-executables = [Executable("PyQt5app.py", base=base)]
+bdist_mac_options = {
+    "bundle_name": "Test",
+}
+
+bdist_dmg_options = {
+    "volume_label": "TEST",
+}
+
+executables = [Executable("PyQt5app.py", base=base, target_name="test_pyqt5")]
 
 setup(
     name="simple_PyQt5",
-    version="0.1",
+    version="0.3",
     description="Sample cx_Freeze PyQt5 script",
-    options=options,
+    options={
+        "build_exe": build_exe_options,
+        "bdist_mac": bdist_mac_options,
+        "bdist_dmg": bdist_dmg_options,
+    },
     executables=executables,
 )
