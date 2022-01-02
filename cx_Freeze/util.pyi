c4b6bb7986132aaac908d038e9df0fe3b4523d2a
Marcelo Duarte <marcelotduarte@gmail.com>
2022-01-02 17:01:11 -0300

    Support Path in BeginUpdateResource and fix UpdateResource

--- cx_Freeze/util.pyi
+++ cx_Freeze/util.pyi
@@ -1,10 +1,28 @@
 from pathlib import Path
-from typing import List, Union
+from typing import List, Optional, Union
+
+HANDLE = Optional[int]
 
 class BindError(Exception):
     ...
 
 def AddIcon(target_path: Union[str, Path], exe_icon: Union[str, Path]):
+    ...
+
+def BeginUpdateResource(
+    path: Union[str, Path], delete_existing_resources: bool = True
+) -> HANDLE:
+    """Wrapper for BeginUpdateResource()."""
+    ...
+
+def UpdateResource(
+    handle: HANDLE, resource_type: int, resource_id: int, resource_data: bytes
+) -> None:
+    """Wrapper for UpdateResource()."""
+    ...
+
+def EndUpdateResource(handle: HANDLE, discard_changes: bool):
+    """Wrapper for EndUpdateResource()."""
     ...
 
 def UpdateCheckSum(target_path: Union[str, Path]):
