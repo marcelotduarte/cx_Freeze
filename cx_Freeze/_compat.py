ed517adb84f4919c1b2ec5a4443e5c322bcc911b
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-01-17 04:30:52 -0300

    Prioritize importlib_metadata in versions lower than 3.10 (#1353)

--- cx_Freeze/_compat.py
+++ cx_Freeze/_compat.py
@@ -3,6 +3,9 @@ import sys
 if sys.version_info >= (3, 10):
     import importlib.metadata as importlib_metadata
 else:
-    import importlib_metadata
+    try:
+        import importlib_metadata
+    except ImportError:
+        import importlib.metadata as importlib_metadata
 
 __all__ = ["importlib_metadata"]
