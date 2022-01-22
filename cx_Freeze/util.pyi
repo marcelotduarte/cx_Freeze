08ba0d78fc8530b70b4dfd7c8b71fa6c3e4897e8
Marcelo Duarte <marcelotduarte@gmail.com>
2022-01-22 02:02:00 -0300

    Add a basic pyproject.toml for build and tools

--- cx_Freeze/util.pyi
+++ cx_Freeze/util.pyi
@@ -4,9 +4,12 @@ from typing import List, Optional, Union
 HANDLE = Optional[int]
 
 class BindError(Exception):
+    """BindError Exception."""
+
     ...
 
 def AddIcon(target_path: Union[str, Path], exe_icon: Union[str, Path]):
+    """Add the icon as a resource to the specified file."""
     ...
 
 def BeginUpdateResource(
@@ -26,13 +29,17 @@ def EndUpdateResource(handle: HANDLE, discard_changes: bool):
     ...
 
 def UpdateCheckSum(target_path: Union[str, Path]):
+    """Update the CheckSum into the specified executable."""
     ...
 
 def GetSystemDir() -> str:
+    """Return the Windows system directory (C:\Windows\system for example)."""
     ...
 
 def GetWindowsDir() -> str:
+    """Return the Windows directory (C:\Windows for example)."""
     ...
 
 def GetDependentFiles(path: Union[str, Path]) -> List[str]:
+    """Return a list of files that this file depends on."""
     ...
