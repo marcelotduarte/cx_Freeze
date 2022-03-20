1714cf478e0edee8744f0d915ff9c89da2de3f73
Marcelo Duarte <marcelotduarte@gmail.com>
2022-03-20 02:26:08 -0300

    Add cached_property (and a compatible function) for planned use

--- cx_Freeze/_compat.py
+++ cx_Freeze/_compat.py
@@ -1,6 +1,8 @@
 """Internal compatible module."""
 import sys
 
+__all__ = ["cached_property", "importlib_metadata"]
+
 if sys.version_info >= (3, 10):
     import importlib.metadata as importlib_metadata
 else:
@@ -9,4 +11,23 @@ else:
     except ImportError:
         import importlib.metadata as importlib_metadata
 
-__all__ = ["importlib_metadata"]
+
+try:
+    from functools import cached_property
+except ImportError:
+
+    class cached_property(object):
+        """Transform a method of a class into a property whose value is
+        computed once and then cached as a normal attribute for the life of
+        the instance."""
+
+        def __init__(self, func):
+            self.func = func
+            self.__doc__ = func.__doc__
+
+        def __get__(self, instance, owner=None):
+            if instance is None:
+                return self
+
+            value = instance.__dict__[self.func.__name__] = self.func(instance)
+            return value
