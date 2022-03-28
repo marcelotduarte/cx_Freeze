0aecdcdc2c41dd912cc02a05cd406256ead4550f
Marcelo Duarte <marcelotduarte@gmail.com>
2022-03-28 20:06:11 -0300

    Enable flake8 in tests

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -7,7 +7,6 @@ from cx_Freeze.exception import ConfigError, DarwinException
 def test_raise_exceptions(custom_exception):
     """This method tests that exceptions can be raised + caught and the __str__
     value is correctly called"""
-    # TODO : Discuss with maintainer, `ConfigError` dosen't seem like it needs a custom implementation?
     try:
         raise custom_exception("Something Bad Happened")
     except custom_exception as err:
