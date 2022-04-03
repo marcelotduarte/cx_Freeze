5eead7ac82eaa616b79c0b53e96e04552e98ddf9
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-04-03 01:43:56 -0300

    Add PySide6 sample (#1442)

---
+++ cx_Freeze/samples/pyside6/test_pyside6.py
@@ -0,0 +1,23 @@
+"""A simple script to demonstrate PySide6."""
+
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QWidget
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("Simple")
+    window.setGeometry(300, 300, 300, 300)
+    label = QLabel(window)
+    label.setText("Hello World!")
+    label.setGeometry(0, 0, 300, 300)
+    label.setAlignment(Qt.AlignCenter)
+    window.show()
+    return app.exec()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
