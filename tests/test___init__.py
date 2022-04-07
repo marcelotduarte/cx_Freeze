da268243d211dc4894d19c6df9301045c7544a49
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-04-06 21:16:35 -0300

    Tests: enable a test by platform (#1463)

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,5 +1,5 @@
-# pylint: disable=C0114,C0115,C0116
-import importlib
+"""Test 'import cx_Freeze'"""
+
 import sys
 
 import pytest
@@ -7,35 +7,21 @@ import pytest
 import cx_Freeze
 
 
-@pytest.mark.skip(
-    reason="needs updating to handle/mock platform specific imports"
-)
 @pytest.mark.parametrize(
     "platform, extra_modules",
     [
         (None, []),
-        ("win32", ["bdist_msi"]),
-        ("darwin", ["bdist_dmg", "bdist_mac"]),
-        ("linux", []),
+        ("win32", ["BdistMSI"]),
+        ("darwin", ["BdistDMG", "BdistMac"]),
+        ("linux", ["BdistRPM"]),
     ],
 )
-def test_exposed_namespaces(mocker, platform, extra_modules):
+def test_exposed_namespaces(platform, extra_modules):
     """This test asserts that all the namespaces that should be exposed when
     `importing cx_Freeze` are available"""
 
-    if platform:  # Mock platform before import :)
-        mocker.patch.object(sys, "platform", platform)
-
-    try:
-        importlib.reload(cx_Freeze)
-    except ImportError:
-        # Pass import errors as some modules are platform specific, we're
-        # testing what's exposed in `__all__` here
-        pass
-
     # These namespaces are there regardless of platform
     expected_namespaces = [
-        "bdist_rpm",
         "build",
         "build_exe",
         "install",
@@ -51,6 +37,6 @@ def test_exposed_namespaces(mocker, platform, extra_modules):
     for ens in expected_namespaces:
         assert ens in dir(cx_Freeze)
 
-    if platform:
+    if platform == sys.platform:
         for ens in extra_modules:
             assert ens in dir(cx_Freeze)
