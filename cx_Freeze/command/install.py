968991799b3c67f6ec358bb4b1ad7876ee0b56f1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-04-08 01:36:17 -0300

    install: suppress known deprecation (#1467)

--- cx_Freeze/command/install.py
+++ cx_Freeze/command/install.py
@@ -1,34 +1,42 @@
 """Extends setuptools 'install' command."""
 
+import contextlib
 import sys
+import warnings
 
-import setuptools.command.install
+from setuptools.command.install import install as _install
 
 __all__ = ["Install"]
 
 
+@contextlib.contextmanager
+def suppress_known_deprecation():  # pylint: disable=missing-function-docstring
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", "setup.py install is deprecated")
+        yield
+
+
 # pylint: disable=attribute-defined-outside-init
-class Install(setuptools.command.install.install):
+class Install(_install):
     """Install everything from build directory."""
 
-    user_options = setuptools.command.install.install.user_options + [
+    user_options = _install.user_options + [
         ("install-exe=", None, "installation directory for executables")
     ]
 
     def expand_dirs(self):
-        setuptools.command.install.install.expand_dirs(self)
+        _install.expand_dirs(self)
         self._expand_attrs(["install_exe"])
 
     def get_sub_commands(self):
-        sub_commands = setuptools.command.install.install.get_sub_commands(
-            self
-        )
+        sub_commands = _install.get_sub_commands(self)
         if self.distribution.executables:
             sub_commands.append("install_exe")
         return [s for s in sub_commands if s != "install_egg_info"]
 
     def initialize_options(self):
-        setuptools.command.install.install.initialize_options(self)
+        with suppress_known_deprecation():
+            _install.initialize_options(self)
         self.install_exe = None
 
     def finalize_options(self):
@@ -41,13 +49,13 @@ class Install(setuptools.command.install.install):
             prefix = str(winreg.QueryValueEx(key, "ProgramFilesDir")[0])
             metadata = self.distribution.metadata
             self.prefix = f"{prefix}/{metadata.name}"
-        setuptools.command.install.install.finalize_options(self)
+        _install.finalize_options(self)
         self.convert_paths("exe")
         if self.root is not None:
             self.change_roots("exe")
 
     def select_scheme(self, name):
-        setuptools.command.install.install.select_scheme(self, name)
+        _install.select_scheme(self, name)
         if self.install_exe is None:
             if sys.platform == "win32":
                 self.install_exe = "$base"
