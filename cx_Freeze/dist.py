86c34dc94740d6718ef4c2fc666131412e8a5e90
Marcelo Duarte <marcelotduarte@gmail.com>
2022-04-13 16:53:52 -0300

    Using a trick to get around a dependency on distutils.

--- cx_Freeze/dist.py
+++ cx_Freeze/dist.py
@@ -1,10 +1,13 @@
 """The classes and functions with which cx_Freeze extends setuptools."""
 
-from distutils.dist import DistributionMetadata  # pylint: disable=W0402
-
 from setuptools import Distribution as _Distribution
 
 __all__ = ["Distribution", "DistributionMetadata"]
+
+
+class DistributionMetadata:
+    """Dummy class to hold the distribution meta-data: name, version,
+    author, and so forth."""
 
 
 class Distribution(_Distribution):
@@ -13,6 +16,16 @@ class Distribution(_Distribution):
     def __init__(self, attrs):
         self.executables = []
         super().__init__(attrs)
+        self.metadata = self._patch_metadata()
+
+    def _patch_metadata(self) -> DistributionMetadata:
+        old_metadata = self.metadata
+        new_metadata = DistributionMetadata()
+        for var in dir(old_metadata):
+            if not hasattr(new_metadata, var):
+                setattr(new_metadata, var, getattr(old_metadata, var))
+        self.distutils_metadata = old_metadata
+        return new_metadata
 
     def has_executables(self):
         """Predicate for build_exe command."""
