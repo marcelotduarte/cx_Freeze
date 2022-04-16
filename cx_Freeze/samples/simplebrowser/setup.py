8f3d3e25fd329c492484ae59127bbbc281fb0355
Marcelo Duarte <marcelotduarte@gmail.com>
2022-04-16 16:31:46 -0300

    Use pyside2 example simplebrowser as sample

---
+++ cx_Freeze/samples/simplebrowser/setup.py
@@ -0,0 +1,46 @@
+"""
+A simple setup script to create an executable using PySide2 WebEngineWidgets.
+This also demonstrates how to use excludes to get minimal package size.
+
+simplebrowser.py is the "PySide2 WebEngineWidgets Example".
+
+Run the build process by running the command 'python setup.py build'
+
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application
+"""
+
+import sys
+
+from cx_Freeze import Executable, setup
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+options = {
+    "build_exe": {
+        # exclude packages that are not really needed
+        "excludes": [
+            "tkinter",
+            "unittest",
+            "email",
+            "http",
+            "xml",
+            "pydoc",
+        ],
+        "zip_include_packages": ["PySide2"],
+    }
+}
+
+executables = [
+    Executable("simplebrowser.py", target_name="test_simplebrowser")
+]
+
+setup(
+    name="simplebrowser",
+    version="0.1",
+    description="Sample cx_Freeze PySide2 simplebrowser script",
+    options=options,
+    executables=executables,
+)
