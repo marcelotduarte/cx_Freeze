04caddba702d1fa79ef4fd30ce8a64695d46c5a4
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-06-28 11:58:01 -0300

    New opencv-python sample (#1535)

---
+++ cx_Freeze/samples/opencv/setup.py
@@ -0,0 +1,42 @@
+"""
+A simple setup script to create an executable using opencv-python. This also
+demonstrates how to use excludes to get minimal package size.
+
+test_opencv.py is a very simple type of opencv-python application.
+
+Run the build process by running the command 'python setup.py build'
+
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application.
+"""
+
+import sys
+
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+options = {
+    "build_exe": {
+        # exclude packages that are not really needed
+        "excludes": [
+            "tkinter",
+            "unittest",
+            "email",
+            "http",
+            "xml",
+            "pydoc",
+        ],
+        "include_files": ["image.png"],
+    }
+}
+
+executables = [Executable("test_opencv.py", base=base)]
+
+setup(
+    name="simple_opencv",
+    version="0.1",
+    description="Sample cx_Freeze opencv-python script",
+    options=options,
+    executables=executables,
+)
---
+++ cx_Freeze/samples/opencv/test_opencv.py
@@ -0,0 +1,27 @@
+"""A simple script to demonstrate opencv-python."""
+
+import os
+import sys
+
+sys.OpenCV_LOADER_DEBUG = True
+import cv2 as cv  # noqa
+
+
+def find_data_file(filename):
+    if getattr(sys, "frozen", False):
+        # The application is frozen
+        datadir = os.path.dirname(sys.executable)
+    else:
+        # The application is not frozen
+        # Change this bit to match where you store your data files:
+        datadir = os.path.dirname(__file__)
+    return os.path.join(datadir, filename)
+
+
+img = cv.imread(find_data_file("image.png"))
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(15000)
+if k == ord("s"):
+    cv.imwrite("image1.png", img)
