eb23dbe22b20d842465478aa79164f36414b5c87
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-07-19 00:17:39 -0300

    Rewrite pyqt hooks to query Qt Library paths instead of guessing (#1555)

--- cx_Freeze/hooks/pyqt5.py
+++ cx_Freeze/hooks/pyqt5.py
@@ -2,12 +2,14 @@
 PyQt5 package is included."""
 
 import sys
+from functools import lru_cache
 from pathlib import Path
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 from ..finder import ModuleFinder
 from ..module import Module
 
+DARWIN = sys.platform == "darwin"
 WIN32 = sys.platform == "win32"
 
 
@@ -16,91 +18,120 @@ def _qt_implementation(module: Module) -> str:
     return module.name.split(".")[0]
 
 
-def _qt_library_paths(name: str) -> List[str]:
+@lru_cache(maxsize=None)
+def _qt_libraryinfo_paths(name: str) -> Dict[str, Tuple[Path, Path]]:
     """Cache the QtCore library paths."""
-    if _qt_library_paths.data:
-        return _qt_library_paths.data
+
     try:
         qtcore = __import__(name, fromlist=["QtCore"]).QtCore
     except RuntimeError:
         print("WARNING: Tried to load multiple incompatible Qt ", end="")
         print("wrappers. Some incorrect files may be copied.")
-        qtcore = None
+        return {}
+
+    # get paths from QLibraryInfo
+    source_paths: Dict[str, Path] = {}
+    lib = qtcore.QLibraryInfo
+    major_version = lib.version().majorVersion()
+    if major_version == 6:
+        for key, value in lib.__dict__.items():
+            if isinstance(value, lib.LibraryPath):
+                source_paths[key] = Path(lib.path(value))
     else:
-        data = [Path(p) for p in qtcore.QCoreApplication.libraryPaths()]
-    if not data:
-        # check the common location for conda
-        plugins_path = Path(sys.base_prefix, "Library", "plugins")
-        if plugins_path.exists():
-            data.append(plugins_path)
-        elif qtcore:
-            # use a hack
-            app = qtcore.QCoreApplication([])
-            data = [Path(p) for p in app.libraryPaths()]
-    if not data and qtcore:
+        for key, value in lib.__dict__.items():
+            if isinstance(value, (lib.LibraryLocation, int)):
+                source_paths[key] = Path(lib.location(value))
+    qt_root_dir = Path(qtcore.__file__).parent
+
+    # if QLibraryInfo has incomplete information
+    if not source_paths.get("PluginsPath"):
         # Qt Plugins can be in a plugins directory next to the Qt libraries
-        qt_root_dir = Path(qtcore.__file__).parent
-        data.append(qt_root_dir / "plugins")
-        data.append(qt_root_dir / "Qt5" / "plugins")
-        data.append(qt_root_dir / "Qt" / "plugins")
-    _qt_library_paths.data = data
+        plugins_path = qt_root_dir / "plugins"
+        if not plugins_path.exists():
+            plugins_path = qt_root_dir / "Qt5" / "plugins"  # PyQt5 5.15.4
+        # or in a special location in conda-forge
+        if not plugins_path.exists():
+            plugins_path = Path(sys.base_prefix, "Library", "plugins")
+        # default location
+        if not plugins_path.exists():
+            plugins_path = qt_root_dir / "Qt" / "plugins"
+        source_paths["PluginsPath"] = plugins_path
+    source_paths.setdefault("PrefixPath", source_paths["PluginsPath"].parent)
+    prefix_path = source_paths["PrefixPath"]
+    source_paths.setdefault("DataPath", prefix_path)
+    source_paths.setdefault("LibrariesPath", prefix_path / "lib")
+
+    # set the target paths
+    data: Dict[str, Tuple[Path, Path]] = {}
+    add_qt_subdir = None
+    if name == "PyQt5" and prefix_path.name != "Qt5":
+        # conda pyqt
+        add_qt_subdir = "Qt5"
+    elif name == "PySide2" and prefix_path.name != "Qt":
+        # conda pyside2, pyside2 windows
+        add_qt_subdir = "Qt"
+    for key, source in source_paths.items():
+        if key == "SettingsPath":
+            if DARWIN:
+                target = Path("Contents/Resources")
+            else:
+                target = Path(".")
+        elif source == Path("."):
+            print(".......", key, source)
+            target = source
+        else:
+            target = Path("lib", name)
+            if add_qt_subdir:
+                target = target / add_qt_subdir
+            try:
+                target = target / source.relative_to(qt_root_dir)
+            except ValueError:
+                # msys2 and conda-forge
+                target = target / source.relative_to(prefix_path)
+        data[key] = source, target
+
+    # debug
+    print("QLibraryInfo:")
+    for key, (source, target) in data.items():
+        print(" ", key, "\n   ", source, "->", target)
     return data
 
 
-_qt_library_paths.data: List[Path] = []
-
-
-def get_qt_subdir_paths(name: str, subdir: str) -> List[Tuple[Path, Path]]:
-    """Helper function to get a list of source and target paths of Qt
-    subdirectories, indicated to be used in include_files."""
-    include_files = []
-    for library_dir in _qt_library_paths(name):
-        if library_dir.parts[-1] == "plugins":
-            library_dir = library_dir.parent
-        source_path = library_dir / subdir
-        if not source_path.exists():
-            continue
-        if library_dir.parts[-1] == name:  # {name}/{subdir}
-            target_path = Path("lib") / name / subdir
-        elif library_dir.parts[-2] == name:  # {name}/Qt*/{subdir}
-            target_path = Path("lib") / name / library_dir.parts[-1] / subdir
-        else:
-            target_path = Path("lib") / name / "Qt" / subdir
-        include_files.append((source_path, target_path))
-    return include_files
-
-
-def get_qt_plugins_paths(name: str, plugins: str) -> List[Tuple[str, str]]:
+def get_qt_plugins_paths(name: str, plugins: str) -> List[Tuple[Path, Path]]:
     """Helper function to get a list of source and target paths of Qt plugins,
     indicated to be used in include_files."""
-    return get_qt_subdir_paths(name, str(Path("plugins", plugins)))
+    libraryinfo_paths = _qt_libraryinfo_paths(name)
+    source_path, target_path = libraryinfo_paths["PluginsPath"]
+    source_path = source_path / plugins
+    if not source_path.exists():
+        return []
+    return [(source_path, target_path / plugins)]
 
 
-def copy_qt_data(name: str, subdir: str, finder: ModuleFinder) -> None:
-    """Helper function to find and copy Qt resources, translations, etc."""
-    for source_path, target_path in get_qt_subdir_paths(name, subdir):
-        finder.include_files(source_path, target_path)
-
-
-def copy_qt_plugins(name: str, plugins: str, finder: ModuleFinder) -> None:
-    """Helper function to find and copy Qt plugins."""
-    for source_path, target_path in get_qt_plugins_paths(name, plugins):
-        finder.include_files(source_path, target_path)
+def copy_qt_files(finder: ModuleFinder, name: str, *args) -> None:
+    """Helper function to find and copy Qt plugins, resources, translations,
+    etc."""
+    variable = args[0]
+    libraryinfo_paths = _qt_libraryinfo_paths(name)
+    source_path, target_path = libraryinfo_paths[variable]
+    for i in range(1, len(args)):
+        source_path = source_path / args[i]
+        target_path = target_path / args[i]
+    if not source_path.exists():
+        return
+    finder.include_files(source_path, target_path)
 
 
 def load_pyqt5(finder: ModuleFinder, module: Module) -> None:
-    """Inject code in PyQt5/PySide2 init to locate and load plugins."""
+    """Inject code in PyQt5 __init__ to locate and load plugins and resources.
+    Also, this fixes issues with conda-forge versions."""
     if module.code is None:
         return
     # With PyQt5 5.15.4, if the folder name contains non-ascii characters, the
     # libraryPaths returns empty. Prior to this version, this doesn't happen.
-    # With PySide2 the same happens in some versions.
-    # So, this hack will be used to:
-    # - fix empty libraryPaths
-    # - workaround issues with anaconda
-    # - locate plugins when using zip_include_packages
     name = _qt_implementation(module)
     code_string = module.file.read_text()
+
     code_string += f"""
 # cx_Freeze patch start
 import os
@@ -131,9 +162,11 @@ if os.environ.get("QT_DEBUG") or os.environ.get("QT_DEBUG_PLUGINS"):
         for key, value in QLibraryInfo.__dict__.items():
             if isinstance(value, (QLibraryInfo.LibraryLocation, int)):
                 data[key] = Path(QLibraryInfo.location(value))
-    print("QLibraryInfo:")
+    print("QLibraryInfo:", file=sys.stderr)
     for key, value in data.items():
-        print(" ", key, value)
+        print(" ", key, value, file=sys.stderr)
+    print("LibraryPaths:", file=sys.stderr)
+    print(" ", QCoreApplication.libraryPaths(), file=sys.stderr)
 # cx_Freeze patch end
 """
     module.code = compile(code_string, str(module.file), "exec")
@@ -147,7 +180,7 @@ def load_pyqt5_phonon(finder: ModuleFinder, module: Module) -> None:
     be present in the build directory inside a folder phonon_backend."""
     if WIN32:
         name = _qt_implementation(module)
-        copy_qt_plugins(name, "phonon_backend", finder)
+        copy_qt_files(finder, name, "PluginsPath", "phonon_backend")
 
 
 def load_pyqt5_qt(finder: ModuleFinder, module: Module) -> None:
@@ -209,18 +242,19 @@ def load_pyqt5_qtdesigner(finder: ModuleFinder, module: Module) -> None:
     name = _qt_implementation(module)
     finder.include_module("datetime")
     finder.include_module(f"{name}.QtWidgets")
-    copy_qt_plugins(name, "designer", finder)
+    copy_qt_files(finder, name, "PluginsPath", "designer")
 
 
 def load_pyqt5_qtgui(finder: ModuleFinder, module: Module) -> None:
     """There is a chance that QtGui will use some image formats, then, add the
     image format plugins."""
     name = _qt_implementation(module)
-    copy_qt_plugins(name, "imageformats", finder)
+    copy_qt_files(finder, name, "PluginsPath", "imageformats")
     # On Qt5, we need the platform plugins. For simplicity, we just copy
     # any that are installed.
-    copy_qt_plugins(name, "platforms", finder)
-    copy_qt_plugins(name, "styles", finder)
+    copy_qt_files(finder, name, "PluginsPath", "platforms")
+    copy_qt_files(finder, name, "PluginsPath", "platformthemes")
+    copy_qt_files(finder, name, "PluginsPath", "styles")
 
 
 def load_pyqt5_qthelp(finder: ModuleFinder, module: Module) -> None:
@@ -240,8 +274,8 @@ def load_pyqt5_qtmultimedia(finder: ModuleFinder, module: Module) -> None:
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtNetwork")
     finder.include_module(f"{name}.QtMultimediaWidgets")
-    copy_qt_plugins(name, "audio", finder)
-    copy_qt_plugins(name, "mediaservice", finder)
+    copy_qt_files(finder, name, "PluginsPath", "audio")
+    copy_qt_files(finder, name, "PluginsPath", "mediaservice")
 
 
 def load_pyqt5_qtmultimediawidgets(
@@ -257,27 +291,27 @@ def load_pyqt5_qtopengl(finder: ModuleFinder, module: Module) -> None:
     """This module depends on another module and plugins."""
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtWidgets")
-    copy_qt_plugins(name, "renderers", finder)
+    copy_qt_files(finder, name, "PluginsPath", "renderers")
 
 
 def load_pyqt5_qtpositioning(finder: ModuleFinder, module: Module) -> None:
     """This module depends on another module."""
     name = _qt_implementation(module)
-    copy_qt_plugins(name, "position", finder)
+    copy_qt_files(finder, name, "PluginsPath", "position")
 
 
 def load_pyqt5_qtprintsupport(finder: ModuleFinder, module: Module) -> None:
     """This module depends on another module and plugins."""
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtWidgets")
-    copy_qt_plugins(name, "printsupport", finder)
+    copy_qt_files(finder, name, "PluginsPath", "printsupport")
 
 
 def load_pyqt5_qtqml(finder: ModuleFinder, module: Module) -> None:
     """This module depends on another module and plugins."""
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtNetwork")
-    copy_qt_plugins(name, "qmltooling", finder)
+    copy_qt_files(finder, name, "PluginsPath", "qmltooling")
 
 
 def load_pyqt5_qtscripttools(finder: ModuleFinder, module: Module) -> None:
@@ -291,7 +325,7 @@ def load_pyqt5_qtsql(finder: ModuleFinder, module: Module) -> None:
     """This module depends on another module and plugins."""
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtWidgets")
-    copy_qt_plugins(name, "sqldrivers", finder)
+    copy_qt_files(finder, name, "PluginsPath", "sqldrivers")
 
 
 def load_pyqt5_qtsvg(finder: ModuleFinder, module: Module) -> None:
@@ -323,6 +357,28 @@ def load_pyqt5_qtwebenginecore(finder: ModuleFinder, module: Module) -> None:
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtGui")
     finder.include_module(f"{name}.QtNetwork")
+    if WIN32:
+        copy_qt_files(
+            finder, name, "LibraryExecutablesPath", "QtWebEngineProcess.exe"
+        )
+        copy_qt_files(
+            finder, name, "LibraryExecutablesPath", "d3dcompiler_47.dll"
+        )
+        copy_qt_files(finder, name, "LibraryExecutablesPath", "libEGL.dll")
+        copy_qt_files(finder, name, "LibraryExecutablesPath", "libGLESv2.dll")
+        copy_qt_files(finder, name, "LibraryExecutablesPath", "opengl32sw.dll")
+    elif DARWIN:
+        copy_qt_files(
+            finder, name, "LibrariesPath", "QtWebEngineCore.framework"
+        )
+    else:
+        copy_qt_files(
+            finder, name, "LibraryExecutablesPath", "QtWebEngineProcess"
+        )
+        # conda-forge linux
+        copy_qt_files(finder, name, "LibrariesPath", "libnssckbi.so")
+    copy_qt_files(finder, name, "DataPath", "resources")
+    copy_qt_files(finder, name, "TranslationsPath")
 
 
 def load_pyqt5_qtwebenginewidgets(
@@ -332,15 +388,10 @@ def load_pyqt5_qtwebenginewidgets(
     name = _qt_implementation(module)
     finder.include_module(f"{name}.QtWebChannel")
     finder.include_module(f"{name}.QtWebEngineCore")
+    finder.include_module(f"{name}.QtWidgets")
     finder.include_module(f"{name}.QtPrintSupport")
-    if WIN32:
-        copy_qt_data(name, "QtWebEngineProcess.exe", finder)
-    else:
-        copy_qt_data(name, "libexec", finder)
-    copy_qt_data(name, "resources", finder)
-    copy_qt_data(name, "translations", finder)
-    copy_qt_plugins(name, "webview", finder)
-    copy_qt_plugins(name, "xcbglintegrations", finder)
+    copy_qt_files(finder, name, "PluginsPath", "webview")
+    copy_qt_files(finder, name, "PluginsPath", "xcbglintegrations")
 
 
 def load_pyqt5_qtwebkit(finder: ModuleFinder, module: Module) -> None:
