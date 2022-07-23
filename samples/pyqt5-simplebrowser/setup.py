f625c7db995803b69b2e7f277e665729cfbd69f1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-07-23 05:17:53 -0300

    New pyqt5 simplebrowser sample (adapted from pyside2 sample) (#1567)

---
+++ samples/pyqt5-simplebrowser/setup.py
@@ -0,0 +1,43 @@
+"""
+A simple setup script to create an executable using PyQt5 QtWebEngineWidgets.
+This also demonstrates how to use excludes to get minimal package size.
+
+Run the build process by running the command 'python setup.py build'
+
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application
+"""
+
+import sys
+
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+options = {
+    "build_exe": {
+        # exclude packages that are not really needed
+        "excludes": [
+            "tkinter",
+            "unittest",
+            "email",
+            "http",
+            "xml",
+            "pydoc",
+        ],
+        "zip_include_packages": ["PyQt5"],
+    },
+    "bdist_mac": {"bundle_name": "PyQt5 Webengine Test"},
+}
+
+executables = [
+    Executable("simplebrowser.py", target_name="test_simplebrowser")
+]
+
+setup(
+    name="simplebrowser",
+    version="0.1",
+    description="cx_Freeze and PyQt5 Webengine sample",
+    options=options,
+    executables=executables,
+)
