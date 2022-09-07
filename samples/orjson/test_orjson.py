1a5e4ffe0bd7a407e95e8c6d1a9dfce8034f394e
Marcelo Duarte <marcelotduarte@gmail.com>
2022-09-06 23:34:11 -0300

    samples: Add orjson sample

---
+++ samples/orjson/test_orjson.py
@@ -0,0 +1,8 @@
+import orjson
+
+print(orjson.dumps({"a": "b", "c": {"d": True}, "e": [1, 2]}))
+print(
+    orjson.dumps(
+        {"a": "b", "c": {"d": True}, "e": [1, 2]}, option=orjson.OPT_INDENT_2
+    ).decode()
+)
