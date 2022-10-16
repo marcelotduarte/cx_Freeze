9d7ce5c9db0cef0e96fce002ee54eabeec5a0444
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-10-15 23:37:36 -0300

    Do not translate newlines (generate identical file across OS) (#1645)

--- ci/requirements.py
+++ ci/requirements.py
@@ -16,12 +16,13 @@ def main():
 
     root_dir = Path(files[0]).parent
     requirements = root_dir / "requirements.txt"
-    requirements_dev = root_dir / "requirements-dev.txt"
+    requires_dev = root_dir / "requirements-dev.txt"
 
     contents = [
         "--extra-index-url https://marcelotduarte.github.io/packages/",
         "",
     ]
+
     try:
         install_requires = config["options"]["install_requires"].splitlines()
         for line in install_requires:
@@ -29,8 +30,8 @@ def main():
                 line = line.replace(" and python_version < '3.10'", "")
             contents.append(line)
         contents.append("")
-        contents_str = "\n".join(contents)
-        requirements.write_text(contents_str, encoding="utf_8")
+        with requirements.open(mode="w", encoding="utf_8", newline="") as file:
+            file.write("\n".join(contents))
     except KeyError:
         pass
     else:
@@ -38,15 +39,15 @@ def main():
 
     try:
         extras_require = config["options.extras_require"]
-        for k, require in extras_require.items():
-            contents.append(f"#{k}\n{require}".replace("\n\n", "\n"))
+        for extra, require in extras_require.items():
+            contents.append(f"# {extra}\n{require}".replace("\n\n", "\n"))
         contents.append("")
-        contents_str = "\n".join(contents)
-        requirements_dev.write_text(contents_str, encoding="utf_8")
+        with requires_dev.open(mode="w", encoding="utf_8", newline="") as file:
+            file.write("\n".join(contents))
     except KeyError:
         pass
     else:
-        print(requirements_dev, "ok")
+        print(requires_dev, "ok")
 
 
 if __name__ == "__main__":
