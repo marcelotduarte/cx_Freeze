a7925ac697d4033331a93c5f087e0dc6566f6eac
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2022-10-25 23:57:40 -0300

    Bump version: 6.13.0-dev0 → 6.13.0 (#1667)

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -49,7 +49,7 @@ copyright = "2022, Marcelo Duarte"
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "6.13.0-dev0"
+release = "6.13.0"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
