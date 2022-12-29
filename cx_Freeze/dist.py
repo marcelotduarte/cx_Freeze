a042dc33611bc8c4fd8543471a9099e30befd86e
Marcelo Duarte <marcelotduarte@gmail.com>
2022-12-29 11:50:28 -0300

    dist: Use project.entry-point to extend Distribution instead of subclassing

--- cx_Freeze/dist.py
+++ cx_Freeze/dist.py
@@ -2,34 +2,42 @@
 
 from __future__ import annotations
 
-from setuptools import Distribution as _Distribution
+from setuptools import Distribution
+from setuptools.errors import SetupError
+
+from .executable import Executable
 
 __all__ = ["Distribution", "DistributionMetadata"]
 
 
-class Distribution(_Distribution):
-    """Distribution with support for executables."""
+# pylint: disable-next=unused-argument
+def validate_executables(dist: Distribution, attr: str, value):
+    """Verify that value is a Executable list."""
 
-    def __init__(self, attrs):
-        self.executables = []
-        super().__init__(attrs)
-        self._fix_py_modules()
-
-    def _fix_py_modules(self):
-        # fix package discovery (setuptools >= 61)
-        if not self.executables:
-            return
-        self.include(
-            py_modules=[
-                executable.main_script.stem for executable in self.executables
-            ]
-        )
-
-    def has_executables(self):
-        """Predicate for build_exe command."""
-        return self.executables and len(self.executables) > 0
+    try:
+        # verify that value is a list or tuple to exclude unordered
+        # or single-use iterables
+        assert isinstance(value, (list, tuple))
+        # verify that elements of value are Executable
+        for executable in value:
+            assert isinstance(executable, Executable)
+    except (TypeError, ValueError, AttributeError, AssertionError) as exc:
+        raise SetupError(
+            f"{attr!r} must be a list of Executable (got {value!r})"
+        ) from exc
 
 
-DistributionMetadata = type(_Distribution().metadata)
+def finalize_distribution_options(dist: Distribution) -> None:
+    """Use a setuptools extension to customize Distribution options."""
+
+    if getattr(dist, "executables", None) is None:
+        return
+
+    # fix package discovery (setuptools >= 61)
+    if getattr(dist.metadata, "py_modules", None) is None:
+        dist.py_modules = []
+
+
+DistributionMetadata = type(Distribution().metadata)
 """Dummy class to hold the distribution meta-data: name, version, author,
 and so forth."""
