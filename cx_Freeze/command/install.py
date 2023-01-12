80196eab29778f5be5ce66e9233ed7dc829ab8d7
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-01-11 21:41:42 -0300

    Add/update commands (provisional) and minor tweaks (#1746)

--- cx_Freeze/command/install.py
+++ cx_Freeze/command/install.py
@@ -22,16 +22,17 @@ def suppress_known_deprecation():  # pylint: disable=missing-function-docstring
 class Install(_install):
     """Install everything from build directory."""
 
+    command_name = "install"
     user_options = _install.user_options + [
         ("install-exe=", None, "installation directory for executables")
     ]
 
     def expand_dirs(self):
-        _install.expand_dirs(self)
+        super().expand_dirs()
         self._expand_attrs(["install_exe"])
 
     def get_sub_commands(self):
-        sub_commands = _install.get_sub_commands(self)
+        sub_commands = super().get_sub_commands()[:]
         if self.distribution.executables:
             sub_commands.remove("install_egg_info")
             sub_commands.remove("install_scripts")
@@ -40,7 +41,7 @@ class Install(_install):
 
     def initialize_options(self):
         with suppress_known_deprecation():
-            _install.initialize_options(self)
+            super().initialize_options()
         self.install_exe = None
 
     def finalize_options(self):
@@ -52,14 +53,14 @@ class Install(_install):
             )
             prefix = str(winreg.QueryValueEx(key, "ProgramFilesDir")[0])
             metadata = self.distribution.metadata
-            self.prefix = f"{prefix}/{metadata.name}"
-        _install.finalize_options(self)
+            self.prefix = f"{prefix}\\{metadata.name}"
+        super().finalize_options()
         self.convert_paths("exe")
         if self.root is not None:
             self.change_roots("exe")
 
     def select_scheme(self, name):
-        _install.select_scheme(self, name)
+        super().select_scheme(name)
         if self.install_exe is None:
             if sys.platform == "win32":
                 self.install_exe = "$base"
