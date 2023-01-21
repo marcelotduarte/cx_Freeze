marcelotduarte/cx_Freeze

commit 80196eab29778f5be5ce66e9233ed7dc829ab8d7

Add/update commands (provisional) and minor tweaks (#1746)

cx_Freeze/__init__.py
@@ -79,11 +79,12 @@ def plugin_install(dist: setuptools.Distribution) -> None:
     if getattr(dist, "py_modules", None) is None:
         dist.py_modules = []
 
+    # Add/update commands (provisional)
+    dist.cmdclass["build_exe"] = build_exe
+    dist.cmdclass["install"] = install
+    dist.cmdclass["install_exe"] = install_exe
+
     # Add build_exe as subcommand of setuptools build (plugin)
-    dist.cmdclass.update(build_exe=build_exe)
     build = dist.get_command_obj("build")
-    build.sub_commands = [
-        *build.sub_commands,
-        ("build_exe", build_exe.has_executables),
-    ]
+    build.sub_commands = [*build.sub_commands, ("build_exe", None)]
     build.build_exe = None

cx_Freeze/command/install.py
@@ -22,16 +22,17 @@ def suppress_known_deprecation():  # pylint: disable=missing-function-docstring
 class Install(_install):
     """Install everything from build directory."""
 
+    command_name = "install"
     user_options = _install.user_options + [
         ("install-exe=", None, "installation directory for executables")
     ]
 
     def expand_dirs(self):
-        _install.expand_dirs(self)
+        super().expand_dirs()
         self._expand_attrs(["install_exe"])
 
     def get_sub_commands(self):
-        sub_commands = _install.get_sub_commands(self)
+        sub_commands = super().get_sub_commands()[:]
         if self.distribution.executables:
             sub_commands.remove("install_egg_info")
             sub_commands.remove("install_scripts")
@@ -40,7 +41,7 @@ class Install(_install):
 
     def initialize_options(self):
         with suppress_known_deprecation():
-            _install.initialize_options(self)
+            super().initialize_options()
         self.install_exe = None
 
     def finalize_options(self):
@@ -52,14 +53,14 @@ class Install(_install):
             )
             prefix = str(winreg.QueryValueEx(key, "ProgramFilesDir")[0])
             metadata = self.distribution.metadata
-            self.prefix = f"{prefix}/{metadata.name}"
-        _install.finalize_options(self)
+            self.prefix = f"{prefix}\\{metadata.name}"
+        super().finalize_options()
         self.convert_paths("exe")
         if self.root is not None:
             self.change_roots("exe")
 
     def select_scheme(self, name):
-        _install.select_scheme(self, name)
+        super().select_scheme(name)
         if self.install_exe is None:
             if sys.platform == "win32":
                 self.install_exe = "$base"

cx_Freeze/command/install_exe.py
@@ -14,6 +14,7 @@ __all__ = ["InstallEXE"]
 class InstallEXE(Command):
     """Install executables built from Python scripts."""
 
+    command_name = "install_exe"
     description = "install executables built from Python scripts"
     user_options = [
         ("install-dir=", "d", "directory to install executables to"),

commit e242ba3cd1fe083ff9d50e90a275bd60d756dd1d

Fix the subclassing of internal commands (regression introduced in #1746) (#1759)

cx_Freeze/__init__.py
@@ -46,23 +46,18 @@ else:
 __version__ = "6.14.0-dev1"
 
 
-def _add_command_class(command_classes, name, cls):
-    if name not in command_classes:
-        command_classes[name] = cls
-
-
 def setup(**attrs):  # pylint: disable=missing-function-docstring
-    command_classes = attrs.setdefault("cmdclass", {})
+    cmdclass = attrs.setdefault("cmdclass", {})
     if sys.platform == "win32":
-        _add_command_class(command_classes, "bdist_msi", bdist_msi)
+        cmdclass.setdefault("bdist_msi", bdist_msi)
     elif sys.platform == "darwin":
-        _add_command_class(command_classes, "bdist_dmg", bdist_dmg)
-        _add_command_class(command_classes, "bdist_mac", bdist_mac)
+        cmdclass.setdefault("bdist_dmg", bdist_dmg)
+        cmdclass.setdefault("bdist_mac", bdist_mac)
     else:
-        _add_command_class(command_classes, "bdist_rpm", bdist_rpm)
-    _add_command_class(command_classes, "build_exe", build_exe)
-    _add_command_class(command_classes, "install", install)
-    _add_command_class(command_classes, "install_exe", install_exe)
+        cmdclass.setdefault("bdist_rpm", bdist_rpm)
+    cmdclass.setdefault("build_exe", build_exe)
+    cmdclass.setdefault("install", install)
+    cmdclass.setdefault("install_exe", install_exe)
     setuptools.setup(**attrs)
 
 
@@ -80,9 +75,10 @@ def plugin_install(dist: setuptools.Distribution) -> None:
         dist.py_modules = []
 
     # Add/update commands (provisional)
-    dist.cmdclass["build_exe"] = build_exe
-    dist.cmdclass["install"] = install
-    dist.cmdclass["install_exe"] = install_exe
+    cmdclass = dist.cmdclass
+    cmdclass.setdefault("build_exe", build_exe)
+    cmdclass.setdefault("install", install)
+    cmdclass.setdefault("install_exe", install_exe)
 
     # Add build_exe as subcommand of setuptools build (plugin)
     build = dist.get_command_obj("build")