3ff39cdf955be0467a9eeaccc463fdf64e24ecb7
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-02-23 14:40:46 -0300

    hooks: add suport for matplotlib 3.7 (#1791)

--- cx_Freeze/hooks/matplotlib.py
+++ cx_Freeze/hooks/matplotlib.py
@@ -13,29 +13,37 @@ from ..module import Module
 
 def load_matplotlib(finder: ModuleFinder, module: Module) -> None:
     """The matplotlib package requires mpl-data subdirectory."""
-    data_path = module.path[0] / "mpl-data"
     target_path = Path("lib", module.name, "mpl-data")
-    # After matplotlib 3.4 mpl-data is guaranteed to be a subdirectory.
+    # mpl-data is always in a subdirectory in matplotlib >= 3.4
+    data_path = module.path[0] / "mpl-data"
     if not data_path.is_dir():
         data_path = __import__("matplotlib").get_data_path()
         need_patch = True
     else:
-        need_patch = module.in_file_system == 0
+        need_patch = module.in_file_system == 0  # zip_include_packages
     finder.include_files(data_path, target_path, copy_dependent_files=False)
     finder.include_package("matplotlib")
     finder.exclude_module("matplotlib.tests")
     finder.exclude_module("matplotlib.testing")
+    # matplotlib >= 3.7 uses an additional library directory
+    module_libs_name = "matplotlib.libs"
+    source_dir = module.path[0].parent / module_libs_name
+    if source_dir.exists():
+        finder.include_files(source_dir, f"lib/{module_libs_name}")
+    # Patch is used only if using zip_include_packages or
+    # with some distributions that have matplotlib < 3.4 installed.
     if not need_patch or module.code is None:
         return
     code_to_inject = f"""
 def _get_data_path():
     import os, sys
-    return os.path.join(os.path.dirname(sys.executable), "{target_path!s}")
+    return os.path.join(sys.frozen_dir, "{target_path}")
 """
     for code_str in [
         code_to_inject,
         code_to_inject.replace("_get_data_path", "get_data_path"),
     ]:
+        # path if the name (_get_data_path and/or get_data_path) is found
         new_code = compile(code_str, os.fspath(module.file), "exec")
         co_func = new_code.co_consts[0]
         name = co_func.co_name
