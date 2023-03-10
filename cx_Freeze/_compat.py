ff679b01190d82f87a511d52be1add9ab0839017
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-03-10 04:07:10 -0300

    hooks: pyside6 - check for conda (#1828)

--- cx_Freeze/_compat.py
+++ cx_Freeze/_compat.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import sys
 import sysconfig
+from pathlib import Path
 
 if sys.version_info >= (3, 10):
     import importlib.metadata as importlib_metadata
@@ -12,12 +13,15 @@ else:
 
 __all__ = ["cached_property", "importlib_metadata"]
 __all__ += ["PLATFORM", "IS_LINUX", "IS_MACOS", "IS_MINGW", "IS_WINDOWS"]
+__all__ += ["IS_CONDA"]
 
 PLATFORM = sysconfig.get_platform()
 IS_LINUX = PLATFORM.startswith("linux")
 IS_MACOS = PLATFORM.startswith("macos")
 IS_MINGW = PLATFORM.startswith("mingw")
 IS_WINDOWS = PLATFORM.startswith("win")
+
+IS_CONDA = Path(sys.prefix, "conda-meta").is_dir()
 
 
 try:
