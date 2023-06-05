e3d098b4357d60885e01dc0231e4efdbe6cbdaca
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-06-05 15:46:04 -0300

    samples: add a new matplotlib sample using Tk

---
+++ samples/matplotlib_tk/README.md
@@ -0,0 +1,15 @@
+# Samples
+
+Here are samples to test cx_Freeze or to show how to use a package in cx_Freeze.
+
+# Source
+
+https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_tk_sgskip.html
+
+# Installation and requirements:
+
+In a virtual environment, install by issuing the command:
+
+```
+pip install --upgrade cx_Freeze matplotlib
+```
---
+++ samples/matplotlib_tk/setup.py
@@ -0,0 +1,48 @@
+"""A simple setup script to create executable using matplotlib."""
+
+# test_matplotlib_tk.py is a very simple matplotlib application that
+# demonstrates its use.
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+
+from __future__ import annotations
+
+import sys
+
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+options = {
+    "build_exe": {
+        # Sometimes a little fine-tuning is needed
+        # exclude all backends except tkinter
+        "excludes": [
+            "gi",
+            "gtk",
+            "PyQt4",
+            "PyQt5",
+            "PyQt6",
+            "PySide2",
+            "PySide6",
+            "shiboken2",
+            "shiboken6",
+            "wx",
+        ]
+    }
+}
+
+executables = [
+    Executable("test_matplotlib_tk.py", base=base),
+]
+
+setup(
+    name="matplotlib_samples",
+    version="0.1",
+    description="Sample matplotlib script",
+    executables=executables,
+    options=options,
+)
