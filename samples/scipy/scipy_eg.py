59a6d53620b673a66e681a7685e6d91caf7331d7
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-06-05 15:32:47 -0300

    black

--- samples/scipy/scipy_eg.py
+++ samples/scipy/scipy_eg.py
@@ -4,4 +4,4 @@ from scipy.stats import norm
 
 
 if __name__ == "__main__":
-    print('bounds of distribution lower: %s, upper: %s' % norm.support())
+    print("bounds of distribution lower: %s, upper: %s" % norm.support())
