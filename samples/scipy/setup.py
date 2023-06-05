cfed204cdfb29fc9bc79200289f17697b32e3276
Diederick Niehorster <dcnieho@gmail.com>
2023-06-05 14:54:35 +0200

    add scipy sample

---
+++ samples/scipy/setup.py
@@ -0,0 +1,28 @@
+"""A simple setup script to create executables using matplotlib."""
+
+# scipy_eg.py is a very simple scipy application that demonstrates
+# its use.
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+
+from __future__ import annotations
+
+import sys
+
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+executables = [
+    Executable("scipy_eg.py", base=base),
+]
+
+setup(
+    name="scipy_samples",
+    version="0.1",
+    description="Sample scipy script",
+    executables=executables,
+)
