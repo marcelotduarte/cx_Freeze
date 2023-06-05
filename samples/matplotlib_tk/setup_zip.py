2cd0a641d6baa652386f7a1bffcaf256ffcf4b36
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-06-05 15:53:56 -0300

    samples: add a new matplotlib sample using Tk (#1901)

---
+++ samples/matplotlib_tk/setup_zip.py
@@ -0,0 +1,58 @@
+"""A simple setup script to create executable using matplotlib
+   This version requires the mpl-data in the zip file."""
+
+from __future__ import annotations
+
+# test_matplotlib_tk.py is a very simple matplotlib application that
+# demonstrates its use.
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+import os
+import sys
+import sysconfig
+
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+platform = sysconfig.get_platform()
+python_version = sysconfig.get_python_version()
+dir_name = f"zip.{platform}-{python_version}"
+build_exe_dir = os.path.join("build", dir_name)
+
+options = {
+    "build_exe": {
+        # Sometimes a little fine-tuning is needed
+        # exclude all backends except tkinter
+        "excludes": [
+            "gi",
+            "gtk",
+            "PyQt4",
+            "PyQt5",
+            "PyQt6",
+            "PySide2",
+            "PySide6",
+            "shiboken2",
+            "shiboken6",
+            "wx",
+        ],
+        "zip_include_packages": ["*"],
+        "zip_exclude_packages": [],
+        "build_exe": build_exe_dir,
+    }
+}
+
+executables = [
+    Executable("test_matplotlib_tk.py", base=base),
+]
+
+setup(
+    name="matplotlib_samples",
+    version="0.1",
+    description="Sample matplotlib script",
+    executables=executables,
+    options=options,
+)
