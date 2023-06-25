35a5275d6c6e080b43ceb680af8190ffaa5085c3
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-06-25 11:15:13 -0300

    tests: add more samples to tests (#1928)

--- samples/asmodule/setup.py
+++ samples/asmodule/setup.py
@@ -1,13 +1,12 @@
-# A very simple setup script to create a single executable built from a module
-# which includes an executable section protected by "if __name__ == '__main__'
+"""A very simple setup script to create a single executable built from a module
+which includes an executable section protected by:
+if __name__ == '__main__'
+"""
 #
 # Run the build process by running the command 'python setup.py build'
 #
 # If everything works well you should find a subdirectory in the build
 # subdirectory that contains the files needed to run the script without Python
-
-from __future__ import annotations
-
 from cx_Freeze import Executable, setup
 
 executables = [Executable("asmodule.py")]
