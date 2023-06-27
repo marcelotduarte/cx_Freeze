c76e3a33326f4c0408530a4f567e5081b9f4527d
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-06-27 04:36:04 -0300

    fix: zip_include_packages/zip_exclude_packages regression (#1922)

--- tests/test_command_build_exe.py
+++ tests/test_command_build_exe.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from sysconfig import get_platform, get_python_version
 
 import pytest
+from generate_samples import create_package, sub_package_test
 
 from cx_Freeze.sandbox import run_setup
 
@@ -19,6 +20,8 @@ FIXTURE_DIR = Path(__file__).resolve().parent
 
 OUTPUT1 = "Hello from cx_Freeze Advanced #1\nTest freeze module #1\n"
 OUTPUT2 = "Hello from cx_Freeze Advanced #2\nTest freeze module #2\n"
+
+OUTPUT_SUBPACKAGE_TEST = "This is p.p1\nThis is p.q.q1\n"
 
 
 @pytest.mark.datafiles(FIXTURE_DIR.parent / "samples" / "advanced")
@@ -87,3 +90,49 @@ def test_build_exe_sqlite(datafiles: Path):
         [os.fspath(executable)], text=True, timeout=10
     )
     assert output.startswith("dump.sql created")
+
+
+def test_zip_include_packages(tmp_path):
+    """Provides test cases for ModuleFinder class."""
+    source = sub_package_test[4]
+    create_package(tmp_path, source)
+    run_setup(
+        tmp_path / "setup.py",
+        [
+            "build_exe",
+            "--silent",
+            "--excludes=tkinter",
+            "--zip-exclude-packages=*",
+            "--zip-include-packages=p",
+        ],
+    )
+    suffix = ".exe" if sys.platform == "win32" else ""
+    executable = tmp_path / BUILD_EXE_DIR / f"main{suffix}"
+    assert executable.is_file()
+    output = subprocess.check_output(
+        [os.fspath(executable)], text=True, timeout=10
+    )
+    assert output == OUTPUT_SUBPACKAGE_TEST
+
+
+def test_zip_exclude_packages(tmp_path):
+    """Provides test cases for ModuleFinder class."""
+    source = sub_package_test[4]
+    create_package(tmp_path, source)
+    run_setup(
+        tmp_path / "setup.py",
+        [
+            "build_exe",
+            "--silent",
+            "--excludes=tkinter",
+            "--zip-exclude-packages=p",
+            "--zip-include-packages=*",
+        ],
+    )
+    suffix = ".exe" if sys.platform == "win32" else ""
+    executable = tmp_path / BUILD_EXE_DIR / f"main{suffix}"
+    assert executable.is_file()
+    output = subprocess.check_output(
+        [os.fspath(executable)], text=True, timeout=10
+    )
+    assert output == OUTPUT_SUBPACKAGE_TEST
