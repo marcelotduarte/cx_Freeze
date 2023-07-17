cf502ea22751790cbab654bbc2d991b4e85906b1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-07-17 00:44:58 -0300

    samples: update samples using wxPython (#1965)

--- samples/wx/setup.py
+++ samples/wx/setup.py
@@ -15,15 +15,33 @@ import sys
 
 from cx_Freeze import Executable, setup
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+base = "Win32GUI" if sys.platform == "win32" else None
 
-executables = [Executable("wxapp.py", base=base)]
+options = {
+    "build_exe": {
+        # Sometimes a little fine-tuning is needed
+        # exclude all backends except wx
+        "excludes": [
+            "gi",
+            "gtk",
+            "PyQt4",
+            "PyQt5",
+            "PyQt6",
+            "PySide2",
+            "PySide6",
+            "shiboken2",
+            "shiboken6",
+            "tkinter",
+        ]
+    }
+}
+
+executables = [Executable("test_wx.py", base=base)]
 
 setup(
-    name="hello",
+    name="wxapp_sample",
     version="0.1",
     description="Sample cx_Freeze wxPython script",
     executables=executables,
+    options=options,
 )
