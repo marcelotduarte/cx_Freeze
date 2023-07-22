a8fa8dd95d9f0e45e06e004f30bcd8e9812982a4
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-07-22 18:53:21 -0300

    hooks: support opencv-python 4.8.0 [msys2] (#1975)

--- cx_Freeze/_compat.py
+++ cx_Freeze/_compat.py
@@ -27,6 +27,7 @@ PLATFORM = sysconfig.get_platform()
 IS_LINUX = PLATFORM.startswith("linux")
 IS_MACOS = PLATFORM.startswith("macos")
 IS_MINGW = PLATFORM.startswith("mingw")
+IS_MINGW64 = PLATFORM.startswith("mingw_x86_64")
 IS_WINDOWS = PLATFORM.startswith("win")
 
 IS_CONDA = Path(sys.prefix, "conda-meta").is_dir()
