1ff637e5aff900f6e6f09f602a60f70c1a9f5a48
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-07-23 03:03:47 -0300

    hooks: Disable sandbox in PySide2 WebEngine

---
+++ cx_Freeze/hooks/pyside2/_append_to_init.py
@@ -0,0 +1,26 @@
+"""Module used to inject a code to PySide2/__init__."""
+import os
+import sys
+from pathlib import Path
+
+
+def _run():
+    # Support for QtWebEngine
+    os.environ["QTWEBENGINE_DISABLE_SANDBOX"] = "1"
+
+    # Inject a option to debug if environment variable QT_DEBUG is set.
+    if os.environ.get("QT_DEBUG"):
+        qtcore = __import__("PySide2", fromlist=["QtCore"]).QtCore
+        # Show QLibraryInfo paths.
+        data = {}
+        for key, value in qtcore.QLibraryInfo.__dict__.items():
+            if isinstance(value, qtcore.QLibraryInfo.LibraryLocation):
+                data[key] = Path(qtcore.QLibraryInfo.location(value))
+        print("QLibraryInfo:", file=sys.stdout)
+        for key, value in data.items():
+            print(" ", key, value, file=sys.stdout)
+        print("LibraryPaths:", file=sys.stdout)
+        print(" ", qtcore.QCoreApplication.libraryPaths(), file=sys.stdout)
+
+
+_run()
