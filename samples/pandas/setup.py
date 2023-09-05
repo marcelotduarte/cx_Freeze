b41cf497d5cfa43fa0c465b381cab4fce670a3a1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-09-05 04:13:33 -0300

    hooks: add support for pandas 2.1.0 (#2032)

--- samples/pandas/setup.py
+++ samples/pandas/setup.py
@@ -1,25 +1,29 @@
-"""A simple setup script to test pandas."""
+"""
+A simple setup script to create an executable using pandas. This also
+demonstrates how to use excludes to get minimal package size.
 
-# test_pandas.py is a very simple script.
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
+test_pandas.py is a very simple type of pandas application.
 
-from __future__ import annotations
+Run the build process by running the command 'python setup.py build'
 
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application.
+"""
 from cx_Freeze import Executable, setup
 
-options = {"build_exe": {"excludes": ["tkinter"]}}
-
+options = {
+    "build_exe": {
+        # exclude packages that are not really needed
+        "excludes": ["tkinter", "unittest", "email", "http", "xml", "pydoc"],
+    },
+}
 executables = [
     Executable("test_pandas.py"),
 ]
 
 setup(
     name="test_pandas",
-    version="0.1",
+    version="0.1.0.1",
     description="Sample pandas script",
     executables=executables,
     options=options,
