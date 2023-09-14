e9eaf0960857da8078c706b6c4a18a42df2fa2b7
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-09-14 05:13:58 -0300

    samples: small tweaks to demonstrate independent options (#2045)

---
+++ samples/manifest/README.md
@@ -0,0 +1,18 @@
+# Icon sample
+
+Creates an executable to demonstrate the use of manifest.
+
+
+# Installation and requirements:
+
+In a virtual environment, install by issuing the command:
+
+```
+pip install cx_Freeze
+```
+
+# Build the executable:
+
+```
+python setup.py build
+```
--- samples/simple/hello.py
+++ samples/simple/hello.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import sys
 from datetime import datetime
 
@@ -12,11 +10,11 @@ print(f"Default encoding: {sys.getdefaultencoding()}")
 print(f"File system encoding: {sys.getfilesystemencoding()}\n")
 
 print("ARGUMENTS:")
-for a in sys.argv:
-    print(f"{a}")
+for arg in sys.argv:
+    print(f"{arg}")
 print()
 
 print("PATH:")
-for p in sys.path:
-    print(f"{p}")
+for path in sys.path:
+    print(f"{path}")
 print()
