0e9a77900e3a811f5405b3bd3216fb4beef58f88
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-09-16 02:22:25 -0300

    fix scipy dylibs too

--- cx_Freeze/hooks/numpy.py
+++ cx_Freeze/hooks/numpy.py
@@ -51,7 +51,7 @@ def load_numpy(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
         "_pyinstaller",
         "random._examples",
         "random.tests",
-        "testing",
+        "testing.tests",
         "tests",
         "typing.tests",
     ]:
@@ -76,9 +76,9 @@ def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
     # patch the code when necessary
     code_string = module.file.read_text(encoding="utf_8")
 
-    # installed from pypi, using zip_include_packages we must fix it
-    numpy_dir = module.file.parent
-    libs_dir = numpy_dir.joinpath(".dylibs" if IS_MACOS else ".libs")
+    # installed from pypi?
+    module_dir = module.file.parent
+    libs_dir = module_dir.joinpath(".dylibs" if IS_MACOS else ".libs")
     if libs_dir.is_dir():
         # copy any file at site-packages/numpy/.libs
         finder.include_files(
@@ -86,7 +86,7 @@ def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
         )
     else:
         # cgohlke/numpy-mkl.whl, numpy 1.23.5+mkl
-        libs_dir = numpy_dir / "DLLs"
+        libs_dir = module_dir / "DLLs"
         if libs_dir.is_dir():
             finder.exclude_module("numpy.DLLs")
             finder.include_files(libs_dir, "lib/numpy/DLLs")
@@ -134,7 +134,7 @@ def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
 
     # do not check dependencies already handled
     extension = EXTENSION_SUFFIXES[0]
-    for file in numpy_dir.rglob(f"*{extension}"):
+    for file in module_dir.rglob(f"*{extension}"):
         finder.exclude_dependent_files(file)
 
     if module.in_file_system == 0:
@@ -206,7 +206,7 @@ def load_numpy_linalg(
 
 def load_numpy__pytesttester(_, module: Module) -> None:
     """Remove optional modules in the numpy._pytesttester module."""
-    module.exclude_names.update(["numpy.distutils", "numpy.testing", "pytest"])
+    module.exclude_names.add("pytest")
 
 
 def load_numpy_random_mtrand(_, module: Module) -> None:
