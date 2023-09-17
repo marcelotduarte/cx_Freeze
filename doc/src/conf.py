f2b87f429fe093d72048dc0f026cb2260477fc02
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-09-17 05:21:25 -0300

    "Bump version: 6.16.0-dev6 → 6.16.0-dev7 [ci skip]"

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -48,7 +48,7 @@ copyright = "2023, Marcelo Duarte"  # noqa: A001
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "6.16.0-dev6"
+release = "6.16.0-dev7"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
