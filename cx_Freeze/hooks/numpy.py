4f11f5172b3adc9efd33c4c8cda59682dec5f687
IperGiove <49456524+IperGiove@users.noreply.github.com>
2023-10-01 04:16:58 +0200

    hooks: initialize blas [numpy conda-forge] (#2074)

--- cx_Freeze/hooks/numpy.py
+++ cx_Freeze/hooks/numpy.py
@@ -122,6 +122,7 @@ def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
         blas_options = ["libopenblas", "mkl"]
         packages += blas_options
         files_to_copy: list[Path] = []
+        blas = None
         for package in packages:
             try:
                 pkg = next(conda_meta.glob(f"{package}-*.json"))
