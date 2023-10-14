ff1cc0bb774a5492047ab8a0fb246f6dc864af69
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-10-14 13:56:40 -0300

    update the manifest sample to use as test

--- samples/manifest/setup.py
+++ samples/manifest/setup.py
@@ -9,12 +9,28 @@ if sys.platform == "win32":
 else:
     icon_ok = "icon.png"
 
-# uac_admin read and write a manifest
-executables = [Executable("test_manifest.py", icon=icon_ok, uac_admin=True)]
+executables = [
+    # use default manifest
+    Executable("test_manifest.py", icon=icon_ok),
+    # write a new manifest
+    Executable(
+        "test_manifest.py",
+        icon=icon_ok,
+        manifest="simple.manifest",
+        target_name="test_simple_manifest",
+    ),
+    # read and write a modified manifest using uac_admin
+    Executable(
+        "test_manifest.py",
+        icon=icon_ok,
+        uac_admin=True,
+        target_name="test_uac_admin",
+    ),
+]
 
 setup(
     name="Manifest sample",
-    version="0.1",
-    description="Test use of manifest changing the uac_admin",
+    version="0.2",
+    description="Test the use of manifest and uac_admin",
     executables=executables,
 )
