435e401c014f36636d9d1438345924751b1ac415
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-10-19 22:04:44 -0300

    bdist_mac: make symlink between Resources/share and Contents/MacOS (#2105)

--- samples/pillow/test_pillow.py
+++ samples/pillow/test_pillow.py
@@ -5,14 +5,21 @@ import sys
 
 from PIL import Image
 
+
+def find_data_file(filename):
+    if getattr(sys, "frozen", False):
+        # The application is frozen
+        datadir = os.path.join(os.path.dirname(sys.executable), "share")
+    else:
+        # The application is not frozen
+        # Change this bit to match where you store your data files:
+        datadir = os.path.dirname(__file__)
+    return os.path.join(datadir, filename)
+
+
 print("Opening image with PIL")
 
-if getattr(sys, "frozen", False):
-    filename_png = os.path.join(os.path.dirname(sys.executable), "favicon.png")
-else:
-    filename_png = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "favicon.png"
-    )
+filename_png = find_data_file("favicon.png")
 filename_pdf = os.path.join(os.path.dirname(filename_png), "test_pillow.pdf")
 with Image.open(filename_png) as im, open(filename_pdf, "w+b") as fp:
     if im.mode == "RGBA":
