3d92019760218d6056c7e8f7a4689217e513fb68
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-11-13 04:57:57 -0300

    samples: improve qt samples (#2128)

--- samples/pyside6-simplebrowser/setup.py
+++ samples/pyside6-simplebrowser/setup.py
@@ -9,13 +9,12 @@ Run the build process by running the command 'python setup.py build'
 If everything works well you should find a subdirectory in the build
 subdirectory that contains the files needed to run the application
 """
-
-from __future__ import annotations
-
+import os
 import sys
 
 from cx_Freeze import Executable, setup
 
+# base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
 options = {
@@ -30,8 +29,18 @@ options = {
             "xml",
             "pydoc",
         ],
-        "zip_include_packages": ["PySide6"],
-    }
+        "zip_include_packages": ["PySide6", "shiboken6"],
+    },
+    "bdist_mac": {
+        "custom_info_plist": None,  # Set this to use a custom info.plist file
+        "codesign_entitlements": os.path.join(
+            os.path.dirname(__file__), "codesign-entitlements.plist"
+        ),
+        "codesign_identity": None,  # Set this to enable signing with custom identity (replaces adhoc signature)
+        "codesign_options": "runtime",  # Ensure codesign uses 'hardened runtime'
+        "codesign_verify": False,  # Enable to get more verbose logging regarding codesign
+        "spctl_assess": False,  # Enable to get more verbose logging regarding codesign
+    },
 }
 
 executables = [
@@ -40,7 +49,7 @@ executables = [
 
 setup(
     name="simplebrowser",
-    version="0.1",
+    version="0.1.1",
     description="Sample cx_Freeze PySide6 simplebrowser script",
     options=options,
     executables=executables,
