3960e3d80df4fa36a2c4d51c499254e20cb2dc81
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-11-20 07:11:23 -0300

    hooks: adds anyio, pyarrow and tiktoken (#2134)

---
+++ cx_Freeze/hooks/tiktoken.py
@@ -0,0 +1,14 @@
+"""A collection of functions which are triggered automatically by finder when
+tiktoken package is included.
+"""
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_tiktoken(
+    finder: ModuleFinder, module: Module  # noqa: ARG001
+) -> None:
+    """The tiktoken must include extension."""
+    finder.include_module("tiktoken_ext.openai_public")
