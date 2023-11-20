25babdb01117d625c7c70bf04541d2242a8461f5
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-11-20 07:11:23 -0300

    hooks: adds anyio, pyarrow and tiktoken (#2134)

---
+++ cx_Freeze/hooks/anyio.py
@@ -0,0 +1,12 @@
+"""A collection of functions which are triggered automatically by finder when
+anyio package is included.
+"""
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_anyio(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
+    """The anyio must include backends."""
+    finder.include_module("anyio._backends._asyncio")
