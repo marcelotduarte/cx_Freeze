9b319b9c0ae8c7262d9e849418650193d634e0bd
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-11-24 11:12:31 -0300

    hooks: add yt_dlp

---
+++ cx_Freeze/hooks/yt_dlp.py
@@ -0,0 +1,15 @@
+"""A collection of functions which are triggered automatically by finder when
+yt_dlp package is included.
+"""
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_yt_dlp(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
+    """The yt_dlp must include backends."""
+    finder.include_module("yt_dlp.compat._deprecated")
+    finder.include_module("yt_dlp.compat._legacy")
+    finder.include_module("yt_dlp.utils._deprecated")
+    finder.include_module("yt_dlp.utils._legacy")
