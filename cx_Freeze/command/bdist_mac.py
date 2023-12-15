311f48ed4803d2124cb05a6321756c498868b695
HeisenBerg? <admin@tiktokastudio.com>
2023-12-15 22:15:09 +0800

    Update bdist_mac.py
    https://github.com/marcelotduarte/cx_Freeze/discussions/2167
    fix create symlink between folders under Resources to Contents/Macos when bdis_dmg

--- cx_Freeze/command/bdist_mac.py
+++ cx_Freeze/command/bdist_mac.py
@@ -429,16 +429,20 @@ class BdistMac(Command):
             target = os.path.join(self.bin_dir, executable.target_name)
             self.move_file(source, target)
 
-        # Make symlink between Resources/lib and Contents/MacOS so we can use
+        # Make symlink between folders under Resources such as lib and other specified by user in include_files and Contents/MacOS so we can use
         # none-relative reference paths in order to pass codesign...
-        resources_lib_dir = os.path.join(self.resources_dir, "lib")
-        origin = os.path.join(self.bin_dir, "lib")
-        relative_reference = os.path.relpath(resources_lib_dir, self.bin_dir)
-        self.execute(
-            os.symlink,
-            (relative_reference, origin, True),
-            msg=f"linking {origin} -> {relative_reference}",
-        )
+        for filename in os.listdir(self.resources_dir):
+            target = os.path.join(self.resources_dir, filename)
+
+            if os.path.isdir(target):
+                target
+                origin = os.path.join(self.bin_dir, filename)
+                relative_reference = os.path.relpath(target, self.bin_dir)
+                self.execute(
+                    os.symlink,
+                    (relative_reference, origin, True),
+                    msg=f"linking {origin} -> {relative_reference}",
+                    )
         # Make symlink between Resources/share and Contents/MacOS too.
         resource_share_dir = os.path.join(self.resources_dir, "share")
         if os.path.exists(resource_share_dir):
