dbd8d0410cace8ba31b9dc011b02d8f264fcdb93
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2023-12-16 16:48:32 -0300

    hooks: add av

--- cx_Freeze/hooks/av.py
+++ cx_Freeze/hooks/av.py
@@ -11,9 +11,13 @@ from cx_Freeze.module import Module
 
 def load_av(finder: ModuleFinder, module: Module) -> None:
     """The PyAV package."""
-    libs_name = "pyav.libs"
-    source_dir = module.file.parent.parent / libs_name
-    if source_dir.exists() and IS_WINDOWS:
-        finder.include_files(source_dir, f"lib/{libs_name}")
-        replace_delvewheel_patch(module, libs_name)
+    if IS_WINDOWS:
+        libs_name = "av.libs"
+        source_dir = module.file.parent.parent / libs_name
+        if not source_dir.exists():
+            libs_name = "pyav.libs"
+            source_dir = module.file.parent.parent / libs_name
+        if source_dir.exists():
+            finder.include_files(source_dir, f"lib/{libs_name}")
+            replace_delvewheel_patch(module, libs_name)
     finder.include_module("av.deprecation")
