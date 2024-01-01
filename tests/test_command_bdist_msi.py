296c1ffe33480a4e6953841923c054a4812672f1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-01-01 14:09:50 -0300

    tests: simplify test using a run_command

--- tests/test_command_bdist_msi.py
+++ tests/test_command_bdist_msi.py
@@ -1,13 +1,11 @@
 """Tests for cx_Freeze.command.bdist_msi."""
 from __future__ import annotations
 
-import os
-import sys
 from pathlib import Path
-from subprocess import check_output
 from sysconfig import get_platform
 
 import pytest
+from generate_samples import run_command
 from setuptools import Distribution
 
 bdist_msi = pytest.importorskip(
@@ -26,11 +24,7 @@ FIXTURE_DIR = Path(__file__).resolve().parent
 @pytest.mark.datafiles(FIXTURE_DIR.parent / "samples" / "msi_binary_data")
 def test_bdist_msi(datafiles: Path):
     """Test the msi_binary_data sample."""
-    output = check_output(
-        [sys.executable, "setup.py", "bdist_msi"],
-        text=True,
-        cwd=os.fspath(datafiles),
-    )
+    output = run_command(datafiles, "python setup.py bdist_msi")
     print(output)
     platform = get_platform().replace("win-amd64", "win64")
     file_created = datafiles / "dist" / f"hello-0.1-{platform}.msi"
@@ -64,10 +58,8 @@ def test_bdist_msi_target_name_with_extension(datafiles: Path):
     includes an ".msi" extension.
     """
     msi_name = "output.msi"
-    output = check_output(
-        [sys.executable, "setup.py", "bdist_msi", "--target-name", msi_name],
-        text=True,
-        cwd=os.fspath(datafiles),
+    output = run_command(
+        datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
     )
     print(output)
     file_created = datafiles / "dist" / msi_name
--- tests/test_command_build.py
+++ tests/test_command_build.py
@@ -1,13 +1,12 @@
 """Tests for cx_Freeze.command.build."""
 from __future__ import annotations
 
-import os
 import sys
 from pathlib import Path
-from subprocess import check_output
 from sysconfig import get_platform, get_python_version
 
 import pytest
+from generate_samples import run_command
 
 PLATFORM = get_platform()
 PYTHON_VERSION = get_python_version()
@@ -18,11 +17,7 @@ FIXTURE_DIR = Path(__file__).resolve().parent
 @pytest.mark.datafiles(FIXTURE_DIR.parent / "samples" / "simple")
 def test_build(datafiles: Path):
     """Test the simple sample."""
-    output = check_output(
-        [sys.executable, "setup.py", "build"],
-        text=True,
-        cwd=os.fspath(datafiles),
-    )
+    output = run_command(datafiles)
     print(output)
     suffix = ".exe" if sys.platform == "win32" else ""
     file_created = datafiles / BUILD_EXE_DIR / f"hello{suffix}"
