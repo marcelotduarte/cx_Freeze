1c23ace2b1e30cb7f128a0950a65407ef54d01f3
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-01-17 20:55:13 -0300

    hooks: add pyproj

---
+++ cx_Freeze/hooks/pyproj.py
@@ -0,0 +1,14 @@
+"""A collection of functions which are triggered automatically by finder when
+pyproj package is included.
+"""
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_pyproj(finder: ModuleFinder, module: Module) -> None:
+    """The pyproj package."""
+    source_dir = module.file.parent.parent / f"{module.name}.libs"
+    if source_dir.exists():
+        finder.include_files(source_dir, f"lib/{source_dir.name}")
