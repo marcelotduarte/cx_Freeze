2621a1d0fe8d7b829767f1ce999721c5ebf61c40
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-02-08 03:40:21 -0300

    tests: add more tests for bdist_msi (#2248)

--- tests/test_command_bdist_msi.py
+++ tests/test_command_bdist_msi.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from sysconfig import get_platform
 
 import pytest
-from generate_samples import run_command
+from generate_samples import create_package, run_command
 from setuptools import Distribution
 
 bdist_msi = pytest.importorskip(
@@ -19,15 +19,6 @@ DIST_ATTRS = {
     "script_name": "setup.py",
 }
 SAMPLES_DIR = Path(__file__).resolve().parent.parent / "samples"
-
-
-@pytest.mark.datafiles(SAMPLES_DIR / "msi_binary_data")
-def test_bdist_msi(datafiles: Path):
-    """Test the msi_binary_data sample."""
-    run_command(datafiles, "python setup.py bdist_msi")
-    platform = get_platform().replace("win-amd64", "win64")
-    file_created = datafiles / "dist" / f"hello-0.1-{platform}.msi"
-    assert file_created.is_file()
 
 
 def test_bdist_msi_target_name():
@@ -52,6 +43,15 @@ def test_bdist_msi_target_name_and_version():
 
 
 @pytest.mark.datafiles(SAMPLES_DIR / "msi_binary_data")
+def test_bdist_msi_default(datafiles: Path):
+    """Test the msi_binary_data sample."""
+    run_command(datafiles, "python setup.py bdist_msi")
+    platform = get_platform().replace("win-amd64", "win64")
+    file_created = datafiles / "dist" / f"hello-0.1-{platform}.msi"
+    assert file_created.is_file()
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "msi_binary_data")
 def test_bdist_msi_target_name_with_extension(datafiles: Path):
     """Test the msi_binary_data sample, with a specified target_name that
     includes an ".msi" extension.
@@ -61,4 +61,66 @@ def test_bdist_msi_target_name_with_extension(datafiles: Path):
         datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
     )
     file_created = datafiles / "dist" / msi_name
+    assert file_created.is_file()
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "advanced")
+def test_bdist_msi_advanced(datafiles: Path):
+    """Test the advanced sample."""
+    msi_name = "output.msi"
+    run_command(
+        datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
+    )
+    file_created = datafiles / "dist" / msi_name
+    assert file_created.is_file()
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "asmodule")
+def test_bdist_msi_asmodule(datafiles: Path):
+    """Test the asmodule sample."""
+    msi_name = "output.msi"
+    run_command(
+        datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
+    )
+    file_created = datafiles / "dist" / msi_name
+    assert file_created.is_file()
+
+
+@pytest.mark.datafiles(SAMPLES_DIR / "sqlite")
+def test_bdist_msi_sqlite(datafiles: Path):
+    """Test the sqlite sample."""
+    msi_name = "output.msi"
+    run_command(
+        datafiles, f"python setup.py bdist_msi --target-name {msi_name}"
+    )
+    file_created = datafiles / "dist" / msi_name
+    assert file_created.is_file()
+
+
+SOURCE_HELLO = """
+hello.py
+    import pkg.hi
+    print("Hello from cx_Freeze")
+setup.py
+    from cx_Freeze import Executable, setup
+
+    setup(
+        name="hello",
+        version="0.1.2.3",
+        description="Sample cx_Freeze script",
+        executables=[Executable("hello.py")],
+    )
+pkg/hi.py
+    print("Hi!")
+"""
+
+
+def test_bdist_msi_advanced2(tmp_path: Path):
+    """Test the executables option."""
+    create_package(tmp_path, SOURCE_HELLO)
+    msi_name = "output.msi"
+    run_command(
+        tmp_path, f"python setup.py bdist_msi --target-name {msi_name}"
+    )
+    file_created = tmp_path / "dist" / msi_name
     assert file_created.is_file()
