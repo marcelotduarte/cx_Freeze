1f1ec83f54769c3a1f40ef209401731a5f8cd3e8
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-02-11 03:35:19 -0300

    tests: add more tests for freezer

--- tests/test_freezer.py
+++ tests/test_freezer.py
@@ -113,42 +113,18 @@ def test_freezer_default_bin_includes_emulated(tmp_path: Path, monkeypatch):
     assert expected in freezer.bin_includes
 
 
-def test_freezer_populate_zip_options(tmp_path: Path, monkeypatch):
-    """Test freezer _populate_zip_options."""
+def test_freezer_populate_zip_options_invalid_values(
+    tmp_path: Path, monkeypatch
+):
+    """Test freezer _populate_zip_options invalid values."""
     create_package(tmp_path, SOURCE)
     monkeypatch.chdir(tmp_path)
-
-    # zip_include_packages and zip_exclude_packages are None (default)
-    freezer = Freezer(executables=[Executable("hello.py")])
-    assert freezer.zip_include_packages == set()
-    assert freezer.zip_exclude_packages == set("*")
-    assert freezer.zip_include_all_packages is False
-
-    # zip_include_packages=*
-    freezer = Freezer(
-        executables=[Executable("hello.py")],
-        zip_include_packages=["*"],
-        zip_exclude_packages=[],
-    )
-    assert freezer.zip_include_packages == set("*")
-    assert freezer.zip_exclude_packages == set()
-    assert freezer.zip_include_all_packages is True
-
-    # zip_exclude_packages=*
-    freezer = Freezer(
-        executables=[Executable("hello.py")],
-        zip_include_packages=[],
-        zip_exclude_packages=["*"],
-    )
-    assert freezer.zip_include_packages == set()
-    assert freezer.zip_exclude_packages == set("*")
-    assert freezer.zip_include_all_packages is False
 
     # zip_include_packages and zip_exclude_packages are "*"
     with pytest.raises(
         OptionError, match="all packages cannot be included and excluded "
     ):
-        freezer = Freezer(
+        Freezer(
             executables=[Executable("hello.py")],
             zip_include_packages=["*"],
             zip_exclude_packages=["*"],
@@ -156,7 +132,7 @@ def test_freezer_populate_zip_options(tmp_path: Path, monkeypatch):
 
     # zip_include_packages and zip_exclude_packages has the same package
     with pytest.raises(OptionError, match="package 'tkinter' cannot be both"):
-        freezer = Freezer(
+        Freezer(
             executables=[Executable("hello.py")],
             zip_include_packages=["tkinter"],
             zip_exclude_packages=["tkinter"],
@@ -164,18 +140,101 @@ def test_freezer_populate_zip_options(tmp_path: Path, monkeypatch):
     with pytest.raises(
         OptionError, match="packages 'tkinter, unittest' cannot be both"
     ):
-        freezer = Freezer(
+        Freezer(
             executables=[Executable("hello.py")],
             zip_include_packages=["tkinter", "unittest"],
             zip_exclude_packages=["tkinter", "unittest", "codeop"],
         )
 
-    # zip_include_packages and zip_exclude_packages are namespace packages
-    freezer = Freezer(
-        executables=[Executable("hello.py")],
-        zip_include_packages=["namespace.test"],
-        zip_exclude_packages=["zope.event", "zope.interface"],
-    )
-    assert freezer.zip_include_packages == {"namespace"}
-    assert freezer.zip_exclude_packages == {"zope"}
-    assert freezer.zip_include_all_packages is False
+
+@pytest.mark.parametrize(
+    ("kwargs", "expected"),
+    [
+        pytest.param(
+            {"include_msvcr": None},
+            {"include_msvcr": False},
+            id="include_msvcr=none",
+            marks=pytest.mark.skipif(not IS_WINDOWS, reason="Windows tests"),
+        ),
+        pytest.param(
+            {"include_msvcr": False},
+            {"include_msvcr": False},
+            id="include_msvcr=false",
+            marks=pytest.mark.skipif(not IS_WINDOWS, reason="Windows tests"),
+        ),
+        pytest.param(
+            {"include_msvcr": True},
+            {"include_msvcr": True},
+            id="include_msvcr=true",
+            marks=pytest.mark.skipif(not IS_WINDOWS, reason="Windows tests"),
+        ),
+        # optimize values
+        pytest.param(
+            {"optimize": None}, {"optimize": 0}, id="optimize=none->0"
+        ),
+        pytest.param({"optimize": 0}, {"optimize": 0}, id="optimize=0->0"),
+        pytest.param({"optimize": 1}, {"optimize": 1}, id="optimize=1->1"),
+        pytest.param({"optimize": 2}, {"optimize": 2}, id="optimize=2->2"),
+        # silent values
+        pytest.param({"silent": None}, {"silent": 0}, id="silent=none->0"),
+        pytest.param({"silent": False}, {"silent": 0}, id="silent=false->0"),
+        pytest.param({"silent": True}, {"silent": 1}, id="silent=true->1"),
+        pytest.param({"silent": 0}, {"silent": 0}, id="silent=0->0"),
+        pytest.param({"silent": 1}, {"silent": 1}, id="silent=1->1"),
+        pytest.param({"silent": 2}, {"silent": 2}, id="silent=2->2"),
+        pytest.param({"silent": "3"}, {"silent": 3}, id="silent=3->3"),
+        # test _populate_zip_options
+        pytest.param(
+            {"zip_include_packages": None, "zip_exclude_packages": None},
+            {
+                "zip_include_packages": set(),
+                "zip_exclude_packages": set("*"),
+                "zip_include_all_packages": False,
+            },
+            id="zip_include_packages/zip_exclude_packages=none/none",
+        ),
+        pytest.param(
+            {"zip_include_packages": ["*"], "zip_exclude_packages": None},
+            {
+                "zip_include_packages": set("*"),
+                "zip_exclude_packages": set(),
+                "zip_include_all_packages": True,
+            },
+            id="zip_include_package=*",
+        ),
+        pytest.param(
+            {"zip_include_packages": None, "zip_exclude_packages": ["*"]},
+            {
+                "zip_include_packages": set(),
+                "zip_exclude_packages": set("*"),
+                "zip_include_all_packages": False,
+            },
+            id="zip_exclude_packages=*",
+        ),
+        pytest.param(  # zip_*_packages are namespace packages
+            {
+                "zip_include_packages": ["namespace.test"],
+                "zip_exclude_packages": ["zope.event", "zope.interface"],
+            },
+            {
+                "zip_include_packages": {"namespace"},
+                "zip_exclude_packages": {"zope"},
+                "zip_include_all_packages": False,
+            },
+            id="zip_include_packages/zip_exclude_packages=namespace/namespace",
+        ),
+    ],
+)
+def test_freezer_options(
+    tmp_path: Path,
+    monkeypatch,
+    kwargs: dict[str, ...],
+    expected: dict[str, ...],
+):
+    """Test freezer options."""
+    create_package(tmp_path, SOURCE)
+    monkeypatch.chdir(tmp_path)
+
+    freezer = Freezer(executables=[Executable("hello.py")], **kwargs)
+    for option, value in expected.items():
+        assert getattr(freezer, option) == value
