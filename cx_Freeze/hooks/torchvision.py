acec3b0454bc28c0005ac8ad63cb07a85d01c04f
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-03-15 03:09:03 -0300

    fix torchvision hook

--- cx_Freeze/hooks/torchvision.py
+++ cx_Freeze/hooks/torchvision.py
@@ -8,9 +8,14 @@ from cx_Freeze.finder import ModuleFinder
 from cx_Freeze.module import Module
 
 
-def load_torchvision_models(finder: ModuleFinder, module: Module) -> None:
+def load_torchvision(finder: ModuleFinder, module: Module) -> None:
     """Hook for torchvision."""
-    source_path = module.file.parent
+    module_path = module.file.parent
+    site_packages_path = module_path.parent
+
+    # include source of torchvision.models
+    source_path = site_packages_path / "torchvision/models"
     for source in source_path.rglob("*.py"):  # type: Path
-        target = "lib" / source.relative_to(source_path)
+        target = "lib" / source.relative_to(site_packages_path)
         finder.include_files(source, target)
+    finder.exclude_module("torchvision.models")
