ee4f848b01803d8da7e6c6059b9d129d6dd345f0
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-03-16 01:27:46 -0300

    hooks: add easyocr and torchvision (also update skickit-image and pytorch) (#2286)

---
+++ cx_Freeze/hooks/easyocr.py
@@ -0,0 +1,20 @@
+"""A collection of functions which are triggered automatically by finder when
+easyocr package is included.
+"""
+
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_easyocr(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
+    """The easyocr package."""
+    finder.include_module("easyocr.easyocr")
+    finder.include_module("easyocr.model.vgg_model")
+    finder.include_module("imageio.plugins.pillow")
+
+
+def load_easyocr_easyocr(_, module: Module) -> None:
+    """Ignore optional modules."""
+    module.ignore_names.update(["six", "pathlib2"])
---
+++ cx_Freeze/hooks/torchvision.py
@@ -0,0 +1,20 @@
+"""A collection of functions which are triggered automatically by finder when
+torchvision package is included.
+"""
+
+from __future__ import annotations
+
+from cx_Freeze.finder import ModuleFinder
+from cx_Freeze.module import Module
+
+
+def load_torchvision(finder: ModuleFinder, module: Module) -> None:
+    """Hook for torchvision."""
+    module_path = module.file.parent
+    site_packages_path = module_path.parent
+
+    # include source of torchvision.models
+    source_path = site_packages_path / "torchvision/models"
+    for source in source_path.rglob("*.py"):  # type: Path
+        target = "lib" / source.relative_to(site_packages_path)
+        finder.include_files(source, target)
