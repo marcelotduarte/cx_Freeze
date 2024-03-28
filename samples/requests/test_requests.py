d926c7111c0fb094f744c2c374db255f11d7637f
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-03-28 03:24:22 -0300

    samples: Add ssl and requests samples

---
+++ samples/requests/test_requests.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import os
+import requests
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
+
+print("Hello from cx_Freeze")
+print(DEFAULT_CA_BUNDLE_PATH)
+print(os.environ.get("SSL_CERT_FILE"))
+
+try:
+    r = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    print(r.json())
+except requests.exceptions.RequestException as exc:
+    print(exc)
