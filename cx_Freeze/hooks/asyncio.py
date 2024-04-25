de2888d31d344c3d50994c4ecaaeb668bcc9b095
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-04-25 06:13:23 -0300

    hooks: fix regression in msys2 (#2357)

--- cx_Freeze/hooks/asyncio.py
+++ cx_Freeze/hooks/asyncio.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from cx_Freeze._compat import IS_WINDOWS
+from cx_Freeze._compat import IS_MINGW, IS_WINDOWS
 
 if TYPE_CHECKING:
     from cx_Freeze.finder import ModuleFinder
@@ -20,11 +20,11 @@ def load_asyncio(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
 
 def load_asyncio_windows_events(_, module: Module) -> None:
     """Ignore modules not found in current OS."""
-    if not IS_WINDOWS:
+    if not IS_WINDOWS and not IS_MINGW:
         module.exclude_names.update({"_overlapped", "_winapi", "msvcrt"})
 
 
 def load_asyncio_windows_utils(_, module: Module) -> None:
     """Ignore modules not found in current OS."""
-    if not IS_WINDOWS:
+    if not IS_WINDOWS and not IS_MINGW:
         module.exclude_names.update({"_winapi", "msvcrt"})
