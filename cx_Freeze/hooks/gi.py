b17c3db943d4e5364da1979fd256f80661ecee6c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-05-27 23:12:48 -0300

    hooks: fix pygobject hook for Linux (#2425)

--- cx_Freeze/hooks/gi.py
+++ cx_Freeze/hooks/gi.py
@@ -5,6 +5,7 @@ PyGObject package is included.
 from __future__ import annotations
 
 import sys
+import sysconfig
 from ctypes.util import find_library
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -15,10 +16,14 @@ if TYPE_CHECKING:
     from cx_Freeze.finder import ModuleFinder
     from cx_Freeze.module import Module
 
+# To test in Ubuntu, pulls all dependencies for pygobject, then install it:
+# sudo apt install python3-gi libgirepository1.0-dev libcairo2-dev
+# pip install pygobject
+
 
 def load_gi(finder: ModuleFinder, module: Module) -> None:
     """The PyGObject package."""
-    required = [
+    libraries = [
         "libgtk-3-0",
         "libgdk-3-0",
         "libepoxy-0",
@@ -29,15 +34,15 @@ def load_gi(finder: ModuleFinder, module: Module) -> None:
         "libpangowin32-1.0-0",
         "libatk-1.0-0",
     ]
-    for lib in required:
+    for lib in libraries:
         name = find_library(lib)
         if name:
             library_path = (
-                Path(sys.prefix, "bin", name) if IS_LINUX else Path(name)
+                Path(sys.base_prefix, "bin", name) if IS_LINUX else Path(name)
             )
             finder.include_files(library_path, f"lib/{library_path.name}")
 
-    required_gi_namespaces = [
+    common_namespaces = [
         "Atk-1.0",
         "GLib-2.0",
         "GModule-2.0",
@@ -52,9 +57,15 @@ def load_gi(finder: ModuleFinder, module: Module) -> None:
         "freetype2-2.0",
     ]
 
-    for ns in required_gi_namespaces:
-        subpath = f"lib/girepository-1.0/{ns}.typelib"
-        fullpath = Path(sys.prefix, subpath)
-        finder.include_files(fullpath, subpath)
+    if IS_LINUX:
+        libdir = Path(sysconfig.get_config_var("LIBDIR"))
+    else:
+        libdir = Path(sys.base_prefix, "lib")
+
+    for ns in common_namespaces:
+        subpath = f"girepository-1.0/{ns}.typelib"
+        fullpath = libdir / subpath
+        if fullpath.exists():
+            finder.include_files(fullpath, f"lib/{subpath}")
 
     finder.include_package(module.name)
