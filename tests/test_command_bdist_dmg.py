bc563515c1df4c6cd3e97f8c0f5224cd3630102a
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-06-11 16:35:40 -0300

    make bdist_dmg tests pass

--- tests/test_command_bdist_dmg.py
+++ tests/test_command_bdist_dmg.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 
 import sys
 from pathlib import Path
+from subprocess import run
 
 import pytest
 from generate_samples import run_command
 
-bdist_mac = pytest.importorskip(
-    "cx_Freeze.command.bdist_mac", reason="macOS tests"
-).bdist_mac
+bdist_dmg = pytest.importorskip(
+    "cx_Freeze.command.bdist_dmg", reason="macOS tests"
+).bdist_dmg
 
 if sys.platform != "darwin":
     pytest.skip(reason="macOS tests", allow_module_level=True)
@@ -35,7 +36,19 @@ def test_bdist_dmg(datafiles: Path) -> None:
     version = "0.1.2.3"
     dist_created = datafiles / "build"
 
-    run_command(datafiles, "python setup.py bdist_dmg")
+    process = run(
+        [sys.executable, "setup.py", "bdist_dmg"],
+        text=True,
+        capture_output=True,
+        check=False,
+        cwd=datafiles,
+    )
+    if process.returncode != 0:
+        expected_err = "hdiutil: create failed - Resource busy"
+        if expected_err in process.stderr:
+            pytest.xfail(expected_err)
+        else:
+            pytest.fail(process.stderr)
 
     base_name = f"{name}-{version}"
     file_created = dist_created / f"{base_name}.dmg"
