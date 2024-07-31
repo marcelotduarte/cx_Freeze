42d697dc6eb55e27192964ff46960d606163901d
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-07-31 02:19:48 -0300

    bdist_rpm: fix use of 'cxfreeze bdsit_rpm' command

--- tests/test_command_install.py
+++ tests/test_command_install.py
@@ -14,11 +14,26 @@ SOURCE = """
 test.py
     print("Hello from cx_Freeze")
 setup.py
-    from cx_Freeze import setup, Executable
+    from cx_Freeze import setup
 
-    setup(name="hello", executables=[Executable("test.py")])
+    setup(name="hello", version = "0.1.2.3", executables=["test.py"])
 command
     python setup.py install --prefix=base --root=root
+"""
+
+SOURCE_PYPROJECT = """
+test.py
+    print("Hello from cx_Freeze")
+pyproject.toml
+    [project]
+    name = "hello"
+    version = "0.1.2.3"
+    description = "Sample cx_Freeze script"
+
+    [tool.cxfreeze]
+    executables = ["test.py"]
+command
+    cxfreeze install --prefix=base --root=root
 """
 
 
@@ -32,7 +47,27 @@ def test_install(tmp_path: Path) -> None:
         prefix = program_files.relative_to(program_files.anchor) / "hello"
     else:
         run_command(tmp_path)
-        prefix = "base/lib/hello-0.0.0"
+        prefix = "base/lib/hello-0.1.2.3"
+    install_dir = tmp_path / "root" / prefix
+
+    file_created = install_dir / f"test{EXE_SUFFIX}"
+    assert file_created.is_file(), f"file not found: {file_created}"
+
+    output = run_command(tmp_path, file_created, timeout=10)
+    assert output.startswith("Hello from cx_Freeze")
+
+
+def test_install_pyproject(tmp_path: Path) -> None:
+    """Test a simple install."""
+    create_package(tmp_path, SOURCE_PYPROJECT)
+
+    if sys.platform == "win32":
+        run_command(tmp_path, "cxfreeze install --root=root")
+        program_files = Path(os.getenv("PROGRAMFILES"))
+        prefix = program_files.relative_to(program_files.anchor) / "hello"
+    else:
+        run_command(tmp_path)
+        prefix = "base/lib/hello-0.1.2.3"
     install_dir = tmp_path / "root" / prefix
 
     file_created = install_dir / f"test{EXE_SUFFIX}"
