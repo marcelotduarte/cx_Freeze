3e2bd772a1900007c89275bf29c571e2abb0d351
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-10-16 13:52:48 -0300

    samples: Add ssl and requests samples

---
+++ samples/ssl/setup.py
@@ -0,0 +1,22 @@
+"""A setup script to demonstrate build using ssl."""
+
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import Executable, setup
+
+setup(
+    name="test_ssl",
+    version="0.1",
+    description="cx_Freeze script to test ssl",
+    executables=[Executable("test_ssl.py")],
+    options={
+        "build_exe": {
+            "zip_include_packages": ["*"],
+            "zip_exclude_packages": [],
+        }
+    },
+)
