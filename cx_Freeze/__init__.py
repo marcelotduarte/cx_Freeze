a2474eab285634ff59f3768a506e6b13ae77715b
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-10-29 00:22:59 -0300

    Bump version: 7.2.3 → 7.2.4 [ci skip]

--- cx_Freeze/__init__.py
+++ cx_Freeze/__init__.py
@@ -45,7 +45,7 @@ else:
     __all__ += ["bdist_appimage", "bdist_deb", "bdist_rpm"]
 
 
-__version__ = "7.2.3"
+__version__ = "7.2.4"
 
 
 def setup(**attrs) -> setuptools.Distribution:  # noqa: D103
