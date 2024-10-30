36de21a64e491288b3666a75c24112b3fd923011
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-10-30 06:36:01 -0300

    hooks: fix for pymupdf 1.24.11+ (#2655)

--- cx_Freeze/hooks/pymupdf.py
+++ cx_Freeze/hooks/pymupdf.py
@@ -11,6 +11,30 @@ if TYPE_CHECKING:
     from cx_Freeze.module import Module
 
 
-def load_pymupdf(finder: ModuleFinder, module: Module) -> None:  # noqa: ARG001
-    """The pymupdf must include mupdf."""
+def load_pymupdf(finder: ModuleFinder, module: Module) -> None:
+    """The pymupdf must include hidden modules."""
+    module.ignore_names.update(
+        ["mupdf_cppyy", "mupdf", "pymupdf_fonts", "PIL"]
+    )
     finder.include_module("pymupdf.mupdf")
+    finder.include_module("pymupdf.utils")
+
+
+def load_pymupdf_extra(_, module: Module) -> None:
+    """Ignore development import."""
+    module.ignore_names.add("_extra")
+
+
+def load_pymupdf_mupdf(_, module: Module) -> None:
+    """Ignore development import."""
+    module.ignore_names.add("_mupdf")
+
+
+def load_pymupdf_table(_, module: Module) -> None:
+    """Ignore optional package."""
+    module.ignore_names.add("pandas")
+
+
+def load_pymupdf_utils(_, module: Module) -> None:
+    """Ignore development import and optional package."""
+    module.ignore_names.update(["mupdf", "fontTools.subset"])
