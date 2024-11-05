e61421433cf1f15389126eedfab606c96bdba35c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-11-05 13:46:00 -0300

    hooks: zoneinfo has an error not catched by a typo in tests (#2666)

--- cx_Freeze/hooks/importlib.py
+++ cx_Freeze/hooks/importlib.py
@@ -4,6 +4,7 @@ importlib namespace is included.
 
 from __future__ import annotations
 
+from contextlib import suppress
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -11,11 +12,15 @@ if TYPE_CHECKING:
     from cx_Freeze.module import Module
 
 
-def load_importlib(_, module: Module) -> None:
+def load_importlib(finder: ModuleFinder, module: Module) -> None:
     """The importlib module should filter import names."""
     if module.in_file_system == 1:
         # Use optimized mode
         module.in_file_system = 2
+    # include module used by importlib._bootstrap_external
+    # (internally mapped to _frozen_importlib_external)
+    with suppress(ImportError):
+        finder.include_module("importlib.readers")  # Python 3.10+
 
 
 def load_importlib_metadata(finder: ModuleFinder, module: Module) -> None:
