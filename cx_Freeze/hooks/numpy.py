89ec624891c49f98fb54e25491b104656957f36c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-11-08 05:22:38 -0300

    hooks: numpy - resolve missing modules

--- cx_Freeze/hooks/numpy.py
+++ cx_Freeze/hooks/numpy.py
@@ -34,7 +34,7 @@ if TYPE_CHECKING:
 def load_numpy(finder: ModuleFinder, module: Module) -> None:
     """The numpy package.
 
-    Supported pypi and conda-forge versions (tested from 1.21.2 to 2.1.1).
+    Supported pypi and conda-forge versions (tested from 1.21.2 to 2.1.3).
     """
     source_dir = module.file.parent.parent / f"{module.name}.libs"
     if source_dir.exists():  # numpy >= 1.26.0
@@ -46,45 +46,128 @@ def load_numpy(finder: ModuleFinder, module: Module) -> None:
             for source in source_dir.iterdir():
                 finder.lib_files[source] = f"{target_dir}/{source.name}"
 
-    distribution = module.distribution
+    # Exclude unnecessary modules
+    finder.exclude_module("numpy._configtool")
+    finder.exclude_module("numpy.conftest")
+    finder.exclude_module("numpy.distutils")
+    finder.exclude_module("numpy.f2py")
+    finder.exclude_module("numpy._pyinstaller")
+    finder.exclude_module("numpy.random._examples")
+    finder.exclude_module("numpy.typing.mypy_plugin")
 
-    # Exclude all tests
+    # Exclude/Include modules based on distribution and/or version
+    distribution = module.distribution
     if distribution:
-        tests = set()
+        # Exclude tests and diagnose
+        excludes = set()
         for file in distribution.original.files:
             if file.parent.match("**/tests"):
-                tests.add(file.parent.as_posix().replace("/", "."))
-        for test in tests:
-            finder.exclude_module(test)
+                excludes.add(file.parent.as_posix().replace("/", "."))
+            if file.match("**/setup.py") or file.match("**/diagnose.py"):
+                excludes.add(file.with_suffix("").as_posix().replace("/", "."))
+        for exclude in excludes:
+            finder.exclude_module(exclude)
 
         # Include dynamically loaded module / exclude unnecessary modules
         if distribution.version >= (2, 0):
+            finder.exclude_module("numpy._core.include")
+            finder.exclude_module("numpy._core.lib")
+            finder.exclude_module("numpy.compat")
             finder.include_package("numpy._core._exceptions")
             finder.include_package("numpy._core._dtype_ctypes")
             finder.include_package("numpy._core._methods")
             finder.include_package("numpy._core._multiarray_tests")
-            finder.exclude_module("numpy._core.include")
-            finder.exclude_module("numpy._core.lib")
         else:
+            finder.exclude_module("numpy.core.include")
+            finder.exclude_module("numpy.core.lib")
             finder.include_package("numpy.core._exceptions")
             finder.include_package("numpy.core._dtype_ctypes")
             finder.include_package("numpy.core._methods")
             finder.include_package("numpy.core._multiarray_tests")
-            finder.exclude_module("numpy.core.include")
-            finder.exclude_module("numpy.core.lib")
     else:
         finder.include_package("numpy.core")
-
-    # Exclude unnecessary modules
-    finder.exclude_module("numpy.conftest")
-    finder.exclude_module("numpy.distutils")
-    finder.exclude_module("numpy._pyinstaller")
-    finder.exclude_module("numpy.random._examples")
 
     # Include dynamically loaded module
     finder.include_module("numpy.lib.format")
     finder.include_module("numpy.polynomial")
     finder.include_module("secrets")
+
+
+def load_numpy_compat_py3k(_, module: Module) -> None:
+    """Ignore errors if optionally imported module cannot be found."""
+    module.ignore_names.add("pickle5")
+
+
+def load_numpy___config__(_, module: Module) -> None:
+    """Ignore errors if optionally imported module cannot be found."""
+    module.ignore_names.add("yaml")
+
+
+# see list from numpy/__init__.py
+NUMPY_GLOBAL_NAMES = """
+    False_, ScalarType, True_, _get_promotion_state, _no_nep50_warning,
+    _set_promotion_state, abs, absolute, acos, acosh, add, all, allclose,
+    amax, amin, any, arange, arccos, arccosh, arcsin, arcsinh,
+    arctan, arctan2, arctanh, argmax, argmin, argpartition, argsort,
+    argwhere, around, array, array2string, array_equal, array_equiv,
+    array_repr, array_str, asanyarray, asarray, ascontiguousarray,
+    asfortranarray, asin, asinh, atan, atanh, atan2, astype, atleast_1d,
+    atleast_2d, atleast_3d, base_repr, binary_repr, bitwise_and,
+    bitwise_count, bitwise_invert, bitwise_left_shift, bitwise_not,
+    bitwise_or, bitwise_right_shift, bitwise_xor, block, bool, bool_,
+    broadcast, busday_count, busday_offset, busdaycalendar, byte, bytes_,
+    can_cast, cbrt, cdouble, ceil, character, choose, clip, clongdouble,
+    complex128, complex64, complexfloating, compress, concat, concatenate,
+    conj, conjugate, convolve, copysign, copyto, correlate, cos, cosh,
+    count_nonzero, cross, csingle, cumprod, cumsum, cumulative_prod,
+    cumulative_sum, datetime64, datetime_as_string, datetime_data,
+    deg2rad, degrees, diagonal, divide, divmod, dot, double, dtype, e,
+    einsum, einsum_path, empty, empty_like, equal, errstate, euler_gamma,
+    exp, exp2, expm1, fabs, finfo, flatiter, flatnonzero, flexible,
+    float16, float32, float64, float_power, floating, floor, floor_divide,
+    fmax, fmin, fmod, format_float_positional, format_float_scientific,
+    frexp, from_dlpack, frombuffer, fromfile, fromfunction, fromiter,
+    frompyfunc, fromstring, full, full_like, gcd, generic, geomspace,
+    get_printoptions, getbufsize, geterr, geterrcall, greater,
+    greater_equal, half, heaviside, hstack, hypot, identity, iinfo, iinfo,
+    indices, inexact, inf, inner, int16, int32, int64, int8, int_, intc,
+    integer, intp, invert, is_busday, isclose, isdtype, isfinite,
+    isfortran, isinf, isnan, isnat, isscalar, issubdtype, lcm, ldexp,
+    left_shift, less, less_equal, lexsort, linspace, little_endian, log,
+    log10, log1p, log2, logaddexp, logaddexp2, logical_and, logical_not,
+    logical_or, logical_xor, logspace, long, longdouble, longlong, matmul,
+    matrix_transpose, max, maximum, may_share_memory, mean, memmap, min,
+    min_scalar_type, minimum, mod, modf, moveaxis, multiply, nan, ndarray,
+    ndim, nditer, negative, nested_iters, newaxis, nextafter, nonzero,
+    not_equal, number, object_, ones, ones_like, outer, partition,
+    permute_dims, pi, positive, pow, power, printoptions, prod,
+    promote_types, ptp, put, putmask, rad2deg, radians, ravel, recarray,
+    reciprocal, record, remainder, repeat, require, reshape, resize,
+    result_type, right_shift, rint, roll, rollaxis, round, sctypeDict,
+    searchsorted, set_printoptions, setbufsize, seterr, seterrcall, shape,
+    shares_memory, short, sign, signbit, signedinteger, sin, single, sinh,
+    size, sort, spacing, sqrt, square, squeeze, stack, std,
+    str_, subtract, sum, swapaxes, take, tan, tanh, tensordot,
+    timedelta64, trace, transpose, true_divide, trunc, typecodes, ubyte,
+    ufunc, uint, uint16, uint32, uint64, uint8, uintc, uintp, ulong,
+    ulonglong, unsignedinteger, unstack, ushort, var, vdot, vecdot, void,
+    vstack, where, zeros, zeros_like
+"""
+
+
+def load_numpy_core(_, module: Module) -> None:  # NumPy < 2.0
+    """Set the numpy.core global names."""
+    module.global_names.update(
+        NUMPY_GLOBAL_NAMES.replace("\n", "").replace(" ", "").split(",")
+    )
+    module.global_names.add("geterrobj")
+
+
+def load_numpy__core(_, module: Module) -> None:
+    """Set the numpy._core global names."""
+    module.global_names.update(
+        NUMPY_GLOBAL_NAMES.replace("\n", "").replace(" ", "").split(",")
+    )
 
 
 def load_numpy__core_overrides(finder: ModuleFinder, module: Module) -> None:
@@ -106,6 +189,7 @@ load_numpy_core_overrides = load_numpy__core_overrides  # numpy < 2.0
 
 def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
     """Fix the location of dependent files in all OS."""
+    module.ignore_names.add("numpy._distributor_init_local")
     # check versions that are handled correctly
     if IS_MINGW:
         return
@@ -207,8 +291,7 @@ def load_numpy__distributor_init(finder: ModuleFinder, module: Module) -> None:
 
 def load_numpy_core_numerictypes(_, module: Module) -> None:
     """The numpy.core.numerictypes module adds a number of items to itself
-    dynamically; define these to avoid spurious errors about missing
-    modules.
+    dynamically; define these to avoid spurious errors about missing modules.
     """
     module.global_names.update(
         [
@@ -228,32 +311,18 @@ def load_numpy_core_numerictypes(_, module: Module) -> None:
     )
 
 
-def load_numpy_distutils_command_scons(_, module: Module) -> None:
-    """The numpy.distutils.command.scons module optionally imports the numscons
+def load_numpy_lib_utils(_, module: Module) -> None:  # numpy<2
+    """The module numpy.lib.utils optionally imports the threadpoolctl module;
+    ignore the error if the module cannot be found.
+    """
+    module.ignore_names.add("threadpoolctl")
+
+
+def load_numpy_lib__utils_impl(_, module: Module) -> None:
+    """The module numpy.lib._utils_impl optionally imports the threadpoolctl
     module; ignore the error if the module cannot be found.
     """
-    module.ignore_names.add("numscons")
-
-
-def load_numpy_distutils_misc_util(_, module: Module) -> None:
-    """The numpy.distutils.misc_util module optionally imports the numscons
-    module; ignore the error if the module cannot be found.
-    """
-    module.ignore_names.add("numscons")
-
-
-def load_numpy_distutils_system_info(_, module: Module) -> None:
-    """The numpy.distutils.system_info module optionally imports the Numeric
-    module; ignore the error if the module cannot be found.
-    """
-    module.ignore_names.add("Numeric")
-
-
-def load_numpy_f2py___version__(_, module: Module) -> None:
-    """The numpy.f2py.__version__ module optionally imports the __svn_version__
-    module; ignore the error if the module cannot be found.
-    """
-    module.ignore_names.add("__svn_version__")
+    module.ignore_names.add("threadpoolctl")
 
 
 def load_numpy_linalg(
@@ -268,7 +337,8 @@ def load_numpy_linalg(
 
 def load_numpy__pytesttester(_, module: Module) -> None:
     """Remove optional modules in the numpy._pytesttester module."""
-    module.exclude_names.add("pytest")
+    module.exclude_names.update(["numpy.distutils", "pytest"])
+    module.ignore_names.update(["numpy.distutils", "pytest"])
 
 
 def load_numpy_random_mtrand(_, module: Module) -> None:
@@ -278,3 +348,15 @@ def load_numpy_random_mtrand(_, module: Module) -> None:
     modules.
     """
     module.global_names.update(["rand", "randn"])
+
+
+def load_numpy_testing__private_utils(_, module: Module) -> None:
+    """Ignore optional modules."""
+    module.ignore_names.update(
+        ["numpy.distutils.misc_util", "psutil", "pytest", "win32pdh"]
+    )
+
+
+def load_numpy__typing(_, module: Module) -> None:
+    """Ignore optional module."""
+    module.ignore_names.add("numpy._typing._ufunc")
