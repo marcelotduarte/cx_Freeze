ab0e7f88516e52baf9a447e059c1d16245586d5a
skilkis <sankilkis@msn.com>
2024-11-13 12:54:12 +0100

    Use embedded code for test

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,51 +1,44 @@
 """Tests for cx_Freeze.module."""
 
-from collections.abc import Generator
 from importlib.machinery import EXTENSION_SUFFIXES
 from pathlib import Path
 
-import pytest
+from generate_samples import create_package
 
 from cx_Freeze import Module
 
 ROOT = Path(__file__).parent
 
 
-@pytest.fixture
-def namespace() -> Module:
-    """Return implicit namespace package ``namespace``."""
-    data = ROOT / "data"
-    return Module(
-        name="namespace",
-        path=[data / "namespace"],
-        filename=None,
-    )
+SOURCE = """
+namespacepack/firstchildpack/__init__{extension}
+    FOO = 42
+namespacepack/firstchildpack/__init__.pyi
+    FOO: int
+"""
 
 
-@pytest.fixture
-def foo(namespace: Module) -> Generator[Module, None, None]:
-    """Return extension module ``namespace.foo.__init__``."""
-    data = ROOT / "data"
-    prefix = data / "namespace" / "foo" / "__init__"
-    tempfile = Path(prefix.with_suffix(EXTENSION_SUFFIXES[-1]))
-    try:
-        tempfile.write_text(prefix.with_suffix(".py").read_text())
-        yield Module(
-            name="namespace.foo",
-            path=[data / "namespace" / "foo"],
-            filename=tempfile,
-            parent=namespace,
-        )
-    finally:
-        tempfile.unlink()
-
-
-def test_implicit_namespace_package(foo: Module, namespace: Module) -> None:
+def test_implicit_namespace_package(tmp_path: Path) -> None:
     """Test `Module.stub_code` with a namespace package.
 
     Implicit namespace packages do not contain an `__init__.py`. Thus
     `Module.root.file` becomes `None`. This test checks that
     `Module.stub_code` does not raise errors in this scenario.
     """
-    assert foo.stub_code is None
-    assert namespace.stub_code is None
+    ext = EXTENSION_SUFFIXES[-1]
+    create_package(tmp_path, SOURCE.format(extension=ext))
+
+    root = tmp_path / "namespacepack"
+    namespacepack = Module(
+        name="namespacepack",
+        path=[root],
+        filename=None,
+    )
+    firstchildpack = Module(
+        name="namespace.foo",
+        path=[root / "firstchildpack"],
+        filename=root / "firstchildpack" / f"__init__{ext}",
+        parent=namespacepack,
+    )
+    assert firstchildpack.stub_code is None
+    assert namespacepack.stub_code is None
