4711d0cdadcfcaedc01d0be0ed9db30a27ff0d76
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-04-10 01:55:53 -0300

    tests: add scipy sample as a hook test (#2888)

--- tests/hooks/test_numpy.py
+++ tests/hooks/test_numpy.py
@@ -1,4 +1,4 @@
-"""Tests for some cx_Freeze.hooks."""
+"""Tests for cx_Freeze.hooks of numpy, pandas and scipy."""
 
 from __future__ import annotations
 
@@ -28,3 +28,26 @@ def test_pandas(tmp_package, zip_packages: bool) -> None:
     assert lines[0].startswith("numpy version")
     assert lines[1].startswith("pandas version")
     assert len(lines) == 8, lines[2:]
+
+
+@pytest.mark.parametrize(
+    "zip_packages", [False, True], ids=["", "zip-include-packages"]
+)
+def test_scipy(tmp_package, zip_packages: bool) -> None:
+    """Test that the scipy/numpy is working correctly."""
+    pytest.importorskip("scipy", reason="Depends on extra package: scipy")
+
+    command = "python setup.py build_exe -O2"
+    if zip_packages:
+        command += " --zip-include-packages=* --zip-exclude-packages="
+
+    tmp_package.create_from_sample("scipy")
+    output = tmp_package.run("python setup.py build_exe -O2")
+    executable = tmp_package.executable("test_scipy")
+    assert executable.is_file()
+
+    output = tmp_package.run(executable, timeout=10)
+    lines = output.splitlines()
+    assert lines[0].startswith("numpy version")
+    assert lines[1].startswith("scipy version")
+    assert len(lines) == 5, lines[2:]
