c4601aeed0fd35506ebb125a4da6f96c5004387c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-04-12 01:24:39 -0300

    tests: improve tests on windows (#2892)

--- tests/test_windows_manifest.py
+++ tests/test_windows_manifest.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import ctypes
-import os
 import sys
 
 import pytest
@@ -61,67 +60,73 @@ simple.manifest
 """
 
 
-@pytest.fixture
-def tmp_manifest(tmp_package):  # noqa: ANN201
-    """Temporary path to build test manifest."""
+def test_manifest(tmp_package) -> None:
+    """With the correct manifest, windows version return 10.0 in Windows 10."""
     tmp_package.create(SOURCE)
     tmp_package.run()
-    return tmp_package
-
-
-def test_manifest(tmp_manifest) -> None:
-    """With the correct manifest, windows version return 10.0 in Windows 10."""
-    executable = tmp_manifest.executable("test_manifest")
+    executable = tmp_package.executable("test_manifest")
     assert executable.is_file()
-    output = tmp_manifest.run(executable, timeout=10)
+    output = tmp_package.run(executable, timeout=10)
     winver = sys.getwindowsversion()
     expected = f"Windows version: {winver.major}.{winver.minor}"
     assert output.splitlines()[0].strip() == expected
 
 
-def test_simple_manifest(tmp_manifest) -> None:
+if sys.version_info[:2] < (3, 12):
+    lief_versions = pytest.mark.parametrize(
+        "lief_version", ["0.16.4", "0.15.1", "0.14.1", "0.13.2"]
+    )
+else:
+    lief_versions = pytest.mark.parametrize(
+        "lief_version", ["0.16.4", "0.15.1", "0.14.1"]
+    )
+
+
+@lief_versions
+def test_simple_manifest(tmp_package, lief_version) -> None:
     """With simple manifest, without "supportedOS Id", windows version returned
     is the compatible version for Windows 8.1, ie, 6.2.
     """
-    if os.environ.get("CX_FREEZE_BIND", "") == "imagehlp":
-        pytest.skip(reason="Use of lief is disabled")
-    pytest.importorskip("lief", reason="Depends on extra package: lief")
-
-    executable = tmp_manifest.executable("test_simple_manifest")
+    tmp_package.create(SOURCE)
+    tmp_package.install(f"lief=={lief_version}")
+    tmp_package.run()
+    executable = tmp_package.executable("test_simple_manifest")
     assert executable.is_file()
-    output = tmp_manifest.run(executable, timeout=10)
+    output = tmp_package.run(executable, timeout=10)
     expected = "Windows version: 6.2"
     assert output.splitlines()[0].strip() == expected
 
     parser = PEParser([], [])
     manifest = parser.read_manifest(executable)
-    simple = tmp_manifest.path / "simple.manifest"
+    simple = tmp_package.path / "simple.manifest"
     assert manifest == simple.read_bytes().decode()
 
 
-def test_uac_admin(tmp_manifest) -> None:
+@lief_versions
+def test_uac_admin(tmp_package, lief_version) -> None:
     """With the uac_admin, should return WinError 740 - requires elevation."""
-    if os.environ.get("CX_FREEZE_BIND", "") == "imagehlp":
-        pytest.skip(reason="Use of lief is disabled")
-    pytest.importorskip("lief", reason="Depends on extra package: lief")
-
-    executable = tmp_manifest.executable("test_uac_admin")
-    assert executable.is_file()
     if ctypes.windll.shell32.IsUserAnAdmin():
         pytest.xfail(reason="User is admin")
+
+    tmp_package.create(SOURCE)
+    tmp_package.install(f"lief=={lief_version}")
+    tmp_package.run()
+    executable = tmp_package.executable("test_uac_admin")
+    assert executable.is_file()
     with pytest.raises(OSError, match="[WinError 740]"):
-        tmp_manifest.run(executable, timeout=10)
+        tmp_package.run(executable, timeout=10)
 
 
-def test_uac_uiaccess(tmp_manifest) -> None:
+@lief_versions
+def test_uac_uiaccess(tmp_package, lief_version) -> None:
     """With the uac_uiaccess, should return WinError 740."""
-    if os.environ.get("CX_FREEZE_BIND", "") == "imagehlp":
-        pytest.skip(reason="Use of lief is disabled")
-    pytest.importorskip("lief", reason="Depends on extra package: lief")
-
-    executable = tmp_manifest.executable("test_uac_uiaccess")
-    assert executable.is_file()
     if ctypes.windll.shell32.IsUserAnAdmin():
         pytest.xfail(reason="User is admin")
+
+    tmp_package.create(SOURCE)
+    tmp_package.install(f"lief=={lief_version}")
+    tmp_package.run()
+    executable = tmp_package.executable("test_uac_uiaccess")
+    assert executable.is_file()
     with pytest.raises(OSError, match="[WinError 740]"):
-        tmp_manifest.run(executable, timeout=10)
+        tmp_package.run(executable, timeout=10)
