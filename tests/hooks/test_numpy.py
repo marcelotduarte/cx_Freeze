9bdfe7ad45d90933bd33dcb36c8cae57c35644be
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-04-15 15:32:37 -0300

    tests: add test for rasterio (test DistributionCache.binary_files) (#2900)

--- tests/hooks/test_numpy.py
+++ tests/hooks/test_numpy.py
@@ -6,10 +6,14 @@ import sys
 
 import pytest
 
+from cx_Freeze._compat import ABI_THREAD
 
-@pytest.mark.parametrize(
-    "zip_packages", [False, True], ids=["", "zip-include-packages"]
+zip_packages = pytest.mark.parametrize(
+    "zip_packages", [False, True], ids=["", "zip_packages"]
 )
+
+
+@zip_packages
 def test_pandas(tmp_package, zip_packages: bool) -> None:
     """Test that the pandas/numpy is working correctly."""
     command = "python setup.py build_exe -O2"
@@ -31,9 +35,53 @@ def test_pandas(tmp_package, zip_packages: bool) -> None:
     assert len(lines) == 8, lines[2:]
 
 
-@pytest.mark.parametrize(
-    "zip_packages", [False, True], ids=["", "zip-include-packages"]
+SOURCE_TEST_RASTERIO = """
+test_rasterio.py
+    import numpy as np
+    import rasterio
+
+    print("Hello from cx_Freeze")
+    print("numpy version", np.__version__)
+    print("rasterio version", rasterio.__version__)
+pyproject.toml
+    [project]
+    name = "test_rasterio"
+    version = "0.1.2.3"
+
+    [tool.cxfreeze]
+    executables = ["test_rasterio.py"]
+
+    [tool.cxfreeze.build_exe]
+    excludes = ["tkinter", "unittest"]
+    silent = true
+"""
+
+
+@pytest.mark.skipif(
+    sys.version_info[:2] >= (3, 13) and ABI_THREAD != "",
+    reason="rasterio does not support Python 3.13t",
 )
+@zip_packages
+def test_rasterio(tmp_package, zip_packages: bool) -> None:
+    """Test if rasterio hook is working correctly."""
+    tmp_package.create(SOURCE_TEST_RASTERIO)
+    if zip_packages:
+        pyproject = tmp_package.path / "pyproject.toml"
+        buf = pyproject.read_bytes().decode().splitlines()
+        buf += ['zip_include_packages = "*"', 'zip_exclude_packages = ""']
+        pyproject.write_bytes("\n".join(buf).encode("utf_8"))
+    tmp_package.install("rasterio")
+    output = tmp_package.run()
+    executable = tmp_package.executable("test_rasterio")
+    assert executable.is_file()
+    output = tmp_package.run(executable, timeout=10)
+    lines = output.splitlines()
+    assert lines[0] == "Hello from cx_Freeze"
+    assert lines[1].startswith("numpy version")
+    assert lines[2].startswith("rasterio version")
+
+
+@zip_packages
 def test_scipy(tmp_package, zip_packages: bool) -> None:
     """Test that the scipy/numpy is working correctly."""
     command = "python setup.py build_exe -O2"
