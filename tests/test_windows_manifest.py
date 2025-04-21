12307bc5980c1cd7b57e47b6b48ff80c53fbffe1
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-04-21 03:35:10 -0300

    fix windows tests

--- tests/test_windows_manifest.py
+++ tests/test_windows_manifest.py
@@ -73,17 +73,16 @@ def test_manifest(tmp_package) -> None:
     assert output.splitlines()[0].strip() == expected
 
 
-LIEF_VERSIONS = ["disabled"]
+LIEF_VERSIONS = []
 if IS_WINDOWS:
-    if sys.version_info[:2] < (3, 12) and not IS_CONDA:
-        LIEF_VERSIONS += ["0.13.2"]
     LIEF_VERSIONS += ["0.14.1", "0.15.1", "0.16.4"]
+    if sys.version_info[:2] < (3, 12) and not IS_CONDA:
+        LIEF_VERSIONS.insert(0, "0.13.2")
 elif IS_MINGW:
     LIEF_VERSIONS += ["installed"]
-lief_versions = pytest.mark.parametrize("lief_version", LIEF_VERSIONS)
 
 
-@lief_versions
+@pytest.mark.parametrize("lief_version", [*LIEF_VERSIONS, "disabled"])
 def test_simple_manifest(tmp_package, lief_version) -> None:
     """With simple manifest, without "supportedOS Id", windows version returned
     is the compatible version for Windows 8.1, ie, 6.2.
@@ -97,25 +96,29 @@ def test_simple_manifest(tmp_package, lief_version) -> None:
     executable = tmp_package.executable("test_simple_manifest")
     assert executable.is_file()
     output = tmp_package.run(executable, timeout=10)
-    expected = "Windows version: 6.2"
+    if lief_version == "disabled":
+        expected = "Windows version: 10.0"
+    else:
+        expected = "Windows version: 6.2"
     assert output.splitlines()[0].strip() == expected
 
     parser = PEParser([], [])
     manifest = parser.read_manifest(executable)
-    simple = tmp_package.path / "simple.manifest"
-    assert manifest == simple.read_bytes().decode()
+    if lief_version == "disabled":
+        assert manifest == ""
+    else:
+        simple = tmp_package.path / "simple.manifest"
+        assert manifest == simple.read_bytes().decode()
 
 
-@lief_versions
+@pytest.mark.parametrize("lief_version", LIEF_VERSIONS)
 def test_uac_admin(tmp_package, lief_version) -> None:
     """With the uac_admin, should return WinError 740 - requires elevation."""
     if ctypes.windll.shell32.IsUserAnAdmin():
         pytest.xfail(reason="User is admin")
 
     tmp_package.create(SOURCE)
-    if lief_version == "disabled":
-        tmp_package.monkeypatch.setenv("CX_FREEZE_BIND", "imagehlp")
-    elif lief_version != "installed":
+    if lief_version != "installed":
         tmp_package.install(f"lief=={lief_version}")
     tmp_package.run()
     executable = tmp_package.executable("test_uac_admin")
@@ -124,16 +127,14 @@ def test_uac_admin(tmp_package, lief_version) -> None:
         tmp_package.run(executable, timeout=10)
 
 
-@lief_versions
+@pytest.mark.parametrize("lief_version", LIEF_VERSIONS)
 def test_uac_uiaccess(tmp_package, lief_version) -> None:
     """With the uac_uiaccess, should return WinError 740."""
     if ctypes.windll.shell32.IsUserAnAdmin():
         pytest.xfail(reason="User is admin")
 
     tmp_package.create(SOURCE)
-    if lief_version == "disabled":
-        tmp_package.monkeypatch.setenv("CX_FREEZE_BIND", "imagehlp")
-    elif lief_version != "installed":
+    if lief_version != "installed":
         tmp_package.install(f"lief=={lief_version}")
     tmp_package.run()
     executable = tmp_package.executable("test_uac_uiaccess")
