e9525681877bb3c6ccbb578ab176bdf20b866c26
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-05-11 06:27:09 -0300

    tests: improve install-tools and fixes build-wheel (#2950)

--- ci/install-tools.sh
+++ ci/install-tools.sh
@@ -11,6 +11,25 @@ else
     PY_PLATFORM=""
 fi
 
+# Usage
+if ! [ -z "$1" ] && [ "$1" == "--help" ]; then
+    echo "Usage:"
+    echo "$0 [--tests]"
+    echo "Where:"
+    echo "  --tests Install additional packages to run `pytest`."
+    exit 1
+fi
+
+INSTALL_TESTS=""
+while ! [ -z "$1" ]; do
+    if [ "$1" == "--tests" ]; then
+        INSTALL_TESTS=true
+    else
+        echo "WARNING: invalid option '$1'"
+    fi
+    shift
+done
+
 echo "::group::Install dependencies and build tools"
 # Install/update uv and dev tools
 if [[ $PY_PLATFORM == mingw* ]]; then
@@ -30,10 +49,12 @@ if [[ $PY_PLATFORM == mingw* ]]; then
     done < requirements.txt
 
     # pytest and dependencies
-    while read -r line; do
-        name=$(echo $line | awk -F '[><=]+' '{ print $1 }')
-        pkgs+=("$MINGW_PACKAGE_PREFIX-python-$name")
-    done < tests/requirements.txt
+    if [ "$INSTALL_TESTS" == "true" ]; then
+        while read -r line; do
+            name=$(echo $line | awk -F '[><=]+' '{ print $1 }')
+            pkgs+=("$MINGW_PACKAGE_PREFIX-python-$name")
+        done < tests/requirements.txt
+    fi
 
     pacman --needed --noconfirm --quiet -S ${pkgs[@]}
 else
@@ -50,7 +71,14 @@ else
             env UV_INSTALL_DIR="$INSTALL_DIR" sh
         fi
     fi
-    uv pip install --extra tests --upgrade -r pyproject.toml
+
+    # Dependencies of cx_Freeze
+    if [ "$INSTALL_TESTS" == "true" ]; then
+        # including pytest and dependencies
+        uv pip install --extra tests --upgrade -r pyproject.toml
+    else
+        uv pip install --upgrade -r pyproject.toml
+    fi
 fi
 
 # Install dev tools
