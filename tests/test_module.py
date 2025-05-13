0dac181464e8168d8e41a3cf97023fbbe8a46f04
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-05-13 20:06:59 -0300

    test: module.DistributionCache test conversion of egg_info to dist_info

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,12 +2,15 @@
 
 from __future__ import annotations
 
+import os
+import shutil
 from importlib.machinery import EXTENSION_SUFFIXES
 from types import CodeType
 
 import pytest
 
 from cx_Freeze import ConstantsModule, Module
+from cx_Freeze._compat import IS_CONDA, IS_MINGW
 from cx_Freeze.exception import OptionError
 
 SOURCE = """
@@ -88,3 +91,60 @@ def test_invalid(
     """Test invalid values to use in ConstantsModule class."""
     with pytest.raises(expected_exception, match=expected_match):
         class_to_test(**kwargs)
+
+
+SOURCE_TEST_EGG_INFO = """
+test_egg_info.py
+    print("Hello from cx_Freeze")
+    import module1
+    import module2
+pyproject.toml
+    [project]
+    name = "test_egg_info"
+    version = "0.1.2.3"
+
+    [tool.cxfreeze]
+    executables = ["test_egg_info.py"]
+
+    [tool.cxfreeze.build_exe]
+    excludes = ["tkinter", "unittest"]
+    silent = true
+extra/module1.py
+    print("Hello module1")
+extra/module1.egg-info
+    Name: module1
+    Version: 1.0
+extra/module2.py
+    print("Hello module2")
+extra/module2.egg-info/PKG-INFO
+    Name: module2
+    Version: 1.0
+extra/module2.egg-info/entry_points.txt
+    [distutils.setup_keywords]
+    entry_points = setuptools.dist:check_entry_points
+extra/module2.egg-info/top_level.txt
+    module2
+"""
+
+
+@pytest.mark.skipif(IS_MINGW, reason="Disabled in MinGW")
+@pytest.mark.skipif(IS_CONDA, reason="Disabled in conda-forge")
+def test_egg_info(tmp_package) -> None:
+    """Use fake packages to test conversion of egg_info to dist_info in
+    DistributionCache.
+    """
+    tmp_package.create(SOURCE_TEST_EGG_INFO)
+
+    # patch the fake packages
+    tmp_package.prefix = tmp_package.path / ".tmp_prefix"
+    tmp_site = tmp_package.prefix / tmp_package.relative_site
+    shutil.copytree(tmp_package.path / "extra", tmp_site, dirs_exist_ok=True)
+    tmp_package.monkeypatch.setenv("PYTHONPATH", os.path.normpath(tmp_site))
+
+    output = tmp_package.run()
+    executable = tmp_package.executable("test_egg_info")
+    assert executable.is_file()
+    output = tmp_package.run(executable, timeout=10)
+    assert output.splitlines()[0] == "Hello from cx_Freeze"
+    assert output.splitlines()[1].startswith("Hello module1")
+    assert output.splitlines()[2].startswith("Hello module2")
