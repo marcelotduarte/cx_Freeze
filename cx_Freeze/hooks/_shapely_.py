2dace6c59765615ca8d476e71e984c279c04ada2
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-05-18 01:18:27 -0300

    hooks: fix shapely in linux - add tests

--- cx_Freeze/hooks/_shapely_.py
+++ cx_Freeze/hooks/_shapely_.py
@@ -6,19 +6,54 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from cx_Freeze.hooks.libs import replace_delvewheel_patch
+from cx_Freeze.module import Module, ModuleHook
+
 if TYPE_CHECKING:
     from cx_Freeze.finder import ModuleFinder
-    from cx_Freeze.module import Module
 
 
-def load_shapely(finder: ModuleFinder, module: Module) -> None:
-    """Hook for shapely."""
-    module_path = module.file.parent
-    source_dir = module_path.parent / f"{module.name}.libs"
-    if not source_dir.exists():
-        source_dir = module_path.parent / "Shapely.libs"
-    if source_dir.exists():
-        for source in source_dir.iterdir():
-            target = f"lib/{source_dir.name}/{source.name}"
-            finder.lib_files[source] = target
-            finder.include_files(source, target)
+__all__ = ["Hook"]
+
+
+class Hook(ModuleHook):
+    """The Hook class for shapely.
+
+    Supported pypi versions (tested from 1.8.0 to 2.1.0).
+    """
+
+    # Note: shapely 1.8.5 supports Python <= 3.11
+
+    def shapely(self, finder: ModuleFinder, module: Module) -> None:
+        """Hook for shapely."""
+        distribution = module.distribution
+        if distribution:
+            for file in distribution.binary_files:
+                finder.include_files(
+                    file.locate().resolve(), f"lib/{file.as_posix()}"
+                )
+        replace_delvewheel_patch(module)
+        finder.exclude_module("shapely.examples")  # shapely < 2.0
+        finder.exclude_module("shapely.tests")  # shapely >= 2.0
+
+    def shapely_geos(self, finder: ModuleFinder, module: Module) -> None:
+        """Hook for shapely.geos for shapely < 2.0."""
+        # patch the code when necessary
+        if module.in_file_system == 0:
+            module.code = compile(
+                module.file.read_bytes().replace(
+                    b"__file__", b"__file__.replace('library.zip', '.')"
+                ),
+                module.file.as_posix(),
+                "exec",
+                dont_inherit=True,
+                optimize=finder.optimize,
+            )
+
+    def shapely__geometry_helpers(
+        self,
+        finder: ModuleFinder,
+        module: Module,  # noqa: ARG002
+    ) -> None:
+        """Hook for shapely._geometry_helpers for shapely >= 2.0."""
+        finder.include_module("shapely._geos")
