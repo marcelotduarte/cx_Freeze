06098c4875e13a6fe314877b19849b42277e2ac3
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-05-27 13:44:09 -0300

    hooks: fix pymupdf - add tests (#2977)
    * hooks: fix pymupdf - add tests
    
    * pymupdf does not support Python 3.13t

--- cx_Freeze/hooks/_pymupdf_.py
+++ cx_Freeze/hooks/_pymupdf_.py
@@ -4,21 +4,27 @@ pymupdf package is included.
 
 from __future__ import annotations
 
+from contextlib import suppress
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from cx_Freeze.finder import ModuleFinder
     from cx_Freeze.module import Module
 
+# Support for pymupdf >= 1.24.4
+
 
 def load_pymupdf(finder: ModuleFinder, module: Module) -> None:
     """The pymupdf must include hidden modules."""
+    if module.in_file_system == 0:
+        module.in_file_system = 1
     module.ignore_names.update(
         ["mupdf_cppyy", "mupdf", "pymupdf_fonts", "PIL"]
     )
     finder.include_module("pymupdf.mupdf")
     finder.include_module("pymupdf.utils")
-    finder.include_module("pymupdf._wxcolors")
+    with suppress(ImportError):
+        finder.include_module("pymupdf._wxcolors")  # 1.25.4
 
 
 def load_pymupdf_extra(_, module: Module) -> None:
