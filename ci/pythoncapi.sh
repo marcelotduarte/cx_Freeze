4e8fa2f844027fb54b631ac8013e40994926f9ac
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-06-07 16:55:14 -0300

    fix pre-commit run

--- ci/pythoncapi.sh
+++ ci/pythoncapi.sh
@@ -7,9 +7,7 @@ TOP_DIR=$(cd "$CI_DIR"/.. && pwd)
 
 CI_TMPDIR=$(python -c "import tempfile; print(tempfile.mkdtemp())")
 pushd "$CI_TMPDIR" >/dev/null || exit
-git clone -q https://github.com/python/pythoncapi-compat
-popd >/dev/null || exit
-pushd "$CI_TMPDIR"/pythoncapi-compat >/dev/null || exit
+git clone -q https://github.com/python/pythoncapi-compat .
 python upgrade_pythoncapi.py --download "$TOP_DIR"/source/bases/
 cp "$TOP_DIR"/source/bases/pythoncapi_compat.h "$TOP_DIR"/source/legacy/
 python upgrade_pythoncapi.py "$TOP_DIR"/source/
