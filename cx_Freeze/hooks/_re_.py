8f9279655be45c3ac9678993b26d447924f46a0c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-06-25 04:06:37 -0300

    typo

--- cx_Freeze/hooks/_re_.py
+++ cx_Freeze/hooks/_re_.py
@@ -19,10 +19,6 @@ __all__ = ["Hook"]
 class Hook(ModuleHook):
     """Ignore names that should not be confused with modules to be imported."""
 
-    def vtkmodules(
-        self,
-        finder: ModuleFinder,  # noqa:ARG002
-        module: Module,
-    ) -> None:
+    def re(self, _finder: ModuleFinder, module: Module) -> None:
         if module.path:  # package since Python 3.11
             module.global_names.update(RE_GLOBAL_NAMES)
