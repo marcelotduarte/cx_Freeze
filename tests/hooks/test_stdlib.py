a423c3860c1463ef70d8cb01e440057f357a21d2
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-07-12 03:14:30 -0300

    fix: use the latest manylinux image to fix sqlite3 issue on linux

--- tests/hooks/test_stdlib.py
+++ tests/hooks/test_stdlib.py
@@ -67,6 +67,65 @@ def test_sqlite(tmp_package, zip_packages: bool) -> None:
     result.stdout.fnmatch_lines("dump.sql created")
 
 
+SOURCE_TEST_SQLITE = """
+test_sqlite.py
+    import sqlite3
+
+    print("Hello from cx_Freeze")
+    print(sqlite3.__name__, sqlite3.sqlite_version)
+
+    conn = sqlite3.connect(":memory:")
+    try:
+        conn.execute("create virtual table fts5test using fts5 (data);")
+    except:
+        import sys
+        sys.exit()
+    conn.execute(
+        "insert into fts5test (data) "
+        "values ('this is a test of full-text search');"
+    )
+    print(
+        conn.execute(
+            "select * from fts5test where data match 'full';"
+        ).fetchall()
+    )
+pyproject.toml
+    [project]
+    name = "test_sqlite"
+    version = "0.1.2.3"
+
+    [tool.cxfreeze]
+    executables = ["test_sqlite.py"]
+
+    [tool.cxfreeze.build_exe]
+    excludes = ["tkinter", "unittest"]
+    silent = true
+"""
+
+
+@zip_packages
+def test_sqlite_ext(tmp_package, zip_packages: bool) -> None:
+    """Test that the sqlite3 is working correctly."""
+    tmp_package.create(SOURCE_TEST_SQLITE)
+    if zip_packages:
+        pyproject = tmp_package.path / "pyproject.toml"
+        buf = pyproject.read_bytes().decode().splitlines()
+        buf += ['zip_include_packages = "*"', 'zip_exclude_packages = ""']
+        pyproject.write_bytes("\n".join(buf).encode("utf_8"))
+    tmp_package.freeze()
+    executable = tmp_package.executable("test_sqlite")
+    assert executable.is_file()
+    result = tmp_package.run(executable, timeout=TIMEOUT)
+    print(result.stdout)
+    result.stdout.fnmatch_lines(
+        [
+            "Hello from cx_Freeze",
+            "sqlite3 *",
+            "[('this is a test of full-text search',)]",
+        ]
+    )
+
+
 SOURCE_TEST_SSL = """
 test_ssl.py
     import os
