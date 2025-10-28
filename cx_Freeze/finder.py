marcelotduarte/cx_Freeze

commit 11c371843ca293bd471ebba51959d1e75e65b268

finder: detect editable packages (Python 3.10+ only) (#3036)
* Update finder to support editable packages

* Add a test for editable packages

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* fix # noqa: SLF001

* fix another # noqa: SLF001

* Skip test_editable_packages on 3.9

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Marcelo Duarte <marcelotduarte@users.noreply.github.com>

cx_Freeze/finder.py
@@ -369,6 +369,42 @@ class ModuleFinder:
             return None
         return module
 
+    @staticmethod
+    def _find_editable_spec(
+        name: str, path: Sequence[str] | None
+    ) -> importlib.machinery.ModuleSpec | None:
+        """Find the spec for a module installed as an editable package."""
+        if hasattr(importlib.metadata, "packages_distributions"):
+            # the distribution name may vary from the module name (eg may include '-').
+            # packages_distributions returns the mapping but is only available on 3.10+
+            dist_names = importlib.metadata.packages_distributions().get(
+                name, []
+            )
+        else:
+            dist_names = [name]
+
+        for dist_name in dist_names:
+            dist = importlib.metadata.distribution(dist_name)
+            if not dist:
+                continue
+            for f in dist.files:
+                if f.name.startswith("__editable__") and f.name.endswith(
+                    "_finder.py"
+                ):
+                    try:
+                        mod = importlib.import_module(f.stem)
+                        spec = mod._EditableFinder.find_spec(  # noqa: SLF001
+                            name, path
+                        )
+                        if spec:
+                            return spec
+                    except (ImportError, AttributeError) as e:
+                        logger.warning(
+                            "Find editable spec failed for [%s]: %s", name, e
+                        )
+                        break
+        return None
+
     def _load_module(
         self,
         name: str,
@@ -401,6 +437,9 @@ class ModuleFinder:
                     self.modules.remove(module)
                 module.in_import = False
                 return module
+
+        if not spec:
+            spec = ModuleFinder._find_editable_spec(name, path)
 
         if spec:
             loader = spec.loader

tests/datatest.py
@@ -445,3 +445,21 @@ setup.py
     )
 """,
 ]
+
+EDITABLE_PACKAGE_TEST = [
+    "main",
+    ["foobar", "foobar.baz", "main"],
+    [],
+    [],
+    """\
+main.py
+    import foobar.baz
+foo-bar/setup.py
+    from distutils.core import setup
+    setup(name="foo-bar", version="0.0.1", packages=["foobar"])
+foo-bar/foobar/__init__.py
+    print('This is foobar')
+foo-bar/foobar/baz.py
+    print('This is foobar.baz')
+""",
+]

tests/test_modulefinder.py
@@ -11,12 +11,14 @@ import pytest
 
 from cx_Freeze import ConstantsModule, ModuleFinder
 
+from .conftest import HAVE_UV
 from .datatest import (
     ABSOLUTE_IMPORT_TEST,
     BYTECODE_TEST,
     CODING_DEFAULT_UTF8_TEST,
     CODING_EXPLICIT_CP1252_TEST,
     CODING_EXPLICIT_UTF8_TEST,
+    EDITABLE_PACKAGE_TEST,
     EXTENDED_OPARGS_TEST,
     IMPORT_CALL_TEST,
     MAYBE_TEST,
@@ -157,4 +159,20 @@ def test_zip_exclude_packages(tmp_package) -> None:
         zip_exclude_packages=["p"],
         zip_include_packages=["*"],
         zip_include_all_packages=True,
+    )
+
+
+@pytest.mark.skipif(
+    not HAVE_UV or sys.version_info < (3, 10),
+    reason="uv and 3.10+ are needed for editable packages",
+)
+def test_editable_packages(tmp_package) -> None:
+    """Provides test cases for ModuleFinder class."""
+    tmp_package.create(EDITABLE_PACKAGE_TEST[4])
+    tmp_package._install_uv(  # noqa: SLF001
+        ["-e", f"{tmp_package.path}/foo-bar"]
+    )
+    _do_test(
+        tmp_package,
+        *EDITABLE_PACKAGE_TEST,
     )

commit 563990d00cb21ce1c62aa4581d979df4df22283a

finder: optimized implementation of '_find_editable_spec' (#3147)
Optimized the "find_editable_spec" implementation by refactoring it from a static method to a class method, resulting in better performance.

Moved "importlib.metadata.packages_distributions()" call from the static method to ModuleFinder initialization to avoid rebuilding package distributions for each package file.

Maintainer note: fix performance regression introduced in PR #3036

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

cx_Freeze/finder.py
@@ -88,6 +88,9 @@ class ModuleFinder:
         self._tmp_dir = TemporaryDirectory(prefix="cxfreeze-")
         self.cache_path = Path(self._tmp_dir.name)
         self.lib_files: dict[Path, str] = {}
+        self.packages_distributions = (
+            importlib.metadata.packages_distributions()
+        )
 
     def cleanup(self) -> None:
         self._tmp_dir.cleanup()
@@ -350,16 +353,13 @@ class ModuleFinder:
             return None
         return module
 
-    @staticmethod
     def _find_editable_spec(
-        name: str, path: Sequence[str] | None
+        self, name: str, path: Sequence[str] | None
     ) -> importlib.machinery.ModuleSpec | None:
         """Find the spec for a module installed as an editable package."""
         # the distribution name may vary from the module name (eg may
         # include '-'). packages_distributions returns the mapping
-        dist_names = importlib.metadata.packages_distributions().get(name, [])
-
-        for dist_name in dist_names:
+        for dist_name in self.packages_distributions.get(name, []):
             dist = importlib.metadata.distribution(dist_name)
             if not dist:
                 continue
@@ -415,7 +415,7 @@ class ModuleFinder:
                 return module
 
         if not spec:
-            spec = ModuleFinder._find_editable_spec(name, path)
+            spec = self._find_editable_spec(name, path)
 
         if spec:
             loader = spec.loader