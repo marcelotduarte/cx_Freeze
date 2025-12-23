d8de5c78502ce1a61433ebd98e5444fe806712c4
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2025-12-23 12:42:44 -0300

    hooks: support pyparsing 3.3.x (#3218)

--- cx_Freeze/hooks/_pyparsing_.py
+++ cx_Freeze/hooks/_pyparsing_.py
@@ -19,10 +19,27 @@ __all__ = ["Hook"]
 class Hook(ModuleHook):
     """The Hook class for pyparsing."""
 
-    def pyparsing(self, _finder: ModuleFinder, module: Module) -> None:
+    def pyparsing(self, finder: ModuleFinder, module: Module) -> None:
         """Define the global names to avoid spurious missing modules."""
         module.global_names.update(PYPARSING_GLOBAL_NAMES)
 
+        # remove testing module
+        finder.exclude_module("pyparsing.testing")
+        code_bytes = module.file.read_bytes()
+        search = b"from .testing import pyparsing_test as testing"
+        replace = b"testing = None"
+        module.code = compile(
+            code_bytes.replace(search, replace),
+            module.file.as_posix(),
+            "exec",
+            dont_inherit=True,
+            optimize=finder.optimize,
+        )
+
+    def pyparsing_core(self, _finder: ModuleFinder, module: Module) -> None:
+        """Ignore optional (excluded) module."""
+        module.ignore_names.add("pyparsing.testing")
+
     def pyparsing_diagram(self, _finder: ModuleFinder, module: Module) -> None:
-        """Ignore optional package."""
+        """Ignore optional packages."""
         module.ignore_names.update(["jinja2", "railroad"])
