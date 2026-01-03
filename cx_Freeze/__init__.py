67921421e7dff0d6e56bc2a3e6430c8596c09a92
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-01-03 06:04:01 -0300

    Bump version: 8.5.2 → 8.5.3 [ci skip]

--- cx_Freeze/__init__.py
+++ cx_Freeze/__init__.py
@@ -49,7 +49,7 @@ else:
     __all__ += ["bdist_appimage", "bdist_deb", "bdist_rpm"]
 
 
-__version__ = "8.5.2"
+__version__ = "8.5.3"
 
 
 def setup(**attrs) -> setuptools.Distribution:  # noqa: D103
