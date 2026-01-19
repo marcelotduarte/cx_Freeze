a82c76e067205713dbe477ee2595ef8adcca5980
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-01-19 18:51:54 -0300

    hooks: add streamlit (#3214)
    * hooks: add streamlit
    
    * add tests and additional fixes
    
    * streamlit does not support Python 3.13t/3.14t
    
    * ignore email prompt on first use
    
    * handles timeout errors

---
+++ cx_Freeze/hooks/_streamlit_.py
@@ -0,0 +1,33 @@
+"""A collection of functions which are triggered automatically by finder when
+streamlit package is included.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from cx_Freeze.module import Module, ModuleHook
+
+if TYPE_CHECKING:
+    from cx_Freeze.finder import ModuleFinder
+
+
+__all__ = ["Hook"]
+
+
+class Hook(ModuleHook):
+    """The Hook class for 'streamlit' package."""
+
+    def streamlit(self, finder: ModuleFinder, module: Module) -> None:
+        """The streamlit must be loaded as a package in the file system."""
+        module.in_file_system = 1
+        finder.exclude_module("streamlit.testing")
+        finder.include_package("streamlit")
+
+    def streamlit_runtime_scriptrunner_script_runner(
+        self,
+        finder: ModuleFinder,
+        module: Module,  # noqa: ARG002
+    ) -> None:
+        """Include a required submodule."""
+        finder.include_module("streamlit.runtime.scriptrunner.magic_funcs")
---
+++ tests/hooks/test_streamlit.py
@@ -0,0 +1,74 @@
+"""Tests for hooks of streamlit."""
+
+from __future__ import annotations
+
+import sys
+
+import pytest
+
+from cx_Freeze._compat import ABI_THREAD, IS_MINGW
+
+TIMEOUT = 15
+
+zip_packages = pytest.mark.parametrize(
+    "zip_packages", [False, True], ids=["", "zip_packages"]
+)
+SOURCE_TEST = """
+test_streamlit.py
+    import sys
+    import streamlit
+
+    streamlit.header("Hello from cx_Freeze")
+    streamlit.write(f"streamlit version {streamlit.__version__}")
+pyproject.toml
+    [project]
+    name = "test_streamlit"
+    version = "0.1.2.3"
+    dependencies = ["streamlit"]
+
+    [[tool.cxfreeze.executables]]
+    script = "test_streamlit.py"
+    init_script = "streamlit"
+
+    [tool.cxfreeze.build_exe]
+    include_msvcr = true
+    excludes = ["tkinter", "unittest"]
+    silent = true
+"""
+
+
+@pytest.mark.xfail(
+    IS_MINGW,
+    raises=ModuleNotFoundError,
+    reason="streamlit not supported in mingw",
+    strict=True,
+)
+@pytest.mark.skipif(
+    sys.version_info[:2] >= (3, 13) and ABI_THREAD == "t",
+    reason="streamlit does not support Python 3.13t/3.14t",
+)
+@pytest.mark.venv
+@zip_packages
+def test_streamlit(tmp_package, zip_packages: bool) -> None:
+    """Test if streamlit hook is working correctly."""
+    tmp_package.create(SOURCE_TEST)
+    if zip_packages:
+        pyproject = tmp_package.path / "pyproject.toml"
+        buf = pyproject.read_bytes().decode().splitlines()
+        buf += ['zip_include_packages = "*"', 'zip_exclude_packages = ""']
+        pyproject.write_bytes("\n".join(buf).encode("utf_8"))
+    tmp_package.freeze()
+
+    executable = tmp_package.executable("test_streamlit")
+    assert executable.is_file()
+    result = tmp_package.run(
+        [
+            executable,
+            "--server.showEmailPrompt=false",
+            "--browser.gatherUsageStats=false",
+        ],
+        timeout=TIMEOUT,
+    )
+    result.stdout.fnmatch_lines(
+        ["*You can now view your Streamlit app in your browser."]
+    )
