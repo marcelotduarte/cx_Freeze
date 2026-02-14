d483fd942008362fcf3f777ffde3696a4daab64a
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-02-14 00:39:22 -0300

    hooks: improve pytz to work w/ setuptools 82+ in zip mode

--- tests/hooks/test_pytz.py
+++ tests/hooks/test_pytz.py
@@ -27,3 +27,25 @@ def test_pytz(tmp_package, zip_packages: bool) -> None:
     result.stdout.fnmatch_lines(
         ["UTC time: *", "Brazil time: *", "US Eastern time: *"]
     )
+
+
+@pytest.mark.venv(install_dependencies=False)
+def test_pytz_setuptools_82plus(tmp_package) -> None:
+    """Test if pytz hook is working with setuptools 82+."""
+    tmp_package.create_from_sample("pytz")
+    pyproject = tmp_package.path / "pyproject.toml"
+    buf = pyproject.read_bytes().decode().splitlines()
+    for i, line in enumerate(buf):
+        if "setuptools" in line:
+            buf[i] = line.replace("setuptools<82.0", "setuptools>=82.0")
+    buf += ['zip_include_packages = "*"', 'zip_exclude_packages = ""']
+    pyproject.write_bytes("\n".join(buf).encode("utf_8"))
+    tmp_package.install_dependencies()
+    tmp_package.freeze()
+
+    executable = tmp_package.executable("test_pytz")
+    assert executable.is_file()
+    result = tmp_package.run(executable)
+    result.stdout.fnmatch_lines(
+        ["UTC time: *", "Brazil time: *", "US Eastern time: *"]
+    )
