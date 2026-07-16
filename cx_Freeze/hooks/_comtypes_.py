6bfa0423515a2da86f73ff946a192174a496e07c
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-07-16 13:42:42 -0300

    Revise docstring in _comtypes_.py
    Updated docstring to clarify purpose of hooks.

--- cx_Freeze/hooks/_comtypes_.py
+++ cx_Freeze/hooks/_comtypes_.py
@@ -1,6 +1,4 @@
-"""A collection of functions which are triggered automatically by finder when
-comtypes package is included.
-"""
+"""Hooks triggered by finder when comtypes package is included."""
 
 from __future__ import annotations
 
