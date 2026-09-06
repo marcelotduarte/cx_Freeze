b50434cc864e09df2fe6102671fa8090280afc11
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-09-05 21:29:00 -0300

    make tests pass

--- tests/hooks/test_streamlit.py
+++ tests/hooks/test_streamlit.py
@@ -29,7 +29,10 @@ pyproject.toml
     [project]
     name = "test_streamlit"
     version = "0.1.2.3"
-    dependencies = ["streamlit>=1.51"]
+    dependencies = [
+        "streamlit>=1.51",
+        "anyio<4.15.1",
+    ]
 
     [[tool.cxfreeze.executables]]
     script = "test_streamlit.py"
