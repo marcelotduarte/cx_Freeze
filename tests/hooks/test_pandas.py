84ed7d9566449039db9f78dc085323348b07c02e
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-09-19 01:55:59 -0300

    merge test_mkl with test_pandas

--- tests/hooks/test_pandas.py
+++ tests/hooks/test_pandas.py
@@ -1,17 +1,26 @@
-"""Tests for hooks of pandas."""
+"""Tests for hooks of pandas (and numpy and mkl)."""
 
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING
 
 import pytest
 
-from cx_Freeze._compat import IS_CONDA, IS_MACOS
+from cx_Freeze._compat import IS_CONDA, IS_LINUX, IS_MACOS, IS_X86_64
 
 if TYPE_CHECKING:
     from tests.conftest import TempPackage
 
 TIMEOUT_SLOW = 30 * (2 if IS_CONDA else 1) * (2 if IS_MACOS else 1)
+
+USE_MKL = [pytest.param(False, id="")]
+if not IS_CONDA and (
+    IS_LINUX and IS_X86_64 and sys.version_info[:2] == (3, 11)
+):
+    USE_MKL.append(pytest.param(True, id="use_mkl"))
+
+use_mkl = pytest.mark.parametrize("use_mkl", USE_MKL)
 
 zip_packages = pytest.mark.parametrize(
     "zip_packages", [False, True], ids=["", "zip_packages"]
@@ -68,16 +77,27 @@ pyproject.toml
 """
 
 
-@pytest.mark.venv
+@pytest.mark.venv(install_dependencies=False)
+@use_mkl
 @zip_packages
-def test_pandas(tmp_package: TempPackage, zip_packages: bool) -> None:
-    """Test that the pandas/numpy is working correctly."""
+def test_pandas(
+    tmp_package: TempPackage, zip_packages: bool, use_mkl: bool
+) -> None:
+    """Test that the pandas/numpy/mkl is working correctly."""
     tmp_package.create(SOURCE_TEST_PANDAS)
     if zip_packages:
         pyproject = tmp_package.path / "pyproject.toml"
         buf = pyproject.read_bytes().decode().splitlines()
         buf += ['zip_include_packages = "*"', 'zip_exclude_packages = ""']
         pyproject.write_bytes("\n".join(buf).encode("utf_8"))
+
+    if use_mkl:  # numpy with mkl
+        tmp_package.install(
+            "numpy", index="https://pypi.anaconda.org/intel/simple"
+        )
+        tmp_package.install("pandas")
+    else:
+        tmp_package.install_dependencies()
     tmp_package.freeze()
 
     executable = tmp_package.executable("test_pandas")
