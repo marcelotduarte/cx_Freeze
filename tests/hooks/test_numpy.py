5f4a0edd952247e8f72340f1f1273d5310ff4fb4
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2026-09-22 01:03:29 -0300

    make tests pass

--- tests/hooks/test_numpy.py
+++ tests/hooks/test_numpy.py
@@ -127,18 +127,6 @@ pyproject.toml
 """
 
 
-@pytest.mark.xfail(
-    sys.version_info[:2] >= (3, 15),
-    raises=ModuleNotFoundError,
-    reason="shapely does not support Python 3.15 yet",
-    strict=not bool(int(os.getenv("PYTEST_LAX_XFAIL", "0"))),
-)
-@pytest.mark.xfail(
-    IS_WINDOWS and IS_ARM_64,
-    raises=ModuleNotFoundError,
-    reason="shapely does not support Windows arm64",
-    strict=not bool(int(os.getenv("PYTEST_LAX_XFAIL", "0"))),
-)
 @pytest.mark.venv
 @zip_packages
 def test_shapely(tmp_package: TempPackage, zip_packages: bool) -> None:
